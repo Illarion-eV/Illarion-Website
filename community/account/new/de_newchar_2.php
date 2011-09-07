6bca373282c5039eb08ed2857bd95ccb5d40f487
kadiya <kadiya@illarion.org>
2011-09-07 15:53:59 +0200

    shellcript aufruf test 1

--- community/account/new/de_newchar_2.php
+++ community/account/new/de_newchar_2.php
@@ -134,7 +134,8 @@
 
 
 			<div style="background-image: url(<?php echo $url; ?>/shared/pics/char_screen.jpg);float:left;border:2px groove #000;width:300px;height:200px;">
-			<img src="<?php echo $url; ?>/shared/pics/chars/hum/hum_m_stand_w.png" style="position:relative;left:133px; top:73px; width:28px; height:75px;" />
+			<?php exec('/shared/scripts/img_convert.sh #99000 /shared/pics/chars/hum/hum_m_stand_w.png /tmp/image_west.png'); ?>
+			<img src="<?php echo $url; ?>/tmp/image_west.png" style="position:relative;left:133px; top:73px; width:28px; height:75px;" />
 			<img src="<?php echo $url; ?>/shared/pics/chars/hum/hum_m_stand_n.png" style="position:relative;left:183px; top:73px; width:28px; height:75px;" />
 			</div>
 
