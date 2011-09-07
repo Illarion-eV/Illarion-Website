daa0c5d0ec88651d7fee3b8a39ffe534180aff28
kadiya <kadiya@illarion.org>
2011-09-07 20:28:52 +0200

    moep

--- community/account/new/de_newchar_2.php
+++ community/account/new/de_newchar_2.php
@@ -147,7 +147,7 @@
 				<span id="skin_color" style="width:80%;height:30px;display:block;"></span>
 				<input type="hidden" id="skincolor" value="" name="skincolor" />
                 <?php foreach ( $skincolors as $color ): ?>
-                <a onclick="colorChange(<?php echo $color; ?>)" style="display: block;height: 10px;width: 10px;float: left;background-color: <?php echo $color; ?>;border: 1px solid black;"></a>
+                <a onclick="colorChange('<?php echo $color; ?>')" style="display: block;height: 10px;width: 10px;float: left;background-color: <?php echo $color; ?>;border: 1px solid black;"></a>
                 <?php endforeach; ?>
 			</div>
 
