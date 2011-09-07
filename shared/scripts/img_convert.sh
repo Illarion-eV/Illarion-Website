67b3156321ac23afb6690676f28ecef4dd7082f4
kadiya <kadiya@illarion.org>
2011-09-07 17:11:55 +0200

    moep

--- shared/scripts/img_convert.sh
+++ shared/scripts/img_convert.sh
@@ -1,3 +1,12 @@
+
+# Farben von Images multiplizieren
+#
+# Parameter:
+# 1: Farbcodierung (ohne Raute)
+# 2: Input-file (mit Pfadangabe und Dateiendung)
+# 3: Output-file (mit Pfadangabe und Dateiendung)
+#
+
 dummy="$(mktemp).png"
 convert -size 300x300 xc:\#$1 $dummy
-convert -compose multiply $2 -channel RGB $dummy -composite $3
+convert -compos
