e0129a00f8a6779fb17457fe702f4bf18dda8adc
kadiya <kadiya@illarion.org>
2011-09-07 15:50:52 +0200

    shellscript zur Farbconvertierung von Images

---
+++ shared/scripts/img_convert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Farben von Images multiplizieren
+#
+# Parameter:
+# 1: Farbcodierung (ohne Raute)
+# 2: Input-file (mit Pfadangabe und Dateiendung)
+# 3: Output-file (mit Pfadangabe und Dateiendung)
+#
+
+dummy="$(mktemp).png"
+convert -size 300x300 xc:\#$1 $dummy
+convert -compose multiply $2 -channel RGB $dummy -composite $3
