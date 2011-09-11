ec5368e6d9a1955715343a401ecf38a5195beb42
kadiya <kadiya@illarion.org>
2011-09-11 23:30:42 +0200

    moep

--- community/account/new/de_newchar_2.php
+++ community/account/new/de_newchar_2.php
@@ -171,7 +171,7 @@
 				echo $race."-".RACE_ELF."-".RACE_LIZARD."<br/>";
 				?>
 				<?php if (($sex == GENDER_MALE) && ($race != RACE_ELF) && ($race != RACE_LIZARD)) ?>
-				<?php {
+				<?php { ?>
 					Bart:
 					<select name="beard" style="width:100%;">
 						<option value="1">Eins</option>
