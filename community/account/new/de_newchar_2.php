0fc5eb811592d76342e87167d557b9175de85456
kadiya <kadiya@illarion.org>
2011-09-29 17:39:23 +0200

    moep

--- community/account/new/de_newchar_2.php
+++ community/account/new/de_newchar_2.php
@@ -141,10 +141,10 @@
 
 			<div style="background-image: url(<?php echo $url; ?>/shared/pics/char_screen.jpg);float:left;border:2px groove #000;width:300px;height:250px;">
 			<div id="ajax_works" style='display:block;position:relative;left:5px;top:5px;width:32px;height:32px;margin-bottom:-32px;'></div>
-			<img id="char_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getImageName($race, $sex),"#DCC3AF"); ?>" style="position:relative;left:133px; top:73px;display:block;margin-bottom:-100px;" />
+			<img id="char_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getImageName($race, $sex),"DCC3AF"); ?>" style="position:relative;left:133px; top:73px;display:block;margin-bottom:-100px;" />
 			<img src="/shared/pics/chars/<?php echo char_create::getImageName($race, $sex); ?>_cloth.png" style="display:block;position:relative;left:133px; top:73px;margin-bottom:-100px;" />
-			<img id="hair_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getStartHairValue($race, $sex),"#482400"); ?>" style="display:block;position:relative;left:133px; top:73px;margin-bottom:-100px;" />
-			<img id="beard_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getStartBeardValue($race,$gender),"#482400"); ?>" style="position:relative;left:133px; top:73px;" />
+			<img id="hair_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getStartHairValue($race, $sex),"482400"); ?>" style="display:block;position:relative;left:133px; top:73px;margin-bottom:-100px;" />
+			<img id="beard_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getStartBeardValue($race,$gender),"482400"); ?>" style="position:relative;left:133px; top:73px;" />
 			</div>
 
 			<div style="height:250px;padding-left:320px;padding-right:20px;">
