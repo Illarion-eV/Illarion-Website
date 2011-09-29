c6ddb4a27dd7eaa3ab3b68b6c92e9a3453e32f35
kadiya <kadiya@illarion.org>
2011-09-29 21:50:27 +0200

    moep

--- community/account/new/de_newchar_2.php
+++ community/account/new/de_newchar_2.php
@@ -144,7 +144,7 @@
 			<?php $start_skin_color  = $skincolor[mt_rand(0,41)]; ?>
 			<?php $start_hair_color  = $haircolor[mt_rand(0,41)]; ?>
 
-			<p><?php echo "1:".$start_hair_color."-".$start_skin_color; ?><p>
+			<p><?php echo "1:".$start_hair_color."-".$start_skin_color; ?></p>
 			<div style="background-image: url(<?php echo $url; ?>/shared/pics/char_screen.jpg);float:left;border:2px groove #000;width:300px;height:250px;">
 			<div id="ajax_works" style='display:block;position:relative;left:5px;top:5px;width:32px;height:32px;margin-bottom:-32px;'></div>
 			<img id="char_image" src="<?php echo char_create::getConvertedImageUrl(char_create::getImageName($race, $sex),$start_skin_color); ?>" style="position:relative;left:133px; top:73px;display:block;margin-bottom:-100px;" />
