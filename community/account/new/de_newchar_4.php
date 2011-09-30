cfd88588320ce546e65cad4a31f508d7b9bffff0
kadiya <kadiya@illarion.org>
2011-09-30 23:53:21 +0200

    moep

---
+++ community/account/new/de_newchar_4.php
@@ -0,0 +1,15 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
+
+IllaUser::requireLogin();
+
+Page::Init();
+
+?>
+
+<h1>Neuen Charakter erstellen</h1>
+
+<h2>Schritt 4</h2>
+<p>Hier wählst Du die Startausrüstung und die Anfangsfähigkeiten Deines Charakters. Im Laufe des Spiel wirst Du neue Fähigkeiten lernen und
+			neue Ausrüstung bekommen. Die Wahl dieser Startausrüstung schränkt den weiteren Verlauf des Spiels nicht ein.
+			Wähle also einfach das Paket, dass Dir für den Anfang am meisten zusagt.</p>
