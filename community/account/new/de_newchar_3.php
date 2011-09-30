5962c8a64dbde560c08d410ebf1a517642bd921f
kadiya <kadiya@illarion.org>
2011-09-30 23:45:53 +0200

    moep

--- community/account/new/de_newchar_3.php
+++ community/account/new/de_newchar_3.php
@@ -5,6 +5,76 @@ IllaUser::requireLogin();
 
 Page::Init();
 
+includeWrapper::includeOnce( Page::getRootPath().'/community/account/inc_editinfos.php' );
+
+$server = ( isset( $_GET['server'] ) && $_GET['server'] == '1' ? 'testserver' : 'illarionserver');
+$charid = ( isset( $_GET['charid'] )  && is_numeric($_GET['charid']) ? (int)$_GET['charid'] : false );
+if (!$charid)
+{
+	exit('Fehler - Charakter ID wurde nicht richtig übergeben.');
+}
+
+$pgSQL =& Database::getPostgreSQL( $server );
+
+$query = 'SELECT chr_race, chr_sex'
+	.PHP_EOL.' FROM chars'
+	.PHP_EOL.' WHERE chr_playerid = '.$pgSQL->Quote( $charid )
+	.PHP_EOL.' AND chr_accid = '.$pgSQL->Quote( IllaUser::$ID )
+	;
+$pgSQL->setQuery( $query );
+list( $race, $sex ) = $pgSQL->loadRow();
+
+if ($race === null || $race === false)
+{
+	Messages::add( 'Charakter wurde nicht gefunden.', 'error' );
+	includeWrapper::includeOnce( Page::getRootPath().'/community/account/de_charlist.php' );
+	exit();
+}
+
+$query = 'SELECT COUNT(*)'
+.PHP_EOL.' FROM player'
+.PHP_EOL.'WHERE ply_playerid = '.$pgSQL->Quote( $charid )
+.PHP_EOL.' AND ply_strength != 0'
+;
+$pgSQL->setQuery( $query );
+if ($pgSQL->loadResult())
+{
+	exit('Fehler - Werte wurden bereits gesetzt');
+}
+
+$account =& Database::getPostgreSQL( 'accounts' );
+
+$query = 'SELECT *'
+.PHP_EOL.' FROM raceattr'
+.PHP_EOL.' WHERE id IN ( -1, '.$account->Quote( $race ).' )'
+.PHP_EOL.' ORDER BY id DESC'
+;
+$account->setQuery( $query, 0, 1 );
+$limits = $account->loadAssocRow();
+
+$limits['curr_agility'] = $limits['minagility'];
+$limits['curr_strength'] = $limits['minstrength'];
+$limits['curr_constitution'] = $limits['minconstitution'];
+$limits['curr_dexterity'] = $limits['mindexterity'];
+$limits['curr_perception'] = $limits['minperception'];
+$limits['curr_willpower'] = $limits['minwillpower'];
+$limits['curr_intelligence'] = $limits['minintelligence'];
+$limits['curr_essence'] = $limits['minessence'];
+$limits['curr_remaining'] = $limits['maxattribs'] - ( $limits['curr_agility']+$limits['curr_strength']+$limits['curr_constitution']+$limits['curr_dexterity']+$limits['curr_perception']+$limits['curr_willpower']+$limits['curr_intelligence']+$limits['curr_essence'] );
+$limits['minremaining'] = 0;
+$limits['maxremaining'] = $limits['maxattribs'];
+
+calculateLimits( &$limits );
+$limit_text = generateLimitTexts( $limits );
+
+$mySQL =& Database::getMySQL();
+$query = 'SELECT `name_de` AS `name`, `str`, `agi`, `dex`, `con`, `int`, `per`, `wil`, `ess`'
+.PHP_EOL.' FROM `homepage_attribtemp`'
+.PHP_EOL.' ORDER BY `id`'
+;
+$mySQL->setQuery( $query );
+$templates = $mySQL->loadAssocList();
+
 ?>
 
 <h1>Neuen Charakter erstellen</h1>
