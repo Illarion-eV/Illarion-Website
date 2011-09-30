d800a67ffd2915b9155798e4da7e1fda3a69408b
kadiya <kadiya@illarion.org>
2011-09-30 22:43:59 +0200

    moep

--- community/account/new/de_newchar_1.php
+++ community/account/new/de_newchar_1.php
@@ -1,31 +1,40 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
 
-IllaUser::requireLogin();
+	IllaUser::requireLogin();
 
-Page::Init();
+	Page::Init();
 
-   // Maximale Anzahl an Charakteren ist erreicht
-   $pgSQL =& Database::getPostgreSQL( 'illarionserver' );
-   $query = 'SELECT COUNT(*)'
-   .PHP_EOL.' FROM chars'
-   .PHP_EOL.' WHERE chr_accid = '.$pgSQL->Quote( IllaUser::$ID )
-   ;
-   $pgSQL->setQuery( $query );
-   $charcount = $pgSQL->loadResult();
+	$pgSQL =& Database::getPostgreSQL( 'illarionserver' );
+	$query = 'SELECT COUNT(*)'
+	.PHP_EOL.' FROM chars'
+	.PHP_EOL.' WHERE chr_accid = '.$pgSQL->Quote( IllaUser::$ID )
+	;
+	$pgSQL->setQuery( $query );
+	$charcount = $pgSQL->loadResult();
 
-   if ($charcount >= IllaUser::$charlimit && !IllaUser::auth('testserver') && IllaUser::$charlimit > 0)
-   {
-   Messages::add( 'Charakterlimit von '.IllaUser::$charlimit.' wurde erreicht.', 'error' );
-   includeWrapper::includeOnce( Page::getRootPath().'/community/account/de_charlist.php' );
-   exit();
-   }
+	$servers = array();
+	if ($charcount < IllaUser::$charlimit || IllaUser::$charlimit == 0)
+	{
+		$servers[] = 'rs';
+	}
+
+	if (IllaUser::auth('testserver'))
+	{
+		$servers[] = 'ts';
+	}
+
+	if (!count($servers))
+	{
+		exit('Fehler - Charakterlimit erreicht.');
+	}
 
    Page::setTitle( array( 'Account', 'Neuen Charakter erstellen' ) );
    Page::setDescription( 'Auf dieser Seite kannst Du einen neuen Charakter für Illarion erstellen' );
    Page::setKeywords( array( 'Charaktere', 'Neu', 'erstellen' ) );
 
-   Page::addJavaScript( array( 'prototype', 'effects', 'newchar_1'));
+   Page::addJavaScript( array( 'prototype', 'effects'));
+   Page::addJavaScript( 'newchar_1');
 
    Page::setXHTML();
 
@@ -35,9 +44,8 @@ Page::Init();
 
 <h2>Schritt 1</h2>
 
-<p>Hier musst Du Name, Rasse und Geschlecht des Charakters festlegen. Bitte beachte dazu die
-<a href="<?php echo Page::getURL(); ?>/illarion/de_name_rules.php">Namensregeln</a>
-von Illarion. Hilfreich kann auch die <a href="<?php echo Page::getURL(); ?>/general/de_rpg_guide.php">RPG-Anleitung</a> sein.</p>
+<p>Hier musst Du Name, Rasse und Geschlecht des Charakters festlegen.
+Hilfreich kann dabei die <a href="<?php echo Page::getURL(); ?>/general/de_rpg_guide.php">RPG-Anleitung</a> sein.</p>
 
 <form method="post" name="char_form" id="char_form" action="<?php echo Page::getURL(); ?>/community/account/new/de_newchar_2.php">
 		<table style="width:100%;">
