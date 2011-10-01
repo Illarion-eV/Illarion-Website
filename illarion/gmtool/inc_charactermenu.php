bf972c88c8a6dffd72e40c55a67b493b8196726b
kadiya <kadiya@illarion.org>
2011-10-01 22:11:19 +0200

    moep

--- illarion/gmtool/inc_charactermenu.php
+++ illarion/gmtool/inc_charactermenu.php
@@ -8,7 +8,8 @@
 		$entries[1] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character.php?id='.$charid, 'name'=>( $language == 'de' ? 'Informationen' : 'Informations' ) );
 		$entries[2] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_attribs.php?id='.$charid, 'name'=>( $language == 'de' ? 'Attribute' : 'Attributs' ) );
 		$entries[3] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_status.php?id='.$charid, 'name'=>( $language == 'de' ? 'Status' : 'State' ) );
-		$entries[4] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_notes.php?id='.$charid, 'name'=>( $language == 'de' ? 'Notizen' : 'Notes' ) );
+		$entries[4] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_skills.php?id='.$charid, 'name'=>( $language == 'de' ? 'Skills' : 'Skills' ) );
+		$entries[4]['subentry'][1] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_skills.php?id='.$charid.'&sub=fight', 'name'=>( $language == 'de' ? 'Kampfskills' : 'fighting skills' ));
 ?>
 <div class="menu">
 	<ul class="menu_top">
@@ -17,6 +18,13 @@
 				<a <?php echo ($active==$key ? ' class="selected"' : 'href="'.$entry['link'].'"'); ?>>
 					<?php echo $entry['name']; ?>
 				</a>
+				<?php foreach ($entries[$key]['subentry'] as $key => $subentry): ?>
+					<li>
+						<a 'href="'.$subentry['link'].'"'); ?>>
+						<?php echo $subentry['name']; ?>
+						</a>
+					</li>
+				<?php endforeach; ?>
 			</li>
 		<?php endforeach; ?>
 		<li class="end" />
