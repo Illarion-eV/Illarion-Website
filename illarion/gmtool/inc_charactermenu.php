7e38cf655bc963f30da30939f8845123e05209c3
kadiya <kadiya@illarion.org>
2011-10-01 22:17:08 +0200

    moep

--- illarion/gmtool/inc_charactermenu.php
+++ illarion/gmtool/inc_charactermenu.php
@@ -10,27 +10,9 @@
 		$entries[3] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_status.php?id='.$charid, 'name'=>( $language == 'de' ? 'Status' : 'State' ) );
 		$entries[4] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_skills.php?id='.$charid, 'name'=>( $language == 'de' ? 'Skills' : 'Skills' ) );
 		$entries[4]['subentries'][1] = array( 'link'=>$url.'/illarion/gmtool/'.$language.'_character_skills.php?id='.$charid.'&sub=fight', 'name'=>( $language == 'de' ? 'Kampfskills' : 'fighting skills' ));
+
+	print_r($entries);
 ?>
-<div class="menu">
-	<ul class="menu_top">
-		<?php foreach ($entries as $key=>$entry): ?>
-			<li<?php echo ($active==$key ? ' class="selected"' : ''); ?>>
-				<a <?php echo ($active==$key ? ' class="selected"' : 'href="'.$entry['link'].'"'); ?>>
-					<?php echo $entry['name']; ?>
-				</a>
-				<?php if (isset($entries[$key]['subentries']) ) : ?>
-				<?php foreach ($entries[$key]['subentries'] as $key => $subentry): ?>
-					<li>
-						<a href="'.$subentry['link'].'">
-						<?php echo $subentry['name']; ?>
-						</a>
-					</li>
-				<?php endforeach; ?>
-				<?php endif ?>
-			</li>
-		<?php endforeach; ?>
-		<li class="end" />
-	</ul>
-</div>
+
 
 <?php } ?>
