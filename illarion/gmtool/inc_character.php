edbcf7a6efd6956fe97261f6c79a5953c02df412
kadiya <kadiya@illarion.org>
2011-10-02 00:18:16 +0200

    moep

--- illarion/gmtool/inc_character.php
+++ illarion/gmtool/inc_character.php
@@ -4,8 +4,9 @@
 		$pgSQL =& Database::getPostgreSQL();
 
 		$query = "SELECT * FROM `".$server."`.`chars` WHERE `chr_playerid` = ".$charid;
-
+		echo "<div>";
 		echo $query;
+		echo "</div>";
 		$char_data = array("charname" => "test", "id" => 666, 1 => "moep", 2 => "blala");
 
 		return $char_data;
