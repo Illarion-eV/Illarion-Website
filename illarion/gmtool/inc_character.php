905f043b8de67682c1cc05353b667956e1bad079
kadiya <kadiya@illarion.org>
2011-10-02 14:34:11 +0200

    moep

--- illarion/gmtool/inc_character.php
+++ illarion/gmtool/inc_character.php
@@ -3,7 +3,28 @@
 	{
 		$pgSQL =& Database::getPostgreSQL();
 
-		$query = "SELECT * FROM ".$server.".chars, ".$server.".player, accounts.account WHERE chr_playerid = ".$pgSQL->Quote( $charid)." AND acc_id = chr_accid AND ply_playerid = chr_playerid";
+		$query = "SELECT "
+					.PHP_EOL."chars.chr_accid,"
+					.PHP_EOL."chars.chr_playerid,"
+					.PHP_EOL."chars.chr_status,"
+					.PHP_EOL."chars.chr_lastsavetime,"
+					.PHP_EOL."chars.chr_lastip,"
+					.PHP_EOL."chars.chr_name,"
+					.PHP_EOL."chars.chr_prefix,"
+					.PHP_EOL."chars.chr_suffix,"
+					.PHP_EOL."chars.chr_race,"
+					.PHP_EOL."chars.chr_sex,"
+					.PHP_EOL."accounts.acc_name,"
+					.PHP_EOL."accounts.acc_email,"
+					.PHP_EOL."player.ply_hitpoints,"
+					.PHP_EOL."player.ply_mana,"
+					.PHP_EOL."player.ply_posx,"
+					.PHP_EOL."player.ply_posy,"
+					.PHP_EOL."player.ply_posz "
+					.PHP_EOL."FROM ".$server.".chars, ".$server.".player, accounts.account "
+					.PHP_EOL."WHERE chr_playerid = ".$pgSQL->Quote( $charid)." "
+					.PHP_EOL."AND acc_id = chr_accid "
+					.PHP_EOL."AND ply_playerid = chr_playerid";
 		$pgSQL->setQuery( $query );
 		$char_data = $pgSQL->loadAssocRow();
 
