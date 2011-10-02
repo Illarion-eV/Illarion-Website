7813c4a1bc560f5ee4ee79153ad3b164ffb07eff
kadiya <kadiya@illarion.org>
2011-10-02 21:20:59 +0200

    moep

--- illarion/gmtool/act_character.php
+++ illarion/gmtool/act_character.php
@@ -16,13 +16,13 @@
 		exit();
 	}
 
-	$newdata['name'] 	= ( strlen($_POST['name']) > 0 ? (string)$_POST['name'] : null );
+	$newdata['name'] 		= ( strlen($_POST['name']) > 0 ? (string)$_POST['name'] : null );
 	$newdata['prefix']   	= ( strlen($_POST['prefix']) > 0 ? (string)$_POST['prefix'] : false );
 	$newdata['suffix']  	= ( strlen($_POST['suffix']) > 0 ? (string)$_POST['suffix'] : false );
 	$newdata['race'] 		= (array_key_exists ($_POST['race'], getRaceArray()) ? (int)$_POST['race'] : false  );
-	$newdata['gender']	= ( $_POST['sex'] == 0 || $_POST['sex'] == 1 ? $_POST['sex'] : false );
-	$newdata['hitpoints']	= ( $_POST['hitpoints'] < 10000 && $_POST['hitpoints'] >= 0 ? (int)$_POST['hitpoints'] : false );
-	$newdata['mana']		= ( $_POST['mana'] < 10000 && $_POST['mana'] >= 0 ? (int)$_POST['mana'] : false );
+	$newdata['gender']		= ( $_POST['sex'] == 0 || $_POST['sex'] == 1 ? $_POST['sex'] : false );
+	$newdata['hitpoints']	= ( (int)$_POST['hitpoints'] < 10000 && (int)$_POST['hitpoints'] >= 0 ? (int)$_POST['hitpoints'] : false );
+	$newdata['mana']		= ( (int)$_POST['mana'] < 10000 && (int)$_POST['mana'] >= 0 ? (int)$_POST['mana'] : false );
 	$newdata['posx']		= (strlen($_POST['posx']) > 0   ? (int)$_POST['posx'] : false);
 	$newdata['posy']		= (strlen($_POST['posy']) > 0 ? (int)$_POST['posy'] : false);
 	$newdata['posz']		= (strlen($_POST['posz']) > 0 ? (int)$_POST['posz'] : false);
