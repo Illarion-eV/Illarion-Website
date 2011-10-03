151af5c8b5455b30b9067fae919394d5ffb3ba02
kadiya <kadiya@illarion.org>
2011-10-03 14:49:20 +0200

    moep

--- illarion/gmtool/inc_character_settings.php
+++ illarion/gmtool/inc_character_settings.php
@@ -19,6 +19,33 @@ function getCharData( $charid, $server )
 		return false;
 	}
 
+	$chardata['ply_dob'] = IllaDateTime::IllaDatestampToDate( $chardata['ply_dob'] );
+	$current_time = IllaDateTime::IllaTimestampToTime( 'array' );
+
+	$chardata[ 'ply_dob' ][ 'age' ] = ( $current_time['year'] - $chardata[ 'ply_dob' ][ 'year' ] );
+	if ( $chardata[ 'ply_dob' ][ 'month' ] > $current_time['month'] || ( $chardata[ 'ply_dob' ][ 'month' ] == $current_time['month'] && $chardata[ 'ply_dob' ][ 'day' ] > $current_time['day'] ) )
+	{
+		$chardata[ 'ply_dob' ][ 'age' ]--;
+	}
+
+	if (IllaUser::usesMeter())
+	{
+		$chardata['ply_body_height'] = round($chardata['ply_body_height']*0.0254,2) .'m';
+	}
+	else
+	{
+		$chardata['ply_body_height'] = floor($chardata['ply_body_height']/12).'ft '.($chardata['ply_body_height']%12).'inch';
+	}
+
+	if (IllaUser::usesGram())
+	{
+		$chardata['ply_weight'] = ($chardata['ply_weight']/100) .' kg';
+	}
+	else
+	{
+		$chardata['ply_weight'] = (round($chardata['ply_weight']*0.02204623,2))." lb";
+	}
+
 
 	return $char_data;
 }
