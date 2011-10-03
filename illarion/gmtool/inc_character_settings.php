a9123d84225ed9db0adbe9706caeec63619452e0
kadiya <kadiya@illarion.org>
2011-10-03 14:50:15 +0200

    moep

--- illarion/gmtool/inc_character_settings.php
+++ illarion/gmtool/inc_character_settings.php
@@ -19,31 +19,31 @@ function getCharData( $charid, $server )
 		return false;
 	}
 
-	$chardata['ply_dob'] = IllaDateTime::IllaDatestampToDate( $chardata['ply_dob'] );
+	$char_data['ply_dob'] = IllaDateTime::IllaDatestampToDate( $char_data['ply_dob'] );
 	$current_time = IllaDateTime::IllaTimestampToTime( 'array' );
 
-	$chardata[ 'ply_dob' ][ 'age' ] = ( $current_time['year'] - $chardata[ 'ply_dob' ][ 'year' ] );
-	if ( $chardata[ 'ply_dob' ][ 'month' ] > $current_time['month'] || ( $chardata[ 'ply_dob' ][ 'month' ] == $current_time['month'] && $chardata[ 'ply_dob' ][ 'day' ] > $current_time['day'] ) )
+	$char_data[ 'ply_dob' ][ 'age' ] = ( $current_time['year'] - $char_data[ 'ply_dob' ][ 'year' ] );
+	if ( $char_data[ 'ply_dob' ][ 'month' ] > $current_time['month'] || ( $char_data[ 'ply_dob' ][ 'month' ] == $current_time['month'] && $char_data[ 'ply_dob' ][ 'day' ] > $current_time['day'] ) )
 	{
-		$chardata[ 'ply_dob' ][ 'age' ]--;
+		$char_data[ 'ply_dob' ][ 'age' ]--;
 	}
 
 	if (IllaUser::usesMeter())
 	{
-		$chardata['ply_body_height'] = round($chardata['ply_body_height']*0.0254,2) .'m';
+		$char_data['ply_body_height'] = round($char_data['ply_body_height']*0.0254,2) .'m';
 	}
 	else
 	{
-		$chardata['ply_body_height'] = floor($chardata['ply_body_height']/12).'ft '.($chardata['ply_body_height']%12).'inch';
+		$char_data['ply_body_height'] = floor($char_data['ply_body_height']/12).'ft '.($char_data['ply_body_height']%12).'inch';
 	}
 
 	if (IllaUser::usesGram())
 	{
-		$chardata['ply_weight'] = ($chardata['ply_weight']/100) .' kg';
+		$char_data['ply_weight'] = ($char_data['ply_weight']/100) .' kg';
 	}
 	else
 	{
-		$chardata['ply_weight'] = (round($chardata['ply_weight']*0.02204623,2))." lb";
+		$char_data['ply_weight'] = (round($char_data['ply_weight']*0.02204623,2))." lb";
 	}
 
 
