032071f06a530cc02138d0f1361921700ec00207
vilarion <vilarion@illarion.org>
2011-10-27 15:05:35 +0200

    use bold bullet points

--- development/de_progress.php
+++ development/de_progress.php
@@ -13,36 +13,36 @@
 
 <p>
     Legende:
-    <span style="color:#FF4500">Geplant</span>,
-    <span style="color:#D2691E">In Arbeit</span>,
-    <span style="color:#FFA500">Testen</span>,
-    <span style="color:green">Fertiggestellt</span>
+    <span style="font-weight:bold; color:#FF4500">Geplant</span>,
+    <span style="font-weight:bold; color:#D2691E">In Arbeit</span>,
+    <span style="font-weight:bold; color:#FFA500">Testen</span>,
+    <span style="font-weight:bold; color:green">Fertiggestellt</span>
 </p>
 
 <h2>Inhalt</h2>
 
 <h3>Systeme</h3>
 <ul>
-    <li><span style="color:#FFA500">Handwerk</span></li>
-    <li><span style="color:#FF4500">Rohstoffe sammeln</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
-    <li><span style="color:#D2691E">Kampf</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
-    <li><span style="color:#FFA500">Arkane Magie</span></li>
-    <li><span style="color:#D2691E">Druiden</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5791">Jupiter</a></li>
-    <li><span style="color:#FF4500">Fraktionen</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5398">Ardian</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Handwerk</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Rohstoffe sammeln</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Kampf</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Arkane Magie</span></li>
+    <li><span style="font-weight:bold; color:#D2691E">Druiden</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5791">Jupiter</a></li>
+    <li><span style="font-weight:bold; color:#FF4500">Fraktionen</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5398">Ardian</a></li>
 </ul>
 
 <h3>NPCs</h3>
 <ul>
-    <li><span style="color:#FF4500">Händler</span></li>
-    <li><span style="color:#FFA500">Monster</span></li>
-    <li><span style="color:#FFA500">Wachen</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
-    <li><span style="color:#FFA500">Maultiere</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Händler</span></li>
+    <li><span style="font-weight:bold; color:#FFA500">Monster</span></li>
+    <li><span style="font-weight:bold; color:#FFA500">Wachen</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Maultiere</span></li>
 </ul>
 
 <h3>Sonstiges</h3>
 <ul>
-    <li><span style="color:#FF4500">Tutorial</span></li>
-    <li><span style="color:#D2691E">Magische Edelsteine</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
-    <li><span style="color:#D2691E">Bücher</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=10083">Skamato</a></li>
-    <li><span style="color:#D2691E">Quests</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Tutorial</span></li>
+    <li><span style="font-weight:bold; color:#D2691E">Magische Edelsteine</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Bücher</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=10083">Skamato</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Quests</span></li>
 </ul>
--- development/us_progress.php
+++ development/us_progress.php
@@ -13,36 +13,36 @@
 
 <p>
     Legend:
-    <span style="color:#FF4500">Planned</span>,
-    <span style="color:#D2691E">Work-In-Progress</span>,
-    <span style="color:#FFA500">Testing</span>,
-    <span style="color:green">Complete</span>
+    <span style="font-weight:bold; color:#FF4500">Planned</span>,
+    <span style="font-weight:bold; color:#D2691E">Work-In-Progress</span>,
+    <span style="font-weight:bold; color:#FFA500">Testing</span>,
+    <span style="font-weight:bold; color:green">Complete</span>
 </p>
 
 <h2>Content</h2>
 
 <h3>Systems</h3>
 <ul>
-    <li><span style="color:#FFA500">Crafting</span></li>
-    <li><span style="color:#FF4500">Gathering</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
-    <li><span style="color:#D2691E">Fighting</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
-    <li><span style="color:#FFA500">Arcane Magic</span></li>
-    <li><span style="color:#D2691E">Druids</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5791">Jupiter</a></li>
-    <li><span style="color:#FF4500">Factions</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5398">Ardian</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Crafting</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Gathering</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Fighting</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Arcane Magic</span></li>
+    <li><span style="font-weight:bold; color:#D2691E">Druids</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5791">Jupiter</a></li>
+    <li><span style="font-weight:bold; color:#FF4500">Factions</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=5398">Ardian</a></li>
 </ul>
 
 <h3>NPCs</h3>
 <ul>
-    <li><span style="color:#FF4500">Traders</span></li>
-    <li><span style="color:#FFA500">Monsters</span></li>
-    <li><span style="color:#FFA500">Guards</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
-    <li><span style="color:#FFA500">Mules</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Traders</span></li>
+    <li><span style="font-weight:bold; color:#FFA500">Monsters</span></li>
+    <li><span style="font-weight:bold; color:#FFA500">Guards</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li><span style="font-weight:bold; color:#FFA500">Mules</span></li>
 </ul>
 
 <h3>Other</h3>
 <ul>
-    <li><span style="color:#FF4500">Tutorial</span></li>
-    <li><span style="color:#D2691E">Magic Gems</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
-    <li><span style="color:#D2691E">Books</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=10083">Skamato</a></li>
-    <li><span style="color:#D2691E">Quests</span></li>
+    <li><span style="font-weight:bold; color:#FF4500">Tutorial</span></li>
+    <li><span style="font-weight:bold; color:#D2691E">Magic Gems</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=1226">martin</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Books</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=10083">Skamato</a></li>
+    <li><span style="font-weight:bold; color:#D2691E">Quests</span></li>
 </ul>
