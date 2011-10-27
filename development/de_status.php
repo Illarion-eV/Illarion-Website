8e84b8960a5d816d29c871c2591a3867c7ab32d1
vilarion <vilarion@illarion.org>
2011-10-27 12:21:11 +0200

    add project status

--- development/de_status.php
+++ development/de_status.php
@@ -14,18 +14,36 @@
 
 <p>
     Legende:
-    <span style="color:#FF0000">Geplant</span>,
-    <span style="color:#CC4400">In Arbeit</span>,
-    <span style="color:#777700">Testen</span>,
-    <span style="color:#00FF00">Fertiggestellt</span>
+    <span style="color:maroon">Geplant</span>,
+    <span style="color:purple">In Arbeit</span>,
+    <span style="color:navy">Testen</span>,
+    <span style="color:green">Fertiggestellt</span>
 </p>
 
 <h2>Inhalt</h2>
+
 <h3>Systeme</h3>
 <ul>
-    <li>Handwerk</li>
-    <li>Rohstoffe sammeln: pharse</li>
-    <li>Kampf: martin</li>
-    <li>Arkane Magie</li>
-    <li>Druiden: Jupiter</li>
+    <li><span style="color:navy">Handwerk</span></li>
+    <li><span style="color:maroon">Rohstoffe sammeln</span>: pharse</li>
+    <li><span style="color:purple">Kampf</span>: martin</li>
+    <li><span style="color:navy">Arkane Magie</span></li>
+    <li><span style="color:purple">Druiden</span>: Jupiter</li>
+    <li><span style="color:maroon">Fraktionen</span>: Ardian</li>
+</ul>
+
+<h3>NPCs</h3>
+<ul>
+    <li><span style="color:maroon">Händler</span></li>
+    <li><span style="color:navy">Monster</span></li>
+    <li><span style="color:navy">Wachen</span>: pharse</li>
+    <li><span style="color:navy">Maultiere</span></li>
+</ul>
+
+<h3>Sonstiges</h3>
+<ul>
+    <li><span style="color:maroon">Tutorial</span></li>
+    <li><span style="color:purple">Magische Edelsteine</span>: martin</li>
+    <li><span style="color:purple">Bücher</span>: Skamato</li>
+    <li><span style="color:purple">Quests</span></li>
 </ul>
