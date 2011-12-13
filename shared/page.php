d38ebb22de30a24122733e1e5fea624f8cadbe80
Martin Karing <nitram@illarion.org>
2011-12-13 19:01:35 +0100

    +1 not working... trying it in synchronous mode

--- shared/page.php
+++ shared/page.php
@@ -1336,7 +1336,7 @@ class Page {
 
 			$js = &self::$js;
 			array_unshift(&$js, 'bookmarks');
-			array_unshift(&$js, 'google_plusone');
+			//array_unshift(&$js, 'google_plusone');
 			if (!IllaUser::loggedIn()) {
 				array_unshift(&$js, 'proxy_killer');
 			}
