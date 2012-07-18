fb49b4d63a616ea6537872614678102eb6c3e128
vilarion <vilarion@illarion.org>
2012-07-18 18:07:04 +0200

    Fix some German spelling errors

--- community/account/new/de_newchar_4.php
+++ community/account/new/de_newchar_4.php
@@ -108,9 +108,9 @@ Page::addJavaScript( 'newchar_3' );
 <h1>Neuen Charakter erstellen</h1>
 
 <h2>Schritt 4</h2>
-<p>Hier wählst Du die Startausrüstung und die Anfangsfähigkeiten Deines Charakters. Im Laufe des Spiel wirst Du neue Fähigkeiten lernen und
+<p>Hier wählst Du die Startausrüstung und die Anfangsfähigkeiten Deines Charakters. Im Laufe des Spiels wirst Du neue Fähigkeiten lernen und
 			neue Ausrüstung bekommen. Die Wahl dieser Startausrüstung schränkt den weiteren Verlauf des Spiels nicht ein.
-			Wähle also einfach das Paket, dass Dir für den Anfang am meisten zusagt.</p>
+			Wähle also einfach das Paket, das Dir für den Anfang am meisten zusagt.</p>
 
 	<form action="<?php echo Page::getURL(); ?>/community/account/new/de_newchar.php?charid=<?php echo $charid,($_GET['server'] == '1' ? '&amp;server=1' : ''); ?>" method="post" name="package" id="package">
 	<h2>Startposition</h2>
