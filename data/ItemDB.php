28c816d007d032cdefc87734126d0fb46e2e265a
vilarion <vilarion@illarion.org>
2012-11-23 13:31:47 +0100

    Add script to create item list for defunct config tool to create graphical item list

---
+++ data/ItemDB.php
@@ -0,0 +1,20 @@
+<?php
+	include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
+	
+	Page::Init();
+	
+	$testserver = &Database::getPostgreSQL('testserver');
+
+	$query = 'SELECT "com_itemid", "com_weight", "itn_german", "itn_english"'
+    .PHP_EOL.' FROM "common", "itemname"'
+	;
+    $testserver->setQuery($query);
+	$common = $testserver->loadAssocList();
+
+    echo '/NOP/'
+	if (!is_null($common)) {
+	   foreach($common as $key=>$item) {
+	       echo '0,0,1,',$item['com_itemid'],',0,0,0,',$item['com_weight'],',0,0,0,"',$item['itn_german'],'","',$item['itn_english'],',0,';	
+	   }
+	}
+?>
