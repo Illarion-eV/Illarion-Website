b574375bcccbf3814c2d97353bd90e5e6c406024
Martin Karing <nitram@illarion.org>
2012-12-26 23:49:37 +0100

    First version of the new startpacks

---
+++ community/account/startpack.php
@@ -0,0 +1,75 @@
+<?php
+	error_reporting(E_ALL);
+	
+	include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
+	
+	if (!isset($_GET['startpack']) || !is_numeric($_GET['startpack']))
+	{
+		exit("No startpack selected");
+	}
+	
+	$packId = (int)$_GET['startpack'];
+	
+	if (!isset($_GET['server']) || !is_numeric($_GET['server']))
+	{
+		$server = 0;
+	}
+	else
+	{
+		$server = (int)$_GET['server'];
+	}
+	
+	if (!isset($_GET['language']) || $_GET['language'] != 'de')
+	{
+		$language = 'us';
+	}
+	else
+	{
+		$language = 'de';
+	}
+	
+	$server = ($server == 0) ? 'illarionserver' : 'testserver';
+	
+	$pgSQL =& Database::getPostgreSQL();
+	
+	$query = 'SELECT COUNT(*)'
+    .PHP_EOL.' FROM "'.$server.'"."startpacks"'
+    .PHP_EOL.' WHERE "stp_id" = '.$pgSQL->Quote($packId);
+    $pgSQL->setQuery($query);
+	
+	if ($pgSQL->getResult() == 0)
+	{
+		exit('no such package: '.$packId);
+	}
+	
+	$query = 'SELECT '.($language == 'de' ? '"skl_name_german"' : '"skl_name_english"').' AS "name"'
+    .PHP_EOL.' FROM "'.$server.'"."startpack_skills"'
+    .PHP_EOL.' INNER JOIN "'.$server.'"."skills" ON "sps_skill_id" = "skl_skill_id"'
+    .PHP_EOL.' WHERE "sps_id" = '.$pgSQL->Quote($packId)
+	.PHP_EOL.' ORDER BY "name" ASC';
+    $pgSQL->setQuery($query);
+	$skills = $pgSQL->loadResultArray();
+	
+	$query = 'SELECT "spi_item_id" AS "itemid", '.($language == 'de' ? '"itn_german"' : '"itn_english"').' AS "name"'
+    .PHP_EOL.' FROM "'.$server.'"."startpack_items"'
+    .PHP_EOL.' INNER JOIN "'.$server.'"."itemname" ON "spi_item_id" = "itn_itemid"'
+    .PHP_EOL.' WHERE "spi_id" = 0'
+    .PHP_EOL.' ORDER BY "spi_linenumber" ASC';
+    $pgSQL->setQuery($query);
+	$items = $pgSQL->loadAssocList();
+	
+	Page::setXML();
+	Page::Init();
+?>
+<pack>
+	<skills>
+		<?php foreach ($skills as $skillName): ?>
+		<skill name="<?php echo $skillName; ?>" />
+		<?php endforeach; ?>
+	</skills>
+	<items>
+		<?php foreach ($items as $item): ?>
+		<item name="<?php echo $item['name']; ?>" id="<?php echo $item['itemid']; ?>" />
+		<?php endforeach; ?>
+	</items>
+</pack>
