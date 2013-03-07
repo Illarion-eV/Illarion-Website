9be3010a9e39bfc9e553b81e091705fd9129b4ec
Raelith <haeltera@gmail.com>
2013-03-07 23:58:59 +0000

    Update to us_links

--- community/us_links.php
+++ community/us_links.php
@@ -2,91 +2,272 @@
 include $_SERVER['DOCUMENT_ROOT'].'/shared/shared.php';
 
 Page::setTitle( 'Links' );
-Page::setDescription( 'This page contains Illarion guild and fan pages.' );
-Page::setKeywords( array( 'Links', 'Guilds', 'Fan', 'Fans', 'Banner' ) );
+Page::setDescription( 'This page contains links to the various projects used by Illarion.' );
+Page::setKeywords( array( 'Links', ) );
 Page::setXHTML();
 Page::Init();
 ?>
 
-<h1>Links to Illarion related sites</h1>
+<h1>Links to the various projects that help provide Illarions foundations</h1>
 
-<h2>Content</h2>
+<h3>The Illarion developers would like to recognise and thank all of the projects that have allowed Illarion to become what it is today.
+Below are links to these projects and a small description of how it has contributed towards the game.</h3>
 
 <ul>
-	<li><a class="hidden" href="#1">Guilds / Tribes / Clans</a></li>
-	<li><a class="hidden" href="#2">Fan sites</a></li>
-	<li><a class="hidden" href="#3">External Sites</a></li>
+	<li><a class="hidden" href="#1">Debian</a></li>
+	<li><a class="hidden" href="#2">Libgdx</a></li>
+	<li><a class="hidden" href="#3">Oganalp Canatan</a></li>
+	<li><a class="hidden" href="#4">NiftyGUI</a></li>
+	<li><a class="hidden" href="#5">Java</a></li>
+	<li><a class="hidden" href="#6">Github</a></li>
+	<li><a class="hidden" href="#7">LWJGL</a></li>
+	<li><a class="hidden" href="#8">Apache Web Server</a></li>
+	<li><a class="hidden" href="#9">CAcert</a></li>
+	<li><a class="hidden" href="#10">Boost</a></li>
+	<li><a class="hidden" href="#11">Lua</a></li>
+	<li><a class="hidden" href="#12">PostgreSQL</a></li>
+	<li><a class="hidden" href="#13">Libpqxx</a></li>
+	<li><a class="hidden" href="#14">GCC</a></li>
+	<li><a class="hidden" href="#15">GPLv3</a></li>
+	<li><a class="hidden" href="#16">PHP</a></li>
+	<li><a class="hidden" href="#17">phpBB</a></li>
+	<li><a class="hidden" href="#18">Mantis Bugtracker</a></li>
 </ul>
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="1"></a></div>
 
-<h2>Guilds / Tribes / Clans</h2>
+<h4>Debian</h4>
 
-<h4>The Order of the Grey Rose</h4>
-
-<img class="float_left" src="<?php echo Page::getURL(); ?>/community/pic_greyrose.de.jpg" alt="Grey Rose" />
 <p>
-	<a rel="external" href="http://www.graurose.de">Link to the homepage of the Order of the Grey Rose (German Only)</a>
+	<a rel="external" href="http://www.debian.org/">Link to the Debian website</a>
 	<br /><br />
-	We, the knights of the Grey Rose, think highly of honour and loyalty. We live with a
-	strict code which is the base for our guild and our behaviour. We strive for peace and
-	order between all beings. We select our squires as carefully as we select our
-	knights, which are chosen from among our squires. In our stronghold (HP) you can
-	find out more about us and our goals. Every visitor who wants to look around in peace
-	is warmly welcome.
+	Illarion uses the Debian operating system for its servers.
+	Debian systems currently use the Linux kernel or the FreeBSD kernel.
 </p>
-
-<h4>Order of the Grey Light</h4>
-
-<img class="float_left" src="<?php echo Page::getURL(); ?>/community/pic_greylight-order.de.vu.jpg" alt="Grey Light" />
-<p>
-	<a rel="external" href="http://greylight.foren-city.de/forum,19,-das-graue-licht-the-grey-order.html">Link to the homepage of the Order of the Grey Light</a>
-	<br /><br />
-	The oldest organisation of illarion is the Order of the Grey Light. Goals of the
-	Order are: upbuilding and keeping law and order; keeping the balance in all things,
-	neutrality and harmony; keeping and enhancing knowledge, history and scholarship.
-	While the "Scribes" of the Order act as scholars, philosophers, tacticians, bards or
-	judges, the "Templars" of the order are fighters or fighter mages, law-keepers,
-	soldiers, judges or paladins. Templars and Scribes follow the Grey Codex. Accepted
-	as members are only those who have proven as honorable and worthy.
-</p>
-
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="2"></a></div>
 
-<h2>Fan sites</h2>
+<h4>Libgdx</h4>
 
-<h4>Moonsilver</h4>
-
-<img class="float_left" src="<?php echo Page::getURL(); ?>/community/pic_moonsilver.jpg" alt="Moonsilver" />
 <p>
-	<a rel="external" href="http://www.moonsilver.de">Link to the homepage of Moonsilver</a>
+	<a rel="external" href="http://libgdx.badlogicgames.com/">Link to the Libgdx website</a>
 	<br /><br />
-	This is a site with the background story of Illarion. Here you will find the
-	description of the gods and races, the history of the gods, information concerning the
-	calendar, descriptions of professions and much more. Take your time and have a look
-	at the content as it is helpful for the game.
+	Libgdx is a Java game development framework that provides a unified API that works across all supported platforms.
+	This allows the developers to create a game that can be played on various different operating systems without having to make drastic changes.
 </p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="3"></a></div>
 
-<h2>External Sites</h2>
+<h4>Oganalp Canatan</h4>
 
-<h4>NiftyGUI</h4>
-
-<img class="float_left" src="<?php echo Page::getURL(); ?>/community/nifty-logo.png" alt="NiftyGUI" />
 <p>
-	<a rel="external" href="http://nifty-gui.lessvoid.com/">Link to the NiftyGUI website</a>
+	<a rel="external" href="http://www.oganalp.com/">Link to the Oganalp Canatan official website</a>
 	<br /><br />
-	Nifty GUI is a Java Library that supports the building of interactive user interfaces
-	for games or similar applications. 
-	It utilizes lwjgl for OpenGL rendering.
-	The configuration of the GUI is stored in xml files with little supporting Java code.
-	In short Nifty helps you to layout stuff, display it in a cool way and interact with it
+	Oganalp Canatan wrote the main theme tune for Illarion.
+	The staff would like to thank him for his time and assistance.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="4"></a></div>
+
+<h4>Nifty GUI</h4>
+
+<p>
+	<a rel="external" href="http://nifty-gui.lessvoid.com/">Link to the Nifty GUI website</a>
+	<br /><br />
+	Nifty GUI is a Java Library that supports the building of interactive user interfaces for games or similar applications.
+	This has assisted greatly with the development of the GUI currently in use within the game.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="5"></a></div>
+
+<h4>Java</h4>
+
+<p>
+	<a rel="external" href="http://www.java.com/">Link to the Oracle Java website</a>
+	<br /><br />
+	http://www.java.com/  - Java is the global standard for developing and delivering mobile applications, 
+	games, Web-based content, and enterprise software.
+	It is used by Illarion to create the client that is capable of running on numerous operating systems.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="6"></a></div>
+
+<h4>Github</h4>
+
+<p>
+	<a rel="external" href="https://github.com/">Link to the Github website</a>
+	<br /><br />
+	Illarion uses Github to help ensure efficient collaboration between developers.
+	Github is used by over 3 million people worldwide.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="7"></a></div>
+
+<h4>LWJGL</h4>
+
+<p>
+	<a rel="external" href="http://www.lwjgl.org/">Link to the LWJGL website</a>
+	<br /><br />
+	LWJGL provides Illarions developers access to high performance cross-platform libraries such as OpenGL, OpenCL and OpenAL.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="8"></a></div>
+
+<h4>Apache Web Server (HTTPD)</h4>
+
+<p>
+	<a rel="external" href="https://httpd.apache.org/">Link to the Apache Web Server (A.K.A. HTTPD) website</a>
+	<br /><br />
+	Apache httpd has been the most popular web server on the Internet since April 1996.
+	It is used to power the Illarion website.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="9"></a></div>
+
+<h4>CAcert</h4>
+
+<p>
+	<a rel="external" href="http://www.cacert.org/">Link to the CAcert website</a>
+	<br /><br />
+	CAcert.org is a community driven, Certificate Authority that issues certificates to the public at large for free. 
+	These certificates are used on the Illarion website and also in the game client.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="10"></a></div>
+
+<h4>Boost</h4>
+
+<p>
+	<a rel="external" href="http://www.boost.org/">Link to the Boost website</a>
+	<br /><br />
+	Boost provides free peer-reviewed portable C++ source libraries.
+	These allow the developers to utilise their time more efficiently by using tried and 
+	tested libraries instead of writing everything from scratch.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="11"></a></div>
+
+<h4>Lua</h4>
+
+<p>
+	<a rel="external" href="http://www.lua.org/">Link to the Lua website</a>
+	<br /><br />
+	Lua is a powerful, fast, lightweight, embeddable scripting language.
+	It is used to create the scripts for the NPCs, Alchemy system and it will also 
+	power the Magic system
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="12"></a></div>
+
+<h4>PostgreSQL</h4>
+
+<p>
+	<a rel="external" href="http://www.postgresql.org/">Link to the PostgreSQL website</a>
+	<br /><br />
+	PostgreSQL is a powerful, open source object-relational database system.
+	It has more than 15 years of active development and a proven 
+	architecture that has earned it a strong reputation for reliability, data integrity and correctness.
+	It is used in all aspects of Illarion from the website, forums and game server.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="13"></a></div>
+
+<h4>Libpqxx</h4>
+
+<p>
+	<a rel="external" href="http://pqxx.org/">Link to the pqxx website</a>
+	<br /><br />
+	libpqxx is the official C++ client API for PostgreSQL, the enterprise-strength open-source relational database.
+	This allows the server to communicate with the database to store and retrieve data.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="14"></a></div>
+
+<h4>GCC</h4>
+
+<p>
+	<a rel="external" href="http://gcc.gnu.org/">Link to the GCC website</a>
+	<br /><br />
+	GCC is a part of the GNU Project.
+	It is used to compile the game server.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="15"></a></div>
+
+<h4>GPLv3</h4>
+
+<p>
+	<a rel="external" href="https://www.gnu.org/licenses/gpl.html">Link to the GPLv3 website</a>
+	<br /><br />
+	The GNU General Public License is a free, copyleft license for software and other kinds of works.
+	A quick guide to GPLv3 can be found at https://www.gnu.org/licenses/quick-guide-gplv3.html.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="16"></a></div>
+
+<h4>PHP</h4>
+
+<p>
+	<a rel="external" href="http://www.php.net/">Link to the PHP website</a>
+	<br /><br />
+	PHP is an HTML-embedded scripting language.
+	It is used extensively in the creation and maintenance of the Illarion website.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="17"></a></div>
+
+<h4>phpBB</h4>
+
+<p>
+	<a rel="external" href="https://www.phpbb.com/">Link to the phpBB website</a>
+	<br /><br />
+	phpBB is free and open source forum software that is easy to use, powerful, and highly customisable.
+	It is used to power the Illarion forums.
+</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<div><a id="18"></a></div>
+
+<h4>Mantis Bug Tracker</h4>
+
+<p>
+	<a rel="external" href="http://www.mantisbt.org/">Link to the Mantis Bug Tracker website</a>
+	<br /><br />
+	MantisBT is a popular free web-based bug tracking system. It is used by Illarion as a tool to report bugs,
+	suggest features and assign workloads to developers.
 </p>
 
 <?php Page::insert_go_to_top_link(); ?>
