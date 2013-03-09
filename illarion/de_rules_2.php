da7e49417c9d96ecbbbdd97a6874f325b066a91d
Raelith <haeltera@gmail.com>
2013-03-09 13:13:29 +0000

    Update to DE rules

--- illarion/de_rules_2.php
+++ illarion/de_rules_2.php
@@ -34,10 +34,9 @@ sind.</p>
     <li><a href="#emotes">Emotes</a></li>
     <li><a href="#pkilling">Player killing</a></li>
     <li><a href="#harm">Gewalt</a></li>
-    <li><a href="#behaivor">Verhalten</a></li>
+    <li><a href="#behavior">Verhalten</a></li>
     <li><a href="#rulebreak">Regelverstöße</a></li>
     <li><a href="#bugs">Spielfehler</a></li>
-    <li><a href="#attribs">Attribute</a></li>
     <li><a href="#gms">Gamemaster</a></li>
     <li><a href="#boards">Foren</a></li>
     <li><a href="#macros">Zusatzprogramme</a></li>
@@ -51,13 +50,12 @@ sind.</p>
 <div><a id="account"></a></div>
 <h2>Account</h2>
 
-<p>Jedem Spieler steht nur ein Account zu. Accounts und Charaktere werden nicht
-an andere Spieler verliehen, getauscht oder verkauft. Der Besitzer eines
-Accounts ist verantwortlich für alle Aktionen seiner Charaktere, auch wenn sich
-ein Dritter Zugriff auf den Account verschafft hat.</p>
+<p>Jedem Spieler steht nur ein Account zu. Accounts und Charaktere werden nicht an andere Spieler verliehen, 
+getauscht oder verkauft. Der Besitzer eines Accounts ist verantwortlich für alle Aktionen seiner Charaktere, 
+auch wenn sich ein Dritter Zugriff auf den Account verschafft hat</p>
 
-<p>Wenn zwei Spieler über dieselbe Internetverbindung ins Spiel einloggen
-wollen, ist dies beim Team von Illarion anzumelden.</p>
+<p>Wenn zwei Spieler über dieselbe Internetverbindung ins Spiel einloggen wollen, 
+ist dies beim Team von Illarion anzumelden.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
@@ -80,44 +78,18 @@ zulässig:</p>
   Spiel separat eingefügt werden</li>
 </ul>
 
-<p>Zusätzlich gelten folgende Richtlinien:</p>
-
-<ul>
-    <li>Ein Name muss zumindest einen Vornamen haben und kann einen Nachnamen
-    haben, was auch sehr empfohlen wird.</li>
-    <li>Der Name muss aussprechbar sein, also Vokale enthalten</li>
-    <li>Echsen dürfen unaussprechliche Namen haben, sollen sich aber nach
-    Echsen-Sprache anhören (Entscheidung der Gamemaster)</li>
-    <li>Namenszusätze wie "el", "al" etc. (z.B. el'Chitarim al Kazar) können mit
-    einem kleinen Buchstaben beginnen</li>
-</ul>
-
-<p>Bei Sippennamen und Adelsgeschlechtern gilt des Weiteren: Wer zuerst kommt,
-mahlt zuerst. Wenn ein weiterer Spieler einen Charakter mit gleichem Nachnamen
-erstellen will, benötigt er die Erlaubnis des Spielers, der diesen Namen zuerst
-verwendet hat.</p>
-
-
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="world"></a></div>
 <h2>Die Spielwelt</h2>
 
-<p>Bei der Auswahl der Rassen eines Charakters ist die Auswahl auf die bei der
-Charaktererstellung angegebenen Rassen begrenzt. Auch über das Rollenspiel ist
-es nicht erlaubt, Mischlingscharaktere irgendeiner Art zu spielen. Die einzige
-tolerierte Ausnahme bilden Halbelfen (Elf/Mensch), die allerdings von ihren
-Eigenschaften (Sprache, Attribute) auf eine der Elternrassen begrenzt sind.</p>
+<p>Die Beschreibung der Spielwelt, wie auf http://www.illarion.org veröffentlicht, ist verpflichtend.
+Bei der Auswahl der Rassen eines Charakters ist die Auswahl auf die bei der Charaktererstellung angegebenen Rassen begrenzt.
+Auch über das Rollenspiel ist es nicht erlaubt, Mischlingscharaktere irgendeiner Art zu spielen.</p>
 
 <p>Bei magischem und göttlichem Wirken oder übernatürlichen Eigenschaften gilt,
-dass alles, was man nicht über die Engine von Illarion umsetzen kann, eine
-Genehmigung eines Gamemasters erfordert. Das gilt insbesondere für magische und
-göttliche Rituale, die einen Effekt erzielen sollen, sowie für alle Arten von
-Artefaktmagie, solange diese nicht über die Engine ihren Effekt erhält.</p>
-
-<p>Die Durchführung von effektbehafteten Ritualen muss immer von einem
-Gamemaster überwacht werden, in dessen Ermessen es liegt, ob das Ritual Erfolg
-hat und welche Effekte im Detail eintreten.</p>
+dass alles, was man nicht über die Engine von Illarion umsetzen kann,
+eine Genehmigung eines Gamemasters erfordert. </p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
@@ -133,61 +105,55 @@ gestattet, NPCs im Rollenspiel zu ignorieren. So wird kein Verbrecher einen
 anderen Charakter ausrauben, wenn dieser direkt neben einer NPC-Stadtwache
 steht.</p>
 
-<p>Handlungen eines Charakters sind nicht den technischen Bedingungen der Engine
-anzupassen, um maximalen Erfolg zu erzielen. Charaktere müssen stets auf äußere
-Einflüsse reagieren und dürfen nicht unbeaufsichtigt im Spiel belassen werden.
-Sich unliebsamen Situationen, die nachteilige Folgen für den Charakter haben
-könnten, durch Ausloggen zu entziehen, ist verboten.</p>
+<p>Unabhängig von denen durch die Engine vorgegebenen Begrenzungen und Möglichkeiten 
+ist das Verhalten eines Charakters an die Spielwelt anzupassen. 
+Schwerter sind tödliche Waffen, Oger bleiben furchteinflößende Wesen und Ackerbau harte Knochenarbeit, 
+auch wenn ein Spieler mit der Engine Herausforderungen leicht meistern kann. 
+Handlungen eines Charakters sind nicht den technischen Bedingungen der Engine anzupassen, 
+um maximalen Erfolg zu erzielen. </p>
 
-<p>Wenn ein Charakter niedergeschlagen oder 'getötet' wird, trägt er
-Verletzungen davon, die auszuspielen sind. Im Falle des Niederschlagens sind das
-leichte Verletzungen, im Falle des Todes sehr schwere. Diese Wunden sind solange
-auszuspielen, bis die Lebenspunkte des Charakters wieder hergestellt sind.</p>
+<p>Charaktere müssen stets auf äußere Einflüsse reagieren und dürfen nicht unbeaufsichtigt im Spiel belassen werden.
+Sich unliebsamen Situationen, die nachteilige Folgen für den Charakter haben könnten,
+durch Ausloggen zu entziehen, ist verboten.</p>
+
+<p>Wenn ein Charakter niedergeschlagen oder 'getötet' wird, trägt er Verletzungen davon, die auszuspielen sind.
+Im Falle des Niederschlagens sind das leichte Verletzungen, im Falle des Todes sehr schwere.
+Diese Wunden sind solange auszuspielen, bis die Lebenspunkte des Charakters wieder hergestellt sind.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="charmixing"></a></div>
 <h2>Charaktere vermischen</h2>
 
-<p>Es ist generell nicht erlaubt, dass sich zwei Charaktere von einem Spieler
-gegenseitig unterstützen. Daher dürfen zwischen diesen Charakteren keine
-Gegenstände, auch nicht über Mittelsleute, ausgetauscht werden und die
-Charaktere dürfen kein Wissen teilen. Außerdem können sich zwei Charaktere eines
-Spielers nicht gegenseitig kennen oder verwandt sein. Es ist niemals gestattet,
-sich mit zwei Charakteren gleichzeitig einzuloggen.</p>
-
-<p>Es ist nicht gestattet, dass zwei Charaktere eines Spielers Mitglied in
-derselben Gilde sind. Zwei Charaktere eines Spielers dürfen, um
-Interessenkonflikten vorzubeugen, keinen Gruppen angehören, die sich gegenseitig
-behindern oder direkt beeinflussen.</p>
+<p>Es ist generell nicht erlaubt, dass sich zwei Charaktere von einem Spieler direkt unterstützen oder bekämpfen.
+Daher dürfen zwischen diesen Charakteren keine Gegenstände, auch nicht über Mittelsleute,
+ausgetauscht werden und die Charaktere dürfen kein Wissen teilen.
+Außerdem können zwei Charaktere eines Spielers nicht verwandt oder eng verbunden sein.
+Es ist niemals gestattet, sich mit zwei Charakteren gleichzeitig einzuloggen.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="ooc"></a></div>
 <h2>Out of character (OOC)</h2>
 
-<p>Out of character ist generell alles, was nicht dem Handeln oder den Gedanken
-des Charakters im Spiel entspringt. Aktionen im Spiel dürfen niemals durch
-OOC-Einflüsse begründet sein, zum Beispiel dadurch, dass man einen anderen
-Spieler nicht leiden kann. Das Herbeirufen von anderen Charakteren durch im
-Spiel nicht vorhandene Möglichkeiten (z.B. Messenger) ist strengstens verboten.
-Außerdem ist die Verwendung von Wissen untersagt, das der Spieler, jedoch nicht
-der Charakter, erlangt hat. Der Spieler ist in der Pflicht nachzuweisen, dass
-sein Charakter das Wissen im Spiel erworben hat.</p>
+<p>Out of character ist generell alles, was nicht dem Handeln oder den Gedanken des Charakters im Spiel entspringt.
+Aktionen im Spiel dürfen niemals durch OOC-Einflüsse begründet sein, zum Beispiel dadurch, 
+dass man einen anderen Spieler nicht leiden kann. 
+Das Herbeirufen von anderen Charakteren durch im Spiel nicht vorhandene Möglichkeiten (z.B. Messenger) ist strengstens verboten. 
+Außerdem ist die Verwendung von Wissen untersagt, das der Spieler, jedoch nicht der Charakter, erlangt hat. 
+Der Spieler ist in der Pflicht nachzuweisen, dass sein Charakter das Wissen im Spiel erworben hat.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
 <div><a id="language"></a></div>
 <h2>Sprache eines Charakters</h2>
 
-<p>Die Sprache muss dem Charakter und der Spielwelt angemessen sein und sollte
-den Regeln der Rechtschreibung und der Grammatik so gut wie möglich folgen.
-Moderne Ausdrücke sind ebenso wie Abkürzungen und "Leet-Speak" (z.B. "u g0t
-pwn3d") zu unterlassen.</p>
+<p>Die Sprache muss dem Charakter und der Spielwelt angemessen sein und sollte 
+den Regeln der Rechtschreibung und der Grammatik so gut wie möglich folgen. 
+Moderne Ausdrücke sind ebenso wie Abkürzungen und "Leet-Speak" (z.B. "u g0t pwn3d") zu unterlassen.</p>
 
-<p>Das Schreiben oder Erzeugen von sinnlosen oder störenden Nachrichten und die
-durchgehende Verwendung von Großbuchstaben als Betonung ist zu unterlassen
-(z.B. "ICH BIN JETZT DRAN!").</p>
+<p>Das Schreiben oder Erzeugen von sinnlosen oder störenden Nachrichten und die durchgehende 
+Verwendung von Großbuchstaben als Betonung ist zu unterlassen (z.B. "ICH BIN JETZT DRAN!").</p>
 
 <p>Die Gespräche im Spiel sind aus Sicht des Charakters zu führen. Gespräche,
 die nichts mit der Kommunikation der Charaktere zu tun haben (OOC), sind auf
@@ -229,13 +195,14 @@ zufälliger Zuhörer nicht verletzt werden.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
-<div><a id="behaivor"></a></div>
+<div><a id="behavior"></a></div>
 <h2>Verhalten</h2>
 
-<p>Jegliche Belästigungen, Bedrohungen oder Beleidigungen, einschließlich
-rassistischer oder sexistischer Äußerungen, werden nicht toleriert. Dies gilt
-für alle Plattformen von Illarion: das Spiel, das Forum und den Chat,
-einschließlich privater Nachrichten.</p>
+<p>Jegliche Belästigungen, Bedrohungen oder Beleidigungen, 
+einschließlich rassistischer oder sexistischer Äußerungen, 
+die sich gegen andere Spieler richten, werden nicht toleriert. 
+Dies gilt für alle Plattformen von Illarion: 
+das Spiel, das Forum und den Chat, einschließlich privater Nachrichten.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
@@ -262,28 +229,13 @@ so werden diese nicht ersetzt.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
-<div><a id="attribs"></a></div>
-<h2>Änderung von Attributen</h2>
-
-<p>Wenn vom Spieler der Wunsch besteht, die Attribute eines Charakters zu
-ändern, dann besteht dazu einmalig die Möglichkeit. Dazu muss bei einem
-Gamemaster die Änderung angegeben werden. Generell werden nur Änderungen durchgeführt, 
-bei denen höchstens vier Attribute um 'jeweils' maximal 2 Punkte geändert werden 
-und bei denen die Summe aller Attribute erhalten bleibt.
-Außerdem darf kein Attribut durch die Änderung auf einen Wert kleiner als 5
-oder größer als 16 geändert werden.</p>
-
-<?php Page::insert_go_to_top_link(); ?>
-
 <div><a id="gms"></a></div>
 <h2>Gamemaster</h2>
 
 <p>Gamemaster sind dafür zuständig, Spielern zu helfen, Events zu veranstalten
-und für Ordnung im Spiel zu sorgen. Das Belügen eines Gamemasters oder das
-Zurückhalten von Informationen in Bezug auf Regelverstöße und Programmfehler ist
-verboten. Die Namen von Spielcharakteren eines Gamemasters oder eines anderen
-Teammitglieds dürfen ohne seine Erlaubnis nicht genannt oder weitergegeben
-werden, um seine Privatsphäre zu wahren.</p>
+und für Ordnung im Spiel zu sorgen.
+Die Gamemaster organisieren auch die Fraktionen Cadomyr, Galmair und Runewick. 
+Somit sind alle Anfragen, die die Spielwelt betreffen, an sie zu richten.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
