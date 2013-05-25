d9b4c5ab6f518293524f1328acab984b83e768aa
vilarion <vilarion@illarion.org>
2013-05-25 09:50:06 +0200

    Add explorer highscore

--- website/statistics/us_highscores.php
+++ website/statistics/us_highscores.php
@@ -19,7 +19,11 @@
                  'SELECT richest.id AS id, richest.money AS points FROM homepage.richest',
                  'us', 61, 'gold coins', 'Gold');
                  
-	addHighscore('Arena Master',
+    addHighscore('Arena Master',
                  'SELECT qpg_userid AS id, SUM(qpg_progress) AS points FROM illarionserver.questprogress WHERE qpg_questid IN (801, 802, 803) GROUP BY id',
                  'us', 1, 'arena points', 'sword');
+                 
+    addHighscore('Illariontrotter',             
+                 "SELECT qpg_userid AS id, SUM(char_length(replace(qpg_progress::bit(32)::text, '0', ''))) AS points FROM illarionserver.questprogress WHERE qpg_questid >= 130 AND qpg_questid < 150 GROUP BY id",
+                 'us', 66, 'marker stones', 'marker stone');
 ?>
