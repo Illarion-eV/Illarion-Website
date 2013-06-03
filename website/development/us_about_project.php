9ab33ddeb29fd2112c07e269279e3d9eaf4b9b5f
vilarion <vilarion@illarion.org>
2013-06-03 21:44:03 +0200

    Replace outdated project page with current vision

--- website/development/us_about_project.php
+++ website/development/us_about_project.php
@@ -1,43 +1,29 @@
 <?php
    include_once ( $_SERVER['DOCUMENT_ROOT'] . "/shared/shared.php" );
-   create_header( "Illarion - About the Project",
-                  "What Illarion is, and why we created it.",
-                  "Project, Beginning" );
+   create_header( "Illarion - The Vision",
+                  "The Vision of the Illarion Team.",
+                  "Project, Vision" );
    include_header();
 ?>
 
-   <h1>What Illarion means to us</h1>
+<h1>The Illarion Vision</h1>
 
-   <?php cap(W); ?>
+<?php cap(T); ?>
 
-   <p>e aim to create a complex online role playing game (multi user dungeon). Some may moan and say &quot;there are so many MUDs. why not join another project?&quot;. The answer is simple: A student of computer science (which we all have been at the beginning of the project) must be familiar with network programming, threads, AI (artificial intelligence), software development in teams, object oriented programming, etc. These, combined with our hobby (roleplaying) and other reasons, led us to the conclusion to program such a game on our own. We want to learn and have fun, and feel that this is best achieved by creating our own game, instead of joining an existing one. We greatly appreciate input and exchanges of knowledge/experience from players and other programmers alike.</p>
+<p>he vision that drives our developers and volunteers daily to develop the game Illarion with no financial reimbursement is motivated by the goal of creating an online role playing game, which concentrates on true role-playing in a classic fantasy world. Illarion is operated for the public without profit, and is free for players. In order to ensure this, the Illarion society was established, which sets this in its constitution.</p>
 
-   <p>A feature that is very important to us is the depth of real roleplaying in Illarion. We want it to stand out from your every other RPG, that mainly puts focus on hack and slash monster fighting. In Illarion, it should be possible to live your whole life without ever killing a monster. This we try to ensure with the vocations system. The different crafts available supplement each other, so that craftsmen need to work together to be truly successful.</p>
+<p>Illarion provides a huge number of possibilities for the creation of characters, without limiting them to classes. Each player should be able to sculpt their own character and the adventures they experience based on their own imagination, and to have them actively participate in the adventures of other characters. The game thrives on cooperative interaction, even amongst opposing characters. The player will continuously write the history of Illarion. Credible political groups shall support this goal.</p>
 
-   <p>We try to let roleplaying run the course of the game, since character interaction is very important to us. Most games underrate roleplaying and instead focus on technical detail, where as in Illarion it&#39;s the other way round (even though Illarion is not the worst game of it&#39;s genre technically either).</p>
+<p>Illarion is open to all players, regardless of nationality, language and role-playing experience. The main target audience are casual gamers who maintain a distant relationship with their characters, and are thus able to engage in real role play.</p>
 
-   <p>In most other games in this genre, you can always see the other characters&#39; names, which often leads to conflicts. In Illarion, you have to introduce yourself to someone if you want them to recognize you.</p>
+<p>Each player has the right, but also the duty, to shape the game world as they desire. This spans from active participation in in-game events to the use of our game content editors, which are accessible to all players.</p>
 
-   <p>The magic system is very complex: There are different runes, each with a specific meaning. Players must explore spells by trying out meaningful combinations (with a total of 4 billion possible combinations). They must learn about each rune from masters of the arcane arts. The chance of success in casting spells is determined by the mage&#39;s skill with each of the combined runes.</p>
+<p>No-one should log into the game without knowing of the tasks and adventures he can partake in. The world Illarion is a dynamic, ever-changing world, giving options to players through its quests and events, while thriving and growing from the input of player actions. No-one should play the game merely for the trivialities of the day to day in-game life amongst small groups, but to get involved in the wider, major global events that affect all players equally.</p>
 
-   <p>Druid magic consists of potion brewing. Once a wise druid has explained the herbs to you, you can set out to find them in the places they grow. Potions are made by experimenting with different mixes of herbs.</p>
+<p>Goals pursued by a player should go beyond generic accumulation of objects, skills and gold coins. Our simple game mechanics ensure that each style of play and each effort is equally rewarded, refraining from the introduction of technical restrictions and keeping monitoring of the players to the bare minimum. Freedom, to bring cooperative role-play into the world as you see fit, is above any point or rule. A player of Illarion is not a consumer of ready-made content, but that which itself shapes the community and the world of Illarion itself.</p>
 
-   <p>You can lead a successful life without killing monsters, because there are no experience points. You can live as a craftsman or trader instead.</p>
+<p>The Illarion Team</p>
 
-   <p>There will be a social system. Your rank in this system will depend on the amount of money you spend on luxury goods (expensive clothes and food for instance), so you can reach powerful positions in politics. This enables you to play a feudal lord, that protects his people in court and war, taking money from them instead.</p>
-
-   <p>It is possible to knock player unconscious instead of killing them. This way you can be a robber instead of a murderer.</p>
-
-   <p>In other games there is often trouble with people talking about their skill values and experience. In Illarion you will not know any of these values although you know their tendency.</p>
-
-   <p>Other graphical online games often have no races. Here you can pick from several races with their own graphics and cities (e.g. the elves live in tree houses, the dwarves in caves, lizardman in the ocean, etc.).</p>
-
-   <p>Many actions will change your alignment. If you kill a unicorn or summon an undead, you will drop in alignment, if you hunt vampires or heal players it will rise. This will influence your spell successes since each spell has an alignment value (i.e. an evil sorcerer casts evil spells easily, and is bad at healing magic).</p>
-
-   <p>These are only some examples on how we will encourage roleplay. Not everything is developed yet since we are still working on the game. You are welcome to check it out and tell us about your ideas and comments on the appropriate boards in the forum.</p>
-
-   <p class="center"><a href="mailto:alatar@illarion.org">Alatar - Project Initiator</a></p>
-
-   <?php insert_go_to_top_link(); ?>
+<?php insert_go_to_top_link(); ?>
 
 <?php include_footer(); ?>
