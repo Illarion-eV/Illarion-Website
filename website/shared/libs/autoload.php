435375b6082e963a56bbbf7c1d809c6590532460
Martin Karing <nitram@illarion.org>
2013-10-18 22:48:37 +0200

    Updated JPGraph

--- website/shared/libs/autoload.php
+++ website/shared/libs/autoload.php
@@ -6,6 +6,10 @@ function libraries_autoload($class) {
     switch ($class) {
         case 'PHPMailer': require __DIR__ . '/PHPMailer/class.phpmailer.php'; break;
         case 'StringParser_BBCode': require __DIR__ . '/stringparser_bbcode/stringparser_bbcode.class.php'; break;
+        case 'Graph': require __DIR__ . '/jpgraph/jpgraph.php'; break;
+        case 'ScatterPlot': require __DIR__ . '/jpgraph/jpgraph_scatter.php'; break;
+        case 'BoxPlot': require __DIR__ . '/jpgraph/jpgraph_stock.php'; break;
+        case 'LinePlot': require __DIR__ . '/jpgraph/jpgraph_line.php'; break;
     }
 }
 
