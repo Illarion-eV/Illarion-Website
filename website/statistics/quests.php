7103495efc46f2f57e7d16541a6b003657c4a369
Martin Karing <nitram@illarion.org>
2013-12-13 14:14:56 +0100

    Added common access page for the quests

---
+++ website/statistics/quests.php
@@ -0,0 +1,13 @@
+<?php
+/** This is the language independent access file for the news. */
+include ( $_SERVER['DOCUMENT_ROOT'] . '/shared/shared.php' );
+
+$query = '';
+if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) {
+    $query = '?' . $_SERVER['QUERY_STRING'];
+}
+if (preg_match('/de/', $_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    Page::redirect(Page::getUrlToFile(__DIR__ . '/de_quests.php') . $query);
+} else {
+    Page::redirect(Page::getUrlToFile(__DIR__ . '/us_quests.php') . $query);
+}
