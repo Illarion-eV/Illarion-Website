9e100d7b65341dccc8feffb8f1c8aea4f0411cb3
alessaina <kpstew012@gmail.com>
2014-03-22 23:29:53 -0500

    Spelling and grammar corrections

--- website/illarion/races/us_story.php
+++ website/illarion/races/us_story.php
@@ -10,14 +10,14 @@
 
 <?php cap(A); ?>
 
-<p>n Eagle is escalating higher and higher into the sky above Illarion. In search of prey it
+<p>n eagle is escalating higher and higher into the sky above Illarion. In search of prey, it
 scans the vast lands. It sees the high mountains, seemingly touching the firmament with their
 jagged peaks and snowcapped summits. With its sharp eyes it recognizes the hidden entrances to
 the great mines of the <a href="us_dwarfs.php" class="eyecatcher">dwarfs</a>, who dug deep into the heart of the mountain ranges to rob the
 cold stone of its most precious treasures.</p>
 
 <p>Not far away dense smoke is rising out of the caves of the <a href="us_gnomes.php" class="eyecatcher">gnomes</a>.
-The eagle drifts for a moment upon the winds and watches the hustle and bustle of this inventive people, before he turn away.</p>
+The eagle drifts for a moment upon the winds and watches the hustle and bustle of this inventive people, before he turns away.</p>
 
 <p>The howling of the wolf packs is accompanying the eagle who is gliding down the flanks of the
 mountains into the wood covered valleys. Here and there a group of bradamen trees rises above
@@ -28,7 +28,7 @@ Sparklings fly up as the eagle lands an a tree for a short rest and the friendly
 
 <p>Further on the eagle flies and on its way the mountains are replaced by a gentle rolling
 hilly landscape, only intermittent by the great rivers slowly streaming towards the sea. In this
-fertile area the villages of the <a href="us_halflings.php" class="eyecatcher">halflings</a> can be found, which dwellings are surrounded by the
+fertile area the villages of the <a href="us_halflings.php" class="eyecatcher">halflings</a> can be found, whose dwellings are surrounded by
 well maintained gardens and fields.</p>
 
 <?php cap(E); ?>
@@ -37,36 +37,36 @@ well maintained gardens and fields.</p>
 between a column of smoke rises into the sky, testifying the dwellings and campgrounds of the
 <a href="us_orcs.php" class="eyecatcher">orcs</a>. Their battle cries echo far across the plains and everyone hearing their shouts is well
 advised to enter their realm with great care.
-In the near he see also the smaller and more chaotic arranged campgrounds of the <a href="us_goblins.php" class="eyecatcher">goblins</a>.
-In spite of the distance it's not hard to recognize the through the camp bustling creatures.</p>
+Near he also sees the smaller and more chaoticly arranged campgrounds of the <a href="us_goblins.php" class="eyecatcher">goblins</a>.
+In spite of the distance, it's not hard to recognize them through the camp of bustling creatures.</p>
 
 <p>Finally the eagle reaches the windswept coast and comes across the big cities of the proud
 <a href="us_humans.php" class="eyecatcher">humans</a>, on which walls and battlements the flags flap in the wind. Between the towers and roofs
 it recognizes the buzz of activity filling the market place and the sunbeams are reflected by
-the armor of the knights riding on horse backs and trying to find their way through the
+the armour of the knights riding on horse back and trying to find their way through the
 crowd.</p>
 
 <p>Starting from the harbor the eagle follows the huge merchant ships setting out on their
-perilous trip across the sea. Above the rolling waves it recognizes the dimly outline of the
-great praying halls, which were build by the Lizardmen deep below the surface. They live in an
+perilous trips across the sea. Above the rolling waves it recognizes the dim outline of the
+great praying halls, which were built by the Lizardmen deep below the surface. They live in an
 element which is alien to the eagle and while it circles in the sky the <a href="us_lizards.php" class="eyecatcher">lizardmen</a> are gliding
-gentle through the sea and only their glistening scales can sometimes be seen through the
+gently through the sea and only their glistening scales can sometimes be seen through the
 water.</p>
 
 <?php cap(T); ?>
 
 <p>he world of Illarion is filled with unexplainable wonders and mysterious places, unutterable
-horrors and dangerous locations. Out of the dark past demons and monsters arise longing for the
+horrors and dangerous locations. Out of the dark past demons and monsters arise, longing for the
 lives of the mortals. Therefore it is not astonishing that in this magical world in which
 everyone&#39;s destiny is fading, the races seek refuge and comfort with the Gods. 16 deities
-determine the life in Illarion and exert influence on everything that proofs to be beneficial to
-them. In consequence wars have been started and reconciled, whole families seized power and were
+determine the life in Illarion and exert influence on everything that proves to be beneficial to
+them. In consequence, wars have been started and reconciled, whole families seized power and were
 overthrown. May it be that there are a few madmen who renounce their existence and defy their
-will, but there is no god, who is not aware of the existence of each living being not matter how
+will, but there is no god, who is unaware of the existence of each living being, no matter how
 insignificant. It would be unwise to ignore such facts.</p>
 
-<p>For the eagle this world is truly alien, nevertheless it continues to circle above Illarion.
-But from far up in the sky it enjoys the broad view of all places and with its sharp eyes it can
+<p>For the eagle this world is truly alien. Nevertheless it continues to circle above Illarion.
+But from far up in the sky, it enjoys the broad view of all places and with its sharp eyes it can
 see everything happening on this world.</p>
 
 <?php insert_go_to_top_link(); ?>
