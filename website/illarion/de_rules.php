5dbbb7fb76597164a3330766ca23f11c89e28818
vilarion <vilarion@illarion.org>
2014-06-17 16:12:53 +0200

    Revert code of conduct to previous state

--- website/illarion/de_rules.php
+++ website/illarion/de_rules.php
@@ -28,19 +28,43 @@ alle Beteiligten.</p>
 
 <h2>Verhaltenscodex</h2>
 
-<p>Dies ist ein echtes Rollenspiel und dein Charakter ist nicht dein 
-Abbild. Lass ihn Schwächen haben und auch mal Fehler machen; wir alle 
-machen Fehler. Unterscheide stets zwischen dir und deinem Charakter und 
-lass Probleme in der Spielwelt dort wo sie hingehören: Im Spiel. Wenn das
-Verhalten eines anderen Spielers für dich unerträglich ist, wende dich 
-an einen Gamemaster.</p>
+<?php Page::cap('R'); ?>
+<p>egeln sind wichtig, um eine gute Rollenspielumgebung zu schaffen. Aber keine
+Regeln können beschreiben, wie sich die Spieler untereinander verhalten. Dieser
+Verhaltenskodex soll aufzeigen, welches Spielklima und welche Spielkultur in
+Illarion erwünscht sind. Denn ebenso wichtig wie ein ausgefeilter Regelkatalog
+ist es, dass die Spieler rücksichtsvoll, respektvoll und mit Nachsicht
+miteinander umgehen.</p>
 
-<p>Wie bereits erwähnt spielen wir miteinander, nicht gegeneinander. 
-Illarion lebt vom kooperativen Zusammenspiel,was bedeutet dass die 
-Spieler gemeinsam das Spiel weiter gestalten. Wir bitten darum, dass 
-alle Spieler respektvoll und besonnen etwas zum Projekt beitragen 
-und aktiv mitarbeiten, um ein attraktives Spiel für Leute aus der 
-ganzen Welt zu gewährleisten.</p>
+<?php Page::insert_go_to_top_link(); ?>
+
+<?php Page::cap('I'); ?>
+<p>llarion ist ein echtes Rollenspiel. Dein Charakter ist nicht dein Abbild und
+sein Erfolg oder Misserfolg ist nicht deiner. Unterscheide stets zwischen dir
+und deinem Charakter und lass ihn Schwächen haben und auch mal Fehler machen.
+Auch Spieler machen mitunter Fehler. Sei tolerant gegenüber den Fehlern anderer.
+Niemand ist perfekt, auch du nicht. Wenn das Verhalten eines anderen Spielers
+für dich unerträglich ist, wende dich an einen Gamemaster und lasse den Spieler
+in Ruhe. Du solltest nie davon ausgehen, dass irgendjemand dir den Spaß an
+Illarion verderben will und genauso solltest du niemandem den Spaß verderben
+wollen. Überlege dir also stets, ob was immer du im Spiel tust, auch anderen
+Spielern Spaß bereitet oder nur dir alleine.</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<?php Page::cap('I'); ?>
+<p>llarion lebt vom kooperativen Zusammenspiel, auch wenn Charaktere Feinde
+sein mögen, so sollen die Spieler doch gemeinsam die Geschichte der Welt
+Illarion weiterschreiben. Ein Gegeneinander hilft niemanden; dies gilt auch für
+das Team von Illarion. Es besteht aus Freiwilligen, die in ihrer Freizeit für
+dich ein kostenloses Spiel betreuen und dabei genauso wenig perfekt sind wie
+du. Gegenseitiger Respekt ist genauso wichtig wie gesittete Kommunikation – ein
+hilfreiches Wort bewirkt manchmal mehr als tausend Beschwerden. Das Team leistet
+einen Teil, um das Spiel zu formen, aber den weit größeren Teil leisten die
+Spieler. Die Geschichten und Abenteuer im Spiel, aber auch der Umgangston
+außerhalb im Forum und Chat, werden von Spielern wie dir geprägt. Bitte hilf
+aktiv dabei mit, dass Illarion seine respektvolle, rücksichtsvolle und
+angenehme Atmosphäre beibehält, die Rollenspieler aus aller Welt schätzen.</p>
 
 <h2>Spielregeln</h2>
 
--- website/illarion/us_rules.php
+++ website/illarion/us_rules.php
@@ -26,17 +26,40 @@ able to handle certain themes and content within the game with maturity.</p>
 
 <h2>Code of Conduct</h2>
 
-<p>Because this is a roleplaying game, your character is not your avatar. 
-Let your character have flaws and commit mistakes; we all make mistakes. 
-You must separate yourself from your character, and keep in-game matters 
-where they belong: in-game. If the behaviour of other players bothers you, 
-please contact a gamemaster.</p>
+<?php Page::cap('R'); ?>
+<p>ules are important to create a good roleplay atmosphere. But no rules can
+describe how players should behave with each other. This code of conduct shows
+what kind of game climate and game culture we would like to create in Illarion.
+Because it is important to have a set of guidelines which shows players how to
+be respectful, considerate and to be lenient with one another.</p>
 
-<p>As stated above, we play with, not against each other. Illarion is 
-based on cooperative roleplaying, meaning all players work together to 
-help shape Illarion. We ask that each player contributes and actively 
-participates in a respectful and considerate manner that helps attract 
-players from all over the world.</p>
+<?php Page::insert_go_to_top_link(); ?>
+
+<?php Page::cap('I'); ?>
+<p>Illarion is a true roleplaying game. Your character is not your avatar in
+another world and his gain and loss is not yours. Separate yourself from your
+character and let him have flaws and commit mistakes. Players commit mistakes
+once in a while, too. Be lenient towards the mistakes of other players, nobody
+is perfect, not even you. If you cannot stand the behaviour of another player,
+leave him alone and contact a gamemaster. Have faith in the fact that no one
+wants to harm your fun on purpose, likewise, you must not want to spoil anyone's
+fun. Consider whether everything you do in game is also fun for other players or
+just for you alone.</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<?php Page::cap('I'); ?>
+<p>llarion is based on cooperative playing, even if characters may be foes, the
+players shall continue the tale of Illarion hand in hand. To battle each other
+is in vain, this holds for the Illarion staff, too. It consists of volunteers
+that manage a free game in their spare time, without being perfect, just like
+you. Respect and common courtesy are as important as good communication; 
+helpful word has commonly more effect than a thousand complaints. The staff
+does its share to shape the world of Illarion, but the major share is done by
+the players. The tales and adventures in the game as well as the way of
+communication on the boards and in the chat are influenced by players just like
+you. Please contribute actively to maintain an atmosphere of respect and
+considerateness that attracts roleplayers from all around the world.</p>
 
 <h2> Common Game Rules </h2>
 
