51a967a74a9fecb0f4df967f5d643ee91ea24b8f
estralis <estralis@illarion.org>
2014-06-28 13:28:26 +0200

    Simplify the page

--- website/community/de_society.php
+++ website/community/de_society.php
@@ -17,9 +17,10 @@
    <p>llarion ist ein kostenloses Spiel, welches von einem Team von Freiwilligen unterhalten wird. Niemand verdient auch nur einen Cent an Illarion, dennoch
 	kostet der Betrieb des Servers Geld. Dieses Geld wird von den Mitgliedern des Illarion e.V. aufgebracht.</p>
 
-   <p>Der einfachste Weg, unsere Ausgaben planbar zu decken, ist Eure freiwillige F&ouml;rdermitgliedschaft im Illarion e.V. f&uuml;r nur 10 Euro im Jahr. Da beide Seiten (ihr und der Verein) davon profitieren k&ouml;nnen und sollen, haben wir uns Gedanken gemacht, welche Vorteile wir f&uuml;r F&ouml;rdermitglieder schaffen. Das Formular für den Antrag auf Fördermitgliedschaft findet ihr hier:</p>
+   <p>Der einfachste Weg, unsere Ausgaben planbar zu decken, ist Eure freiwillige F&ouml;rdermitgliedschaft im Illarion e.V. f&uuml;r nur 10 Euro im Jahr. Da beide Seiten (ihr und der Verein) davon profitieren k&ouml;nnen und sollen, haben wir uns Gedanken gemacht, welche Vorteile wir f&uuml;r F&ouml;rdermitglieder schaffen. Das Formular für den Antrag auf Fördermitgliedschaft und die Satzung des Illarion e.V. findet ihr hier:</p>
    <ul>
       <li><a href="Illarion_e.V._-_Antrag_auf_Foerdermitgliedschaft.pdf">Illarion e.V. - Antrag auf F&ouml;rdermitgliedschaft</a> (pdf)</li>
+	  <li><a href="Illarion_e.V._-_Satzung.pdf">Illarion e.V. - Satzung</a> (pdf)</li>
    </ul>
    <p>Grundgedanke ist dabei, dass keine Vorteile im Spiel aus diesen Boni entstehen d&uuml;rfen, denn sonst w&auml;re es ein verkappter Bezahl-Account - kurz, wer zahlt w&uuml;rde im Spiel bessere Chancen haben und dies wollen wir vermeiden.</p>
 
@@ -31,11 +32,6 @@
    </ul>
 
    <p>Diese Liste kann sich jederzeit &auml;ndern, weitere Ideen sind bereits vorhanden. Ein Rechtsanspruch auf die Boni besteht nicht. </p>
-	<p>Die Satzung des Illarion e.V. findet ihr hier:</p>
-
-   <ul>
-      <li><a href="Illarion_e.V._-_Satzung.pdf">Illarion e.V. - Satzung</a> (pdf)</li>
-   </ul>
    
    <?php insert_go_to_top_link(); ?>
    
