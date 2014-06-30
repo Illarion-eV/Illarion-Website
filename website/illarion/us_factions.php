9dd1b01b86d416b7a66bb249321e3fa2238712ed
estralis <estralis@illarion.org>
2014-06-30 20:23:40 +0200

    Reduce spacing

--- website/illarion/us_factions.php
+++ website/illarion/us_factions.php
@@ -19,7 +19,7 @@
 
 <h2><a name="1"></a>Cadomyr</h2>
 
-<p><img align="left" vspace="10" hspace="20" src="<?php echo $url; ?>/shared/pics/factions/cadomyrShieldDirt150.png" alt="Cadomyr"/>Flanked by the impenetrable Lonely Mountains and the Great Ocean, Cadomyr’s foreboding fortress walls rise from the desert sands. From the Lonely Mountains the Kantabi River cuts through the Kantabi desert, demarking the territory of Cadomyr as it flows towards the ocean where it diverges into the Kantabi Delta. The hot desert sun reflects off the uniform sandstone walls of the town while grand paintings and statues depict glorious feats of past rulers.</p>
+<p><img align="left" vspace="10" hspace="10" src="<?php echo $url; ?>/shared/pics/factions/cadomyrShieldDirt150.png" alt="Cadomyr"/>Flanked by the impenetrable Lonely Mountains and the Great Ocean, Cadomyr’s foreboding fortress walls rise from the desert sands. From the Lonely Mountains the Kantabi River cuts through the Kantabi desert, demarking the territory of Cadomyr as it flows towards the ocean where it diverges into the Kantabi Delta. The hot desert sun reflects off the uniform sandstone walls of the town while grand paintings and statues depict glorious feats of past rulers.</p>
 
 <p>Sheltered behind buttressed walls is the Royal Palace, home to Queen Rosaline Edwards. She is the daughter of Sir Reginald, last of a long line of illustrious Kings of Albarian origin who ruled the desert fortress. A rich heritage of combat and stringent codes of honour sustains the traditional customs. With this proud and ambitious Queen the royal bloodline continues, some say blessed by the gods themselves. As a woman of considerable honour, and thus power, Queen Rosaline Edwards is the undisputed ruler of Cadomyr, holding the two stones of power, amethyst and topaz.</p>
 
@@ -31,7 +31,7 @@
 
 <h2><a name="2"></a>Galmair</h2>
 
-<img align="right" vspace="10" hspace="20" src="<?php echo $url; ?>/shared/pics/factions/galmairShieldDirt150.png" alt="Galmair"/>
+<img align="right" vspace="10" hspace="10" src="<?php echo $url; ?>/shared/pics/factions/galmairShieldDirt150.png" alt="Galmair"/>
 
 <p>Nestled in the foothills of the Lonely Mountains, Galmair’s imposing walls join with rocky outcrops to form formidable fortifications. Malachite Creek courses through Galmairian territory as it flows from the mountains to Lake Syrita, bordering edge of Nargun’s Plain. Originally built by dwarves, much of the town itself lies underground with cavernous halls and tunnels cleaved deep into the mountains.</p>
 
@@ -45,7 +45,7 @@
 
 <h2><a name="1"></a>Runewick</h2>
 
-<img align="left" vspace="10" hspace="20" src="<?php echo $url; ?>/shared/pics/factions/runewickShieldDirt150.png" alt="Runewick"/>
+<img align="left" vspace="10" hspace="10" src="<?php echo $url; ?>/shared/pics/factions/runewickShieldDirt150.png" alt="Runewick"/>
 
 <p>Set in an archipelago of forested islands, Runewick’s towers soar above fertile fields and bountiful glades. Across Lunord Bridge lies the farming community of Yewdale with Runewick harbour at the entrance to Snakehead Bay, whilst animals graze as far as the Dewy Swamps. Founded by the Archmage, the towers themselves provide an eclectic seat of learning amongst a bustling town.</p>
 
