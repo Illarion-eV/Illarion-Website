82c08dc8bdfea00ef981a53b5fd45bd8ea3b32a4
estralis <estralis@illarion.org>
2014-11-08 19:07:00 +0100

    Improve progress page

--- website/development/de_progress.php
+++ website/development/de_progress.php
@@ -11,7 +11,9 @@
 
 <h1>Entwicklungsfortschritt</h1>
 
-<h2>Meilenstein I: Stabiler Zustand für Reviewer und Werbung</h2>
+<p>Die Entwicklung Illarions folgt klar definierten Meilensteinen, welche darauf abzielen, für die existierenden Spielbereiche einen Zustand zu erreichen, der keine zukünftige Überarbeitung mehr erfordert. Somit soll das Spiel anschließend nur noch durch Verbesserungen und zusätzliche Inhalte verbessert werden, wobei keine grundlegenden Probleme oder Fehler mehr auftreten. Wenn du bei der Entwicklung Illarions mithelfen willst, wende dich an einen der <a href="http://illarion.org/community/de_contact.php#4">Entwickler</a> oder schau einfach mal im <a href="http://illarion.org/community/de_chat.php">Chat</a> vorbei. Klicke auf Titel der Meilensteine, um auf die entsprechende Zusammenstellung der Entwicklungspunkte im Mantis-Bugtracker zu gelangen.</p>
+
+<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Meilenstein I: Stabiler Zustand für Reviewer und Werbung</a></h2>
 
 <p>Text</p>
 
@@ -23,7 +25,7 @@
     <li><span class="planned">Text</span></li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Meilenstein II: Neue Gegenstände und verbessertes Handwerk</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Meilenstein II: Neue Gegenstände und verbessertes Handwerk</a></h2>
 
 <p>Text</p>
 
@@ -35,7 +37,7 @@
     <li><span class="planned">Text</span></li>
 </ul>
 
-<h2>Meilenstein III: Magie</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Meilenstein III: Magie</a></h2>
 
 <p>Text</p>
 
@@ -47,7 +49,7 @@
     <li><span class="planned">Text</span></li>
 </ul>
 
-<h2>Meilenstein IV: Überarbeitung des Kampfsystems</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Meilenstein IV: Überarbeitung des Kampfsystems</a></h2>
 
 <p>Text</p>
 
@@ -59,7 +61,19 @@
     <li><span class="planned">Text</span></li>
 </ul>
 
-<h2>Meilenstein V: Sandkasten und Immersion</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Meilenstein V: Sandkasten und Immersion</a></h2>
+
+<p>Text</p>
+
+<h3>Highlights</h3>
+
+<ul>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+</ul>
+
+<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Meilenstein VI: Inhalte, Quests und Dungeons</a></h2>
 
 <p>Text</p>
 
