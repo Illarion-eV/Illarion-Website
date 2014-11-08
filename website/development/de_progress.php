c5512cb4559ca5ee63dc66b09f707222c3d620e1
estralis <estralis@illarion.org>
2014-11-08 18:56:21 +0100

    #10480: Publish milestones

--- website/development/de_progress.php
+++ website/development/de_progress.php
@@ -10,54 +10,63 @@
 ?>
 
 <h1>Entwicklungsfortschritt</h1>
-<p>
-    Legende:
-    <span class="planned">Geplant</span>,
-    <span class="inProgress">In Arbeit</span>,
-    <span class="testing">Testen</span>,
-    <span class="done">Fertiggestellt</span>
-</p>
 
-<h2>Server</h2>
+<h2>Meilenstein I: Stabiler Zustand für Reviewer und Werbung</h2>
+
+<p>Text</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Entfernen von Luabind</span></li>
-    <li><span class="inProgress">Test-Suite</span></li>
-    <li><span class="inProgress">Effiziente Datenstrukturen zur Spielersuche</span></li>
-    <li><span class="planned">Benachrichtigung &uuml;ber verf&uuml;gbare Quests</span></li>
-    <li><span class="planned">Neue Login-Prozedur</span></li>
-    <li><span class="planned">Charaktererstellung</span></li>
-    <li><span class="planned">Bereitstellung als Debian-Paket (.deb)</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
 </ul>
 
-<h2>Client</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Meilenstein II: Neue Gegenstände und verbessertes Handwerk</a></h2>
+
+<p>Text</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Sitzungsverwaltung</span></li>
-    <li><span class="planned">Anzeige von verf&uuml;gbaren Quests</span></li>
-    <li><span class="planned">Neue Login-Prozedur</span></li>
-    <li><span class="planned">Charaktererstellung</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
 </ul>
 
-<h2>Inhalt</h2>
+<h2>Meilenstein III: Magie</h2>
 
-<h3>Systeme</h3>
+<p>Text</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Balancierung des Handwerks</span></li>
-    <li><span class="planned">Magie</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
 </ul>
 
-<h3>NPCs</h3>
+<h2>Meilenstein IV: Überarbeitung des Kampfsystems</h2>
+
+<p>Text</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Dynamische Wachen</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
 </ul>
 
-<h3>Sonstiges</h3>
-<ul>
-    <li><span class="inProgress">Quests</span></li>
-    <li><span class="inProgress">Quest-Logs</span></li>
-</ul>
+<h2>Meilenstein V: Sandkasten und Immersion</h2>
 
-<h2>Homepage</h2>
+<p>Text</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">GM-Tool</span></li>
-    <li><span class="inProgress">Umstellung auf CMS Drupal</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Text</span></li>
 </ul>
