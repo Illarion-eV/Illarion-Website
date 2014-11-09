79ad00b4a81440b70f06a6bcf7fe2f749f67f9e1
estralis <estralis@illarion.org>
2014-11-09 10:27:55 +0100

    Complete progress page

--- website/development/de_progress.php
+++ website/development/de_progress.php
@@ -17,72 +17,72 @@
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Meilenstein I: Stabiler Zustand für Reviewer und Werbung</a></h2>
 
-<p>Text</p>
+<p>Dieser Meilenstein zielt darauf ab, das Spiel von Fehlern zu befreien und den Eindruck während der ersten Spielstunde und den Weg dorthin zu verbessern. Der gesamte Weg vom Auffinden des Spiels, seiner Installation und dem Start ins Spiel ist enthalten. Inhalte, die sich nicht in der ersten Spielstunde bemerkbar machen, sind nicht Inhalt des Meilensteins, seien sie noch so wichtig für das Spiel.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Verbesserung der Charaktererstellung</span></li>
+    <li><span class="planned">Optische Verbesserungen des Clients und seiner Bedienbarkeit</span></li>
+    <li><span class="planned">Aktualisierung der Homepage</span></li>
 </ul>
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Meilenstein II: Neue Gegenstände und verbessertes Handwerk</a></h2>
 
-<p>Text</p>
+<p>Mit diesem Meilenstein werden dem Spiel eine sinnvolle Anzahl von Gegenständen hinzugefügt, insbesondere Waffen und Rüstungen. Ihr Nutzen und ihre Herstellung werden in Hinblick auf die Handwerke, deren Verteilung über die Städte, Ressourcen, Verbrauchsmaterialien und Preise gestaltet.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Über 100 neue Waffen und Rüstungen</span></li>
+    <li><span class="planned">Neugstaltung der Handwerksrezepte</span></li>
+    <li><span class="planned">Verbesserung des Sammelns von Ressourcen</span></li>
 </ul>
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Meilenstein III: Magie</a></h2>
 
-<p>Text</p>
+<p>Magie ist ein notwendiges Features eines Fantasyspiels. Mit diesem Meilenstein werden wir die arkane Magie wieder einführen und ebenso die heilige Magie der Götter. Entsprechende Anpassungen an handwerksähnliche Systeme wie Alchemie werden erfolgen.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Arkane Magie mit dutzenden Zaubersprüchen</span></li>
+    <li><span class="planned">Magie der Götter für Templer und Priester</span></li>
+    <li><span class="planned">Handwerksähnliche Verzauberungen und Alchemieverbesserungen</span></li>
 </ul>
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Meilenstein IV: Überarbeitung des Kampfsystems</a></h2>
 
-<p>Text</p>
+<p>Das derzeitige Kampfsystem bedarf einer Überarbeitung, um fairer, klarer und verlässlicher zu werden. Dieser Meilenstein wird für die Spieler nur wenige sichtbare Veränderungen bringen, jedoch zu einem deutlich verbesserten und flexibleren Kampfsystem führen.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Klarere Verwendung der Attribute</span></li>
+    <li><span class="planned">Fehlerfreier Fertigkeitenzuwachs</span></li>
+    <li><span class="planned">Der Nutzen der verschiedenen Waffen und Rüstungen wird fairer verteilt</span></li>
 </ul>
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Meilenstein V: Sandkasten und Immersion</a></h2>
 
-<p>Text</p>
+<p>Illarion steht im Wettbewerb mit anderen Spielen. Um sich als Indie-Spiel abzuheben, bedarf es Features, die kein anderes Spiel aufweisen kann. Mit diesem Meilenstein wollen wir solche Features einführen, die der Immersion dienen und dem Spieler zusätzliche Freiräume ermöglichen, seinen Charakter auszuspielen.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Bau eigener Häuser</span></li>
+    <li><span class="planned">Dynamische Ereignisse</span></li>
+    <li><span class="planned">Erhöhung der Atmosphäre im Spiel</span></li>
 </ul>
 
 <h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Meilenstein VI: Inhalte, Quests und Dungeons</a></h2>
 
-<p>Text</p>
+<p>Durch das Hinzufügen weiterer Quests und Dungeons soll in diesem Meilenstein die Attraktivität des Spiels erhöht werden. Mit ihnen soll der Hintergrund des Spiels ("Lore") dem Spieler nähergebracht werden. Bücher, NPCs, Inschriften und Ereignisse sollen an jedem wichtigen Ort den Hintergrund präsent machen.</p>
 
 <h3>Highlights</h3>
 
 <ul>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
-    <li><span class="planned">Text</span></li>
+    <li><span class="planned">Neugestaltung der Geschichte und Beschreibung Illarions</span></li>
+    <li><span class="planned">Veröffentlichung von dutzenden neuen Büchern</span></li>
+    <li><span class="planned">Hinzufügen von neuen, komplexen und herausfordernden Quests</span></li>
 </ul>
