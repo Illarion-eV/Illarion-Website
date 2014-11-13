2162f4a890da4702a9f30ca2c4e5c9fe4b35fd0a
estralis <estralis@illarion.org>
2014-11-13 16:22:59 +0100

    Start of translation (will finish asap)

--- website/development/us_progress.php
+++ website/development/us_progress.php
@@ -11,54 +11,79 @@
 
 <h1>Development Progress</h1>
 
-<p>
-    Legend:
-    <span class="planned">Planned</span>,
-    <span class="inProgress">Work-In-Progress</span>,
-    <span class="testing">Testing</span>,
-    <span class="done">Complete</span>
-</p>
+<p>Illarion's development follows clearly specified milestones. Those milestones stand for a state of the game that does not require any future reworking on existing features. Hence, only improvements and new features will be added without fundamental flaws or bugs. </p>
 
-<h2>Server</h2>
+<p>If you want to help to develop Illarion, please contact one of the <a href="http://illarion.org/community/us_contact.php#4">developers</a> or join the <a href="http://illarion.org/community/us_chat.php">chat</a>. Click on the title of a milestone to learn more about the individual development items on Mantis Bugtracker.</p>
+
+<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Milestone I: Stable state for reviewers and promotion</a></h2>
+
+<p>With this milestone, we want to get rid of any bugs and improve the impression of the game during the first hour of playing. The whole road from finding the game over installation until starting the game is affected. Content that is not part of the first hour is not part of this milestone albeit important for the overall game.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Removal of Luabind</span></li>
-    <li><span class="inProgress">Test Suite</span></li>
-    <li><span class="inProgress">Efficient data structures for character search</span></li>
-    <li><span class="planned">Notification about available quests</span></li>
-    <li><span class="planned">New login procedure</span></li>
-    <li><span class="planned">Character creation</span></li>
-    <li><span class="planned">Provide Debian package (.deb)</span></li>
+    <li>Improving the character creation</li>
+    <li>Visual improvements for the client and the interface</li>
+    <li>Update of the website</li>
 </ul>
 
-<h2>Client</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Milestone II: New items and improved crafting</a></h2>
+
+<p>Mit diesem Milestone werden dem Spiel eine sinnvolle Anzahl von Gegenständen hinzugefügt, insbesondere Waffen und Rüstungen. Ihr Nutzen und ihre Herstellung werden in Hinblick auf die Handwerke, deren Verteilung über die Städte, Ressourcen, Verbrauchsmaterialien und Preise gestaltet.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Session management</span></li>
-    <li><span class="planned">Display of available quests</span></li>
-    <li><span class="planned">New login procedure</span></li>
-    <li><span class="planned">Character creation</span></li>
+    <li>More than 100 new weapons and armours</li>
+    <li>Neugstaltung der Handwerksrezepte</li>
+    <li>Verbesserung des Sammelns von Ressourcen</li>
 </ul>
 
-<h2>Content</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Milestone III: Magic</a></h2>
 
-<h3>Systems</h3>
+<p>Magie ist ein notwendiges Features eines Fantasyspiels. Mit diesem Milestone werden wir die arkane Magie wieder einführen und ebenso die heilige Magie der Götter. Entsprechende Anpassungen an handwerksähnliche Systeme wie Alchemie werden erfolgen.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Balancing crafting</span></li>
-    <li><span class="planned">Magic</span></li>
+    <li>Arkane Magie mit dutzenden Zaubersprüchen</li>
+    <li>Magie der Götter für Templer und Priester</li>
+    <li>Handwerksähnliche Verzauberungen und Alchemieverbesserungen</li>
 </ul>
 
-<h3>NPCs</h3>
+<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Milestone IV: Review the fighting system</a></h2>
+
+<p>Das derzeitige Kampfsystem bedarf einer Überarbeitung, um fairer, klarer und verlässlicher zu werden. Dieser Milestone wird für die Spieler nur wenige sichtbare Veränderungen bringen, jedoch zu einem deutlich verbesserten und flexibleren Kampfsystem führen.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Dynamic guards</span>: <a href="http://illarion.org/community/forums/memberlist.php?mode=viewprofile&u=7216">pharse</a></li>
+    <li>Klarere Verwendung der Attribute</li>
+    <li>Fehlerfreier Fertigkeitenzuwachs</li>
+    <li>Der Nutzen der verschiedenen Waffen und Rüstungen wird fairer verteilt</li>
 </ul>
 
-<h3>Other</h3>
+<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Milestone V: Sandboxing and immersion</a></h2>
+
+<p>Illarion steht im Wettbewerb mit anderen Spielen. Um sich als Indie-Spiel abzuheben, bedarf es Features, die kein anderes Spiel aufweisen kann. Mit diesem Milestone wollen wir solche Features einführen, die der Immersion dienen und dem Spieler zusätzliche Freiräume ermöglichen, seinen Charakter auszuspielen.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">Quests</span></li>
-    <li><span class="inProgress">Quest logs</span></li>
+    <li>Build your own house</li>
+    <li>Dynamic events</li>
+    <li>Enhancement of the game atmosphere</li>
 </ul>
 
-<h2>Homepage</h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Milestone VI: Content, quests and dungeons</a></h2>
+
+<p>By adding further quests and dungeons das Hinzufügen weiterer Quests und Dungeons soll in diesem Milestone die Attraktivität des Spiels erhöht werden. Mit ihnen soll der Hintergrund des Spiels ("Lore") dem Spieler nähergebracht werden. Bücher, NPCs, Inschriften und Ereignisse sollen an jedem wichtigen Ort den Hintergrund präsent machen.</p>
+
+<h3>Highlights</h3>
+
 <ul>
-    <li><span class="inProgress">GM Tool</span></li>
-    <li><span class="inProgress">Migration to CMS Drupal</span></li>
+    <li>Redesign of Illarion's lore and history</li>
+    <li>Release of dozens of new books</li>
+    <li>Addition of new, complex and challenging quests</li>
 </ul>
+
