9531664f1ed55d30c897c3ff55a8229d72b660b0
estralis <estralis@illarion.org>
2014-11-16 11:30:40 +0100

    Correct a link

--- website/general/us_faq_general.php
+++ website/general/us_faq_general.php
@@ -225,7 +225,7 @@ Page::Init();
 		
 		<p>If you have advanced skills in Java, C++, LUA, PHP or creating 3D graphics, you might want to contact one of the <a href="http://illarion.org/community/us_contact.php#4">lead developers</a> to get involved in the development of Illarion. Please note that Illarion is a non profit project and hence, no compensation can be paid for your work.
 		
-	<li class="question"><a id="co4"></a><strong>What features are planned for the future?</strong></li>
+	<li class="question"><a id="co5"></a><strong>What features are planned for the future?</strong></li>
 	
 		<p>Illarion's development follows six clearly specified <a href="http://illarion.org/development/us_progress.php">milestones</a>. Those milestones stand for a state of the game that does not require any future reworking on existing features. Crafting and fighting shall be reworked and more types of magic will be added to the game. Also, we'll add unique features to the game no other game can offer to enhance the immersion.</p>
 	
