dc8f3e0c89d9097976fdfd7ee5f5da8be6c2fe1e
estralis <estralis@illarion.org>
2014-11-16 11:51:45 +0100

    Fix typo

--- website/general/us_faq_general.php
+++ website/general/us_faq_general.php
@@ -134,7 +134,7 @@ Page::Init();
 		
 		<p>Crafting generally consists of three steps: Gathering of raw materials such as ores, producing intermediate items such as ingots and crafting final items that can be used such as swords. For each step, you need dedicated tools and locations, e.g. you will need to use a pick axe in a mine to mine ores and a hammer and anvil to forge a sword. Almost every item in this game can be crafted by players and crafting can be a lot of fun, taking into account the vast variety of crafts, resources and products.</p>
 		
-		<p>Repairing items is to by dedicated NPCs, though. You can find one in each major city. The charge a fee for their service, but repairing is usually more cheap than buying a new item from a NPC.</p>
+		<p>Repairing items is done by dedicated NPCs, though. You can find one in each major city. The charge a fee for their service, but repairing is usually more cheap than buying a new item from a NPC.</p>
 
 	<li class="question"><a id="fs6"></a><strong>What about magic? How can I cast spells?</strong></li>
 		
