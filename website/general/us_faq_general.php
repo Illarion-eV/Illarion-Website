08de5212cfd0a39886698bddabeef44e060e87fa
estralis <estralis@illarion.org>
2014-11-16 10:52:11 +0100

    Correct old answers

--- website/general/us_faq_general.php
+++ website/general/us_faq_general.php
@@ -15,10 +15,10 @@ Page::Init();
 	<li><a class="hidden" href="#gs2">How much do I have to pay for Illarion? Is there a monthly fee or an item shop?</a></li>
 	<li><a class="hidden" href="#gs3">What are the system requirements for Illarion?</a></li>
 	<li><a class="hidden" href="#gs4">What do I need to play?</a></li>
-	<li><a class="hidden" href="#gs5">How do I download the game?</a></li>
+	<li><a class="hidden" href="#gs5">Where can I download the game?</a></li>
 	<li><a class="hidden" href="#gs6">How do I log into the game?</a></li>
     <li><a class="hidden" href="#gs7">From what races and classes can I chose?</a></li>
-	<li><a class="hidden" href="#gs8">What attributes should I chose?</a></li>
+	<li><a class="hidden" href="#gs8">What attributes should I pick?</a></li>
 	<li><a class="hidden" href="#gs9">I have a technical problem. Where can I get support?</a></li>
 
 </ul>
@@ -73,15 +73,15 @@ Page::Init();
 		
 	<li class="question"><a id="gs2"></a><strong>How much do I have to pay for Illarion? Is there a monthly fee or an item shop?</strong></li>
 		
-		<p> </p>
+		<p>Illarion is completely free. You have the guarantee that you will never have to pay a cent for this game. There are no premium items nor an item shop. Illarion is run a non profit organisation called <a href="http://illarion.org/community/us_society.php">Illarion e.V.</a>. If you want, you can support the work of the <a href="http://illarion.org/community/us_society.php">society</a> by a voluntary <a href="http://illarion.org/community/us_society.php">donation</a> or by becoming a supporting member.</p>
 		
 	<li class="question"><a id="gs3"></a><strong>What are the system requirements for Illarion?</strong></li>
 
-	    <p>You can review the system requirements on the <a href="http://illarion.org/illarion/us_java_download.php">download page of the website</a>. Illarion is a cross platform game that runs on Windows, Linux and MacOS. Some operation systems like Windows Vista do not support the free graphics standard OpenGL by default. Make sure that you have the latest drivers installed for your graphics adapter.</p>
+	    <p>Illarion is a cross platform game that runs on Windows, Linux and MacOS. You can review the system requirements on the <a href="http://illarion.org/illarion/us_java_download.php">download page of the website</a>. You will need a computer with a graphics adapter that supports Shaders 2.0 and OpenGL. Some operation systems like Windows Vista do not support the free graphics standard OpenGL by default. Make sure that you have the latest drivers installed for your graphics adapter.</p>
 
 	<li class="question"><a id="gs4"></a><strong>What do I need to play?</strong></li>
 		
-		<p> </p>
+		<p>Account, character</p>
 					
 	<li class="question"><a id="gs5"></a><strong>Where can I download the game?</strong></li>
 		
@@ -95,7 +95,7 @@ Page::Init();
 	
 		<p> </p>
 		
-	<li class="question"><a id="gs8"></a><strong>What attributes should I chose?</strong></li>
+	<li class="question"><a id="gs8"></a><strong>What attributes should I pick?</strong></li>
 	
 		<p> </p>
 		
@@ -114,21 +114,21 @@ Page::Init();
 <ul>
 	<li class="question"><a id="fs1"></a><strong>I just started playing and don't know what to do!</strong></li>
 		
-		<p>There is a tutorial that will guide you through your first minutes of the game, Afterwards you're free to do whatever you like. You could become a proud Knight, a dedicated Priest, an industrious Crafter, a prudent Druid or a mysterious Mage.</p>
+		<p>There is a tutorial that will guide you through your first minutes of the game. Afterwards you're free to do whatever you like. You could become a proud Knight, a dedicated Priest, an industrious Crafter, a prudent Alchemist or a simple peasant.</p>
 
 	<li class="question"><a id="fs2"></a><strong>How can I attack and kill somebody?</strong></li>
 	
-		<p>You can either right click on a character or monster and choose "Attack", or you can hold down CTRL and left click the character or monster. Please respect that it is only allowed to attack other characters when there is a valid role play reason to do so. You should also give your target the chance to interact before and during a fight. Emotes that show your character drawing his or her weapon, or moving in an intimidating fashion to another character are appreciated.</p>
+		<p>You can right click on any other character or monster to attack. While Illarion is fully player versuc player (PvP), please respect that it is only allowed to attack other characters when there is a valid role play reason to do so. You should also give your target the chance to interact before and during a fight. Emotes that show your character drawing his or her weapon, or moving in an intimidating fashion to another character are appreciated.</p>
 		
 
 	<li class="question"><a id="fs3"></a><strong>How do I pick up items?</strong></li>
 		
-		<p>You may move the item by "Dragging and Dropping" it to a free place in your inventory. This process is similar to the way icons are moved on Windows/Linux/MacOS-desktops. Your inventory is composed of your character's clothing (which is the spaces on top of the image of a man), the belt (which is the six spaces under your character's clothing/armour) and a bag. The bag must be worn and can be opened to be able to use it. If you want to open the bag of your character, perform a double click or press 'B'.</p>
+		<p>Press 'P' to pick up all items in range. You may also move the item by "Dragging and Dropping" it to a free place in your inventory. The inventory can be opened by pressing 'I'. This process is similar to the way icons are moved on Windows/Linux/MacOS-desktops. Your inventory is composed of your character's clothing, the belt which is the six spaces under your character's clothing/armour and a bag. The bag must be worn and can be opened to be able to use it. If you want to open the bag of your character, perform a double click or press 'B'.</p>
 
 		
 	<li class="question"><a id="fs4"></a><strong>How do I use or equip items?</strong></li>
 	
-		<p>To equip an item, move the item you want to equip to a free slot in your inventory (the 'man' at the right of the screen). If you hold your mouse over a slot it shows you what you can put here. The slot has to be free to put something new in it. To do this, move the previous item somewhere else (e.g in the bag of your character). This works for weapons, armour or anything else you wear. If you want to use an item (e.g. eating an apple), right click and choose "Use". Alternatively you can hold SHIFT and click the item and release.</p>
+		<p>To equip an item, press 'I' to open the inventory move the item you want to equip to a free slot in your inventory. If you hold your mouse over a slot it shows you what you can put here. The slot has to be free to put something new in it. To do this, move the previous item somewhere else (e.g in the bag of your character). This works for weapons, armour or anything else you wear. If you want to use an item (e.g. eating an apple), perform a double click.</p>
 
 	<li class="question"><a id="fs5"></a><strong>How does crafting work in this game?</strong></li>
 		
@@ -144,7 +144,7 @@ Page::Init();
 		
 	<li class="question"><a id="fs8"></a><strong>Does the game have storage facilities?</strong></li>
 		
-		<p>Yes. The game uses a "depot" system. These are yellow storage boxes scattered around key areas of towns. To use them, move next to them and "open" them as you would a bag, you can then drag and drop any items you wish to store into the depot.</p>
+		<p>Yes. The game uses a "depot" system. These are yellow storage boxes scattered around key areas of towns. To use them, move next to them and "open" them with a double click. You can then drag and drop any items you wish to store into the depot. Note that you can access the depot system of one town only in that town.</p>
 		
 	<li class="question"><a id="fs9"></a><strong>I died! How do I resurrect? Is there a death penalty?</strong></li>
 		
@@ -160,7 +160,7 @@ Page::Init();
 <ul>
 	<li class="question"><a id="gc1"></a><strong>What is an RPG and what makes Illarion special?</strong></li>
 	
-		<p>Role playing games (RPG) are a popular game genre. Players take on the role of a fantasy character and are able to do many things that may be impossible in Real Life. Illarion takes the concept of Role playing further than other games, players are expected to immerse themselves completely into the life of their character. Whilst playing Illarion you cease to be John/Jane Smith from New York and instead take on the role of a fictional character by feeling, saying, and thinking what he or she would. Illarion is set in a medieval world with no electricity, cars, televisions or power tools. Whilst playing you are expected to refrain from mentioning anything Out Of Character (OOC).</p>
+		<p>Role playing games (RPG) are a popular game genre. Players take on the role of a fantasy character and are able to do many things that may be impossible in Real Life. Illarion takes the concept of Role playing further than other games. Players are expected to immerse themselves completely into the life of their character. Whilst playing Illarion you cease to be John/Jane Smith from New York and instead take on the role of a fictional character by feeling, saying, and thinking what he or she would. Illarion is set in a medieval world with no electricity, cars, televisions or power tools. Whilst playing you are expected to stay in your chosen role and keep Out Of Character (OOC) talk to a bare minimum.</p>
 		
 
 	<li class="question"><a id="gc2"></a><strong>What are CMs and GMs?</strong></li>
@@ -170,29 +170,28 @@ Page::Init();
 
 	<li class="question"><a id="gc3"></a><strong>Why do some of the players talk funny?</strong></li>
 		
-		<p>First of all, Illarion is played by players from all around the world. So it's no wonder that they don't speak the same language. English and German are the most common languages and it is a matter of courtesy to switch language to a language commonly understood when encountering other players. Special dialects could be applied as well. Most players try to make their characters sound medieval without using Old English as many people would struggle to understand. Abbreviations and chat slang are also against the rules. Take your time using capital letters and punctuation marks too.<BR> Bad Example: "ok hi wanna buy sword lol!!!111"<BR> Good Example: "Greetings, noble sir. May I draw your attention on this well crafted blade, a blade I'd like to offer to you for a reasonable price?"</p>
+		<p>Illarion is played by players from all around the world. So it's no wonder that they don't speak the same language. English and German are the most common languages and it is a matter of courtesy to switch language to a language commonly understood when encountering other players. Special dialects could be applied as well. Most players try to make their characters sound medieval without using Old English as many people would struggle to understand. Abbreviations and chat slang are frowned upon. Take your time using capital letters and punctuation marks too.<BR> Bad Example: "ok hi wanna buy sword lol!!!111"<BR> Good Example: "Greetings, noble sir. May I draw your attention on this well crafted blade, a blade I'd like to offer to you for a reasonable price?"</p>
 		
 
 	<li class="question"><a id="gc4"></a><strong>How do I find out an item's stats?</strong></li>
 
-		<p></p>	
+		<p>Hover.</p>	
 
 	<li class="question"><a id="gc5"></a><strong>Where can I see my skill levels?</strong></li>
 
-		<p></p>	
+		<p>Press 'C' to review your character's traits. The bars for each skill represent how far you are away from the next level up. Skills range from 1-100, from a novice to a true grandmaster.</p>	
 
 	<li class="question"><a id="gc6"></a><strong>How do I send global chat messages?</strong></li>
 	
-		 <p>We might grant powerful mages a special ability to communicate by telepathy, but nothing is planned yet. If you need to spread a message, you can always hire another player as a messenger or use the <a href="http://illarion.org/community/forums/index.php">Forums</a> to leave messages on the town walls.</p>
-		 
-
+		 <p>In order not to distract players from the actual game, the <a href="http://illarion.org/community/us_chat.php">global chat</a> can be found on the website. We might grant powerful mages a special ability to communicate by telepathy in the game. If you need to spread a message, you can always hire another player as a messenger or use the <a href="http://illarion.org/community/forums/index.php">Forums</a> to leave messages on the town walls.</p>
+		
 	<li class="question"><a id="gc7"></a><strong>The game took taxes from me! What is this all about?</strong></li>
 	
-		<p></p>	
+		<p>Gems.</p>	
 		
 	<li class="question"><a id="gc8"></a><strong>How can I find out my rank with my faction?</strong></li>
 	
-		<p></p>	
+		<p>Notaries.</p>	
 		
 	<li class="question"><a id="gc9"></a><strong>I've been reading about the special skill system of Illarion and Mental Capacity. What's that?</strong></li>
 	
@@ -212,19 +211,21 @@ Page::Init();
 		
 	<li class="question"><a id="co2"></a><strong>I found a bug or have a feature request. How do I report it?</strong></li>
 		
-		<p>The best way to report a bug or request features for the game or website is to use <a href="http://illarion.org/mantis/index.php" >Mantis</a> which is the Bug Reporting system used by the developers. Do not post bugs and feature requests in the forum as there is a chance they may not be seen.</p>
+		<p>The best way to report a bug or request features for the game or website is to use the <a href="http://illarion.org/mantis/index.php">Mantis Bugtracker</a> which is the Bug Reporting system used by the developers. You can also post bugs and feature requests in the <a href="http://illarion.org/community/forums/index.php">Forums</a> but there is a chance they may not be seen by the developers.</p>
 
 	<li class="question"><a id="co3"></a><strong>Can players write books, NPCs or even quests?</strong></li>
 	
-		<p></p>
+		<p>Yes.</p>
 		
 	<li class="question"><a id="co4"></a><strong>How can I contribute to Illarion? What skills are needed?</strong></li>
 	
-		<p>There are many ways in which you can contribute to Illarion. You could provide financial support, assist with the development of the game or help promote the game on external sites.</p>
+		<p>There are many ways in which you can contribute to Illarion. You could support the <a href="http://illarion.org/community/us_society.php">Illarion e.V. society</a>, assist with the <a href="http://illarion.org/development/us_content_tutorial.php">development of the game</a> or help to <a href="http://illarion.org/community/forums/viewforum.php?f=77">promote the game</a> on external sites. Also tell your friends about Illarion!</p>
+		
+		<p>If you have advanced skills in Java, C++, LUA, PHP or creating 3D graphics, you might want to contact one of the <a href="http://illarion.org/community/us_contact.php#4">lead developers</a> to get involved in the development of Illarion. Please note that Illarion is a non profit project and hence, no compensation can be paid for your work.
 		
 	<li class="question"><a id="co4"></a><strong>What features are planned for the future?</strong></li>
 	
-		<p></p>
+		<p>Milestones.</p>
 	
 </ul>
 
