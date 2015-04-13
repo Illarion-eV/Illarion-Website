5fc65385e0f683d31ebcf3b6f5fee88369076bb5
estralis <estralis@illarion.org>
2015-04-13 18:41:14 +0200

    #10898: Show quests on the start page

--- website/general/us_startpage.php
+++ website/general/us_startpage.php
@@ -48,6 +48,83 @@ resist the magic of this world.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
 
+<?php
+	$pgSQL =& Database::getPostgreSQL();
+
+	if(IllaUser::auth('quests'))
+	{
+		$query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
+		.PHP_EOL.' FROM homepage.quests'
+		.PHP_EOL.' WHERE q_status != 3'
+		.PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_us , q_title_de ) ASC'
+		;
+	}
+	else
+	{
+		$query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
+		.PHP_EOL.' FROM homepage.quests'
+		.PHP_EOL.' WHERE q_status != 3 AND ( q_type != 2 OR q_user_id = '.$pgSQL->Quote( IllaUser::$ID ).' )'
+		.PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_us , q_title_de ) ASC'
+		;
+	}
+
+	$pgSQL->setQuery( $query );
+	$quests = $pgSQL->loadAssocList();
+	if ( count($quests) > 0):
+?>
+
+<h2>Quests and Events</h2>
+
+<table class="quests">
+	<?php foreach($quests as $key=>$quest): ?>
+	<?php
+		if ($quest['q_type'] == 2)
+		{
+			$quest['q_status'] = 3;
+		}
+		if (!is_null( $quest['q_starttime'] ))
+		{
+		    $quest['q_starttime'] = strtotime( $quest['q_starttime'] );
+		}
+	?>
+	<tr>
+		<td class="title">
+			<a style="font-weight:bold;" href="<?php echo Page::getURL(); ?>/statistics/us_quests.php?id=<?php echo $quest['q_id']; ?>">
+				<?php echo ( is_null($quest['q_title_us']) ? $quest['q_title_de'] : $quest['q_title_us'] ); ?>
+			</a>
+		</td>
+		<td class="type"><?php echo ( $quest['q_type'] == 1 ? 'Official Quest' : 'Player Quest'); ?></td>
+		<td class="status<?php echo $quest['q_status']; ?>">
+			<?php
+				switch($quest['q_status'])
+				{
+					case 0:	echo 'Quest is planned'; break;
+					case 1:	echo 'Quest starts soon'; break;
+					case 2:	echo 'Quest takes place now'; break;
+					case 3:	echo 'Not activated'; break;
+				}
+				if (!is_null( $quest['q_starttime'] ))
+				{
+					echo '<br />', strftime( '%d. %B %Y %I:%M %P', IllaDateTime::TimestampWithOffset( $quest['q_starttime'] ) );
+				}
+			?>
+		</td>
+	</tr>
+	<?php endforeach; ?>
+</table>
+
+<?php endif; ?>
+
+<?php if (IllaUser::auth('quests')): ?>
+<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/us_quests_edit.php'">New quest</button></p>
+<?php elseif (IllaUser::loggedIn()): ?>
+<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/us_quests_edit.php'">New player quest</button></p>
+<?php endif; ?>
+
+<?php if ( count($quests) > 0): ?>
+<?php Page::insert_go_to_top_link(); ?>
+<?php endif; ?>
+
 <h1>News</h1>
 
 <?php
