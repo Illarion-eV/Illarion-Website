f8220d2577d6f052aaec300aa06bac58fcccddd6
estralis <estralis@illarion.org>
2015-04-13 19:22:18 +0200

    Show screenshots on the start page

--- website/general/de_startpage.php
+++ website/general/de_startpage.php
@@ -8,6 +8,10 @@
 	Page::addJavaScript( array( 'prototype', 'effects', 'lightwindow' ) );
 	Page::setXHTML();
 	Page::Init();
+	
+	$xmlC = new XmlC( 'UTF-8' );
+	$xml_file = file_get_contents( Page::getRootPath().'/illarion/screenshots_start.xml' );
+	$xmlC->Set_XML_data( $xml_file );
 ?>
 
 <h1>Willkommen in Illarion!</h1>
@@ -37,6 +41,23 @@ eines Tages die Seiten der Geschichtsbücher füllen.</p>
 <p>Illarion - Welche Rolle wirst du spielen?</p>
 
 <?php Page::insert_go_to_top_link(); ?>
+
+<h2>Screenshots</h2>
+
+<?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
+<div><a id="group<?php echo $currGroup->index; ?>"></a></div>
+<h2><?php echo $currGroup->gName; ?></h2>
+<?php foreach( $currGroup->screenshot as $index=>$currScreen ): ?>
+<div style="margin:3px;float:left;width:206px;height:116px;text-align:center;vertical-align:center;">
+	<a style="margin:auto;" href="<?php echo Page::getMediaURL(); ?>/screenshots/<?php echo $currScreen->filename; ?>" title="<?php echo $currScreen->gName; ?>" rel="Illarion Screenshots--<?php echo $currGroup->gName; ?>" class="lightwindow" onclick="return false;">
+		<img src="<?php echo Page::getMediaURL(); ?>/screenshots/preview/<?php echo $currScreen->filename; ?>" width="206" height="116" alt="Auf das Bild klicken um es in voller Größe zu sehen" />
+	</a>
+</div>
+<?php endforeach; ?>
+<div class="clr"></div>
+
+<?php Page::insert_go_to_top_link(); ?>
+<?php endforeach; ?>
 
 <?php
 	$pgSQL =& Database::getPostgreSQL();
--- website/general/us_startpage.php
+++ website/general/us_startpage.php
@@ -8,6 +8,11 @@
 	Page::addJavaScript( array( 'prototype', 'effects', 'lightwindow' ) );
 	Page::setXHTML();
 	Page::Init();
+	
+	$xmlC = new XmlC( 'UTF-8' );
+	$xml_file = file_get_contents( Page::getRootPath().'/illarion/screenshots_start.xml' );
+	$xmlC->Set_XML_data( $xml_file );
+	
 ?>
 
 <h1>Welcome to Illarion!</h1>
@@ -35,6 +40,25 @@ shape the world around you. Your actions will determine the events that will
 one day fill the pages of Illarion's history books.</p>
 
 <p>Illarion - What role will you play?</p>
+
+<?php Page::insert_go_to_top_link(); ?>
+
+<h2>Screenshots</h2>
+
+<?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
+<div><a id="group<?php echo $currGroup->index; ?>"></a></div>
+<h2><?php echo $currGroup->eName; ?></h2>
+<?php foreach( $currGroup->screenshot as $index=>$currScreen ): ?>
+<div style="margin:3px;float:left;width:206px;height:116px;text-align:center;vertical-align:center;">
+	<a style="margin:auto;" href="<?php echo Page::getMediaURL(); ?>/screenshots/<?php echo $currScreen->filename; ?>" title="<?php echo $currScreen->eName; ?>" rel="Illarion Screenshots--<?php echo $currGroup->eName; ?>" class="lightwindow" onclick="return false;">
+		<img src="<?php echo Page::getMediaURL(); ?>/screenshots/preview/<?php echo $currScreen->filename; ?>" width="206" height="116" alt="Click here to view the picture in full size" />
+	</a>
+</div>
+<?php endforeach; ?>
+<div class="clr"></div>
+
+<?php Page::insert_go_to_top_link(); ?>
+<?php endforeach; ?>
 
 <?php Page::insert_go_to_top_link(); ?>
 
