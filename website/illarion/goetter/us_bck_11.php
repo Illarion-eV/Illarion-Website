8e5fdd599ad746428a9b7fbd4ee68f2470780291
estralis <estralis@illarion.org>
2015-04-16 17:12:20 +0200

    Improving Ushi's page

--- website/illarion/goetter/us_bck_11.php
+++ website/illarion/goetter/us_bck_11.php
@@ -12,7 +12,8 @@
 
 <img align="right" vspace="5" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_4.gif" alt="The Consistent Bear"/>
 
-<p>Usharas is the god of the element Earth. Wise and calm, she is respected as mother of the world. Her restrained personality rejects wealth or any other kind of transitory values. If Ushara ever appears on Illarion, she choses the shape of an old dark-haired, dark-skinned woman dressed in dark colours. To foster her creation, she has put Oldra in charge, who is now being worshipped in new temples.</p>
+<?php cap(U); ?>
+<p>sharas is the god of the element Earth. Wise and calm, she is respected as mother of the world. Her restrained personality rejects wealth or any other kind of transitory values. If Ushara ever appears on Illarion, she choses the shape of an old dark-haired, dark-skinned woman dressed in dark colours. To foster her creation, she has put Oldra in charge, who is now being worshipped in new temples.</p>
 
 <h2>Followers</h2>
 <p>Her believers are very few, most are female old druids or old elves who met Ushara personally. She is very well respected among dwarven kind, but true admiration is rarely found. As all elder gods, she is worshipped at the Temple of the Five, east of Galmair. There is also the Chapel of the Five, east of the Kantabi Desert, where the name of the Five is held in honour. The people of Runewick are known to show more respect for the Five as the citizens of Cadomyr or Galmair - but any miner who finds a gem in the Malachite Mine might thank Ushara in a quick prayer for her creation.</p>
