bd4ea11c3059b95c44162eeaae53e79d0928e813
estralis <estralis@illarion.org>
2015-04-16 16:50:41 +0200

    Improving Ushi's page

--- website/illarion/goetter/us_bck_11.php
+++ website/illarion/goetter/us_bck_11.php
@@ -6,26 +6,24 @@
    include_header();
 ?>
 
-   <?php navBarTop( "us_bck_10.php","us_bck_10.php","us_bck_12.php" ); ?>
+<?php navBarTop( "us_bck_10.php","us_bck_10.php","us_bck_12.php" ); ?>
 
-   <h2>Ushara - Goddess of Earth. </h2>
+<h1>Ushara - Goddess of Earth.</h1>
 
-   <p>Element: Earth</p>
+<p>Usharas is the god of the element Earth. Wise and calm, she is respected as mother of the world. Her restrained personality rejects wealth or any other kind of transitory values. If Ushara ever appears on Illarion, she choses the shape of an old dark-haired, dark-skinned woman dressed in dark colours. To foster her creation, she has put Oldra in charge, who is now being worshipped in new temples.</p>
 
-   <p>Personality:<br />
-   mother image, wise, restrained, quiet, calm</p>
+<h2>Followers</h2>
+Her believers are very few, most are female old druids or old elves who met Ushara personally. She is very well respected among dwarven kind, but true admiration is rarely found. As all elder gods, she is worshipped at the Temple of the Five, east of Galmair. There is also the Chapel of the Five, east of the Kantabi Desert, where the name of the Five is held in honour. The people of Runewick are known to show more respect for the Five as the citizens of Cadomyr or Galmair - but any miner who finds a gem in the Malachite Mine might thank Ushara in a quick prayer for her creation.
+</p>
+   
+<h2>Ushos - Month of Sowing</h2>
 
-   <p>Figure:<br />
-   dressed in dark colours, dark-haired, dark-skinned; appears as a young or as an old woman</p>
+Ushos, named after the creator goddess Ushara, is the last month of spring. In Galmair they celebrate the "Crocodile Feast" from the 3rd to 17th Ushos. This is a huge, lasting street festival in which the people wear animal masks and consume large amounts of alcoholic drinks. Not too seldomly, this leads to the fact that the Galmair town guards have to rescue drunken or even drowned people out of the swamps and brooks. In Gynk, Galmair's mother city, there is even a rumor that the Crocodile Feast was named so because in fact it would be a feast for the crocodiles who lurk the canals of Gynk.
 
-   <p>Likes/Dislikes:<br />
-   rejects wealth or any other kind of transitory values</p>
+<h2>Sign of the Zodiac - The consistent Bear</h2>
 
-   <p>Followers:<br />
-   Her believers are very few, most are female old druids or old elves who met Ushara personally.
-   Since the leaving of the Elder Gods, she has put Oldra in charge, who is now being worshipped in
-   new temples.</p>
+Those born in the month of Ushara are quiet and introverted people. They love nature, and material things hold only little value to them. The are family people and constantly strive for harmony. Balanced persons, who are quiet and wise are blessed by Ushara. With wisdom, there comes confidence - Consistent bears do not hesitate to pursuit unconventional solutions. Rumor tells us that Archmage Elvaine Morgan of Runewick was actually born in the month of Ushos.
 
-   <?php navBarBottom( "us_bck_10.php", "us_bck_12.php" ); ?>
+<?php navBarBottom( "us_bck_10.php", "us_bck_12.php" ); ?>
 
 <?php include_footer(); ?>
