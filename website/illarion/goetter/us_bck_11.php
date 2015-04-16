22ba082dd4f4db2203d976e3a38f4814ad2fe616
estralis <estralis@illarion.org>
2015-04-16 16:57:24 +0200

    Improving Ushi's page

--- website/illarion/goetter/us_bck_11.php
+++ website/illarion/goetter/us_bck_11.php
@@ -10,7 +10,7 @@
 
 <h1>Ushara - Goddess of Earth</h1>
 
-<img align="left" vspace="5" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_4.gif" alt="The Consistent Bear"/>
+<img align="right" vspace="5" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_4.gif" alt="The Consistent Bear"/>
 
 <p>Usharas is the god of the element Earth. Wise and calm, she is respected as mother of the world. Her restrained personality rejects wealth or any other kind of transitory values. If Ushara ever appears on Illarion, she choses the shape of an old dark-haired, dark-skinned woman dressed in dark colours. To foster her creation, she has put Oldra in charge, who is now being worshipped in new temples.</p>
 
