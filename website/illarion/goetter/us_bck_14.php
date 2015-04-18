17ba3a528b8982552ceecbe3a304329772a846e0
estralis <estralis@illarion.org>
2015-04-18 09:13:54 +0200

    Improve Tanora's page

--- website/illarion/goetter/us_bck_14.php
+++ website/illarion/goetter/us_bck_14.php
@@ -8,43 +8,46 @@
 
    <?php navBarTop( "us_bck_13.php","us_bck_10.php","us_bck_15.php" ); ?>
 
-   <h2>Tanora - Goddess of Water</h2>
+<h1>Tanora - Goddess of Water</h1>
 
-   <p>Element: Water</p>
+<?php cap(T); ?>
+<p>anora is the goddess of the element Water. Sadness has overcome her with the years that passed by. Patiently, she awaits things to come only gods may understand. 
+Longing is associated with Tanora who is also called Zelphia by the lizardmen. She is the elder god who often walks on Illarion, maybe in search of something long lost to her.
+She appears as a lizardwoman or as an ageless mermaid of unadorned beauty.</p>
 
-   <p>Personality:<br />
-   wise, quiet, just, patient, sometimes sad</p>
+<h2>Followers</h2>
+<p>Tanora's religion dominates the lizard
+society. Many of her temples are located at the coast. In the native language of the lizardman she is called
+Zeshhel-pheey-arrr, almost unspeakable for the human tongue, and hence commonly spelled Zelphia. 
+The lizardmen always knew her under this name and members of other peoples might even think that Zelphia is in fact another goddess. 
+As all elder gods, he is worshipped at the Temple of the Five, east of Galmair. 
+There is also the Chapel of the Five, east of the Kantabi Desert, where the name of the Five is held in honour.
+As faith needs no house but one's mind and soul, she is also worshipped at remarkable sites at the coast of Illarion such as Cape Farewell, south of Cadomyr.</p>
 
-   <p>Figure:<br />
-   appears as a lizardwoman or as an ageless mermaid</p>
+<p>Most Zelphia believers are Lizards, sailors, or even elves. Many healers pray to her too. Her
+followers believe that she is a very powerful righteous goddess. Since most of her lizard
+priests follow ancient traditions, some might be quite fanatic believers who crush evil beings
+wherever they can, while many other followers are very peaceful in nature. Zelphia followers
+believe that the world and the magic are all one flow, and life springs from it where ever the
+goddess touches it. They think, that when they die, they will have to cross an endless sea. 
+When they have been good beings, the goddess may even send them a boat to bring them ashore. 
+If they have been bad, they must swim and get a stone pile over the sea. For
+every bad thing they did in life, a new stone is added to their pile. If the stone pile is too
+heavy, they have to wait for other swimmers who show mercy and take one or another stone with
+them. When they drown, the goddess will send them back to live as an animal that serves the mortals
+until the guilt is cleaned.</p>
 
-   <p>Likes/Dislikes:<br />
-   likes the unadorned beauty; the only elder god who still often walks on earth, maybe in search
-   of something long lost to her</p>
+<h2>Tanos - Month of Flood</h2>
 
-   <p>Followers:<br />
-   Tanora is the only still active Elder God in Illarion. Her religion dominates the lizard
-   society. Many of her temples lie under water, and most are located at the centre of the
-   lizardmen&#39;s under water towns. In the native language of the lizardman she is called
-   &quot;Zshhel- pheey- arrr&quot;, almost unspeakable for the human tongue, and lately often
-   called &quot;Zelphia&quot;. Some legends say that when the Elder Gods left, Tanora gave her
-   believers to Zelphia; but both Tanora and Zelphia are the same person. She herself had been
-   assuring this to several mortal beings. The lizardmen, however, always knew her as
-   Zelphia.<br />
-   Most Zelphia believers are Lizards, sailors, or even elves. Many healers pray to her too. Her
-   followers believe that she is a very powerful righteous goddess. Since most of her lizard
-   priests follow ancient traditions, some might be quite fanatic believers who crush evil beings
-   wherever they can, while many other followers are very pieceful in nature. Zelphia followers
-   believe that the world and the magic are all one flow, and life springs from it where ever the
-   goddess touches it. They think, that when they die, they will have to cross a sea with
-   unbreathable water (for lizards). When they have been good beings, the goddess may even send
-   them a boat, and when they have been bad, they must swim and get a stone pile over the sea. For
-   every bad thing they did in life, a new stone is added to their pile. If the stone pile is too
-   heavy, they have to wait for other swimmers who show mercy and take one or another stone with
-   them. When they carry too many stones, they will not be able to cross the sea:<br />
-   When they sink, the goddess will send them back to live as an animal that serves the mortals
-   until the guilt is cleaned.</p>
+<p>The second month of spring is named after the creator-goddess Tanora, who is supposed to be identical with the water-goddess "Zelphia" of the lizard people. A harbour feast is held in many coastal towns such as Runewick. The ice has usually completely melted by this time and ships can sail out again.</p>
 
-   <?php navBarBottom( "us_bck_13.php", "us_bck_15.php" ); ?>
+<h2>Sign of the Zodiac - The Dreaming Dolphin</h2>
+
+<img align="right" vspace="5" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_2.png" alt="The Dreaming Dolphin"/>
+
+<p>The dolphins oftenly appear to be melancholic. They tend to dream of goals they cannot reach and quarrel with their fate. They are also very calm, just, and possess good taste. They constantly seek the vicinity of their beloved and strive for attention. Dreaming Dolphins are not everyone's preferred companions, as the tend to be sad or worried. Those who are impatient will never understand the ways of the Dreaming Dolphins.</p>
+
+
+<?php navBarBottom( "us_bck_13.php", "us_bck_15.php" ); ?>
 
 <?php include_footer(); ?>
