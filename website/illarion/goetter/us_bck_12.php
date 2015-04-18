7e5d0796670e1334da5b4ff47eeea7a40c96fe8f
estralis <estralis@illarion.org>
2015-04-18 08:11:13 +0200

    Encoding

--- website/illarion/goetter/us_bck_12.php
+++ website/illarion/goetter/us_bck_12.php
@@ -11,24 +11,24 @@
 <h1>Br&aacute;gon - God of Fire  </h1>
 
 <?php cap(U); ?>
-<p>rágon is the god of the element Fire. Those who ever met him report about his imperious personality that does not tolerate a second opinion. Brágon is noble and just yet those who oppose his justice will face the most fearsome wrath among all the elder gods. Brágon is said to have sparked all desires in the souls of the mortals. Desires to pursuit what is good and what is just selfish. Brágon is associated with a certain preference for gold and he has an exquisite taste. If Brágon ever decides to interevene among the mortals, he appears as a giant with a red beard or as a dark-haired knight with his armor on fire - but he prefers to walks on Illarion in disguise.</p>
+<p>rÃ¡gon is the god of the element Fire. Those who ever met him report about his imperious personality that does not tolerate a second opinion. BrÃ¡gon is noble and just yet those who oppose his justice will face the most fearsome wrath among all the elder gods. BrÃ¡gon is said to have sparked all desires in the souls of the mortals. Desires to pursuit what is good and what is just selfish. BrÃ¡gon is associated with a certain preference for gold and he has an exquisite taste. If BrÃ¡gon ever decides to interevene among the mortals, he appears as a giant with a red beard or as a dark-haired knight with his armor on fire - but he prefers to walks on Illarion in disguise.</p>
 
 <h2>Followers</h2>
 <p>Br&aacute;gon is said to care least for Illarion of all elder gods. But sometimes, he still visits it, and he has built
 temples by himself - but there are few believers known to worship him. He has given Malach&iacute;n
 the power over many of his former worshippers. As all elder gods, she is worshipped at the Temple of the Five, east of Galmair.
-At the Chapel of the Five, east of the Kantabi Desert, Highpriest Bathelor praises his Lord in Brágon's sanctum.</p>
+At the Chapel of the Five, east of the Kantabi Desert, Highpriest Bathelor praises his Lord in BrÃ¡gon's sanctum.</p>
 
-<h2>Brás - Month of Immolation</h2>
+<h2>BrÃ¡s - Month of Immolation</h2>
 
-<p>Brás is named after Brágon, the third known creator god: he gave the sun and the fire to the world. The longest day in the year followed by the shortest night is the 24th of Brás. In this night, elves celebrate the feast of fire, in which on seas and rivers small, carven wood pieces swim and carry many candles. The humans have different way to celebrate this celebration. In Salkamar and Gynk this night is celebrated with a long grill feast, where all kinds of artists show they skills. In Albar just the nobles usually celebrate, but therefor their decadent feasts goes through the whole of Brás. It's not unusally that they have a special tax enforced in many Albarian regions to finance this.</p>
+<p>BrÃ¡s is named after BrÃ¡gon, the third known creator god: he gave the sun and the fire to the world. The longest day in the year followed by the shortest night is the 24th of BrÃ¡s. In this night, elves celebrate the feast of fire, in which on seas and rivers small, carven wood pieces swim and carry many candles. The humans have different way to celebrate this celebration. In Salkamar and Gynk this night is celebrated with a long grill feast, where all kinds of artists show they skills. In Albar just the nobles usually celebrate, but therefor their decadent feasts goes through the whole of BrÃ¡s. It's not unusally that they have a special tax enforced in many Albarian regions to finance this.</p>
 
-<h2>Sign of the Zodiac - The Noble Dragon</2>
+<h2>Sign of the Zodiac - The Noble Dragon</h2>
 
 <img align="right" vspace="5" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_7.gif" alt="The Noble Dragon"/>
 
 <p>Those who are born under the sign of the Dragon have a fiery temper. They are ambitious and intelligent. They usually reach highly-staked goals with ease, and thus tend to challenge their destiny too much.
-Their sense for justice and passion makes them good leaders. Noble Dragons tend to be commanding and impulsive - but with their elegance and grace, it is easy to fall to their charisma. It is not a secret that Queen Rosaline Edwards of Cadomyr is a true Noble Dragon, born in the month of Brágon.</p>
+Their sense for justice and passion makes them good leaders. Noble Dragons tend to be commanding and impulsive - but with their elegance and grace, it is easy to fall to their charisma. It is not a secret that Queen Rosaline Edwards of Cadomyr is a true Noble Dragon, born in the month of BrÃ¡gon.</p>
 
 <?php navBarBottom( "us_bck_11.php", "us_bck_13.php" ); ?>
 
