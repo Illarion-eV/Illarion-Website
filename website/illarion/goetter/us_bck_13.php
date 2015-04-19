a1c80ee104120d3dd044c433f9b0a19485a804bd
estralis <estralis@illarion.org>
2015-04-19 09:55:22 +0200

    Improve gods pages

--- website/illarion/goetter/us_bck_13.php
+++ website/illarion/goetter/us_bck_13.php
@@ -11,26 +11,28 @@
 <h1>Eldan - God of Spirit </h1>
 
 <?php cap(E); ?>
-<p>ldan is the god of the element Spirit. With his pensive attitude, one might conclude that his mind left the world and he sought refugee in the ivory tower of his thoughts. Wisdom is his mark, yet does he prefer to remain quiet when other shout out their foolishnesses. Eldan might appear as a mage, scholar, sometimes as a beggar or drunken philosopher; but always will he carry a wooden pole-staff. He avoids the the big cities or large crowds and prefers solitude.</p>
+<p>ldan is the god of the element Spirit. With his pensive attitude, one might conclude that his mind left the world and he sought refuge in the ivory tower of his thoughts. Wisdom is his mark, yet does he prefer to remain quiet when other shout out their foolishnesses. Eldan may appear as a wise mage, scholar, beggar or even a drunken philosopher but will always carry a wooden staff. He avoids the the big cities or large crowds and prefers solitude.</p>
 
 <h2>Followers</h2>
-<p>Eldan has never been too active amongst mortals. Very few people pray to him. As he was rarely
-heard or seen in public, only few know how to identify him. His believers do not talk much
-about him, and there are many rumors that he still wanders around in Illarion in disguise.
-People who follow him are often hermits, or people who love drinks and long philosophical talks.
-All claim to have met him personally, and instead of praying, they talk or discuss at forlorn places - or in Runewick.
-As all elder gods, he is worshipped at the Temple of the Five, east of Galmair. 
-There is also the Chapel of the Five, east of the Kantabi Desert, where the name of the Five is held in honour.</p>
+<p>Eldan has never been very active among mortals. Very few people pray to him. Because he was rarely seen in public, only a few know how to identify this god. 
+His worshipers are secretive and there are strange rumors of him wandering around Illarion in disguise.
+People who follow him are often hermits, or people who love their wine with long philosophical discussions. 
+These followers claim to have met Eldan personally, and instead of praying, they talk or discuss at forlorn places - or in Runewick.
+He is also worshipped at the Temple of the Five east of Galmair and the Chapel of the Five east of the Kantabi Desert.</p>
 
 <h2>Eldas - Month of Abrosia</h2>
 
-<p>Eldas was named after the creator god Eldan. This month is in all cultures a month of peace and comtemplation. People recollect on the things the live gave them and commemorate to the people who has less luck then they. Its not unusal to abstain from a beloved or inured thing to remember its worth during the time of Eldas. Halflings of Yewdale celebrat in the last week of Eldas the "Burrow Day". It bears in remembrance the deprivations of the resistance at the time of the fall of Firnismill. Especially in the countryside many Halflings do still celebrate these days in a quite traditional way. For the whole week they withdraw into the woods and spend the nights in self-made burrows or hollows. In this week they only feed upon the fruits of the forest. No fire is allowed to be lit. At the end of the week they return in a procession to their village. Thereby a great clamor and noise is performed and the participants of the procession are armed, thus symbolizing the recapture of the areas sized by the Orcs. Nowadays in the towns and with Halflings of the upper class it has become common practice to stay this week in tents erected in the garden or the inner courtyard of their houses. There sometimes even meat dishes are served but they are decorated in the form of fruits and mushrooms.</p>
+<p>Eldas was named after the creator god Eldan. This month is in all cultures a month of peace and comtemplation. 
+People reminisce about things that life gave them and remember the less fortunate in their prayers. 
+The more devout could even abstain from their beloved or some favorite item they possess in order to remember the worth of all during this time of Eldas.</p>
+
+<p>The Halflings of Yewdale celebrate the last week of Eldas called "Burrow Days" which signifies the resistance at the time of the fall of Firnismill. Traditionally they withdraw into the woods for the entire week and spend the nights in self-made burrows or hollows only eating fruits of the forest as no fire is allowed. At the end of the week they return in a procession to their village creating a great clamor and noise as all are armed, thus symbolizing the recapture of the areas seized by the Orcs. The more modern halflings that live in towns along with the upper class of the race modify this practice by erecting tents in their gardens or maybe an inner courtyard of their house. Sometimes even a meat dish may be served yet care is made to elaborately decorated it in the form of fruits and mushrooms.</p>
 
 <h2>Sign of the Zodiac - The Prudent Owl</h2>
 
 <img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_8.gif" alt="The Consistent Bear"/>
 
-<p>People of this sign seem to be rather innocuous. They are reasonable, love books, and avoid any possible aggression. Their vast discretion keeps them from interfering with the lives of other people. They hate large crowds of people and would never push into the foreground. Very cultured persons are known to be born under this sign, but don't expect them to be a partyer, as they are usually unsocial.</p>
+<p>People of this sign seem to be rather innocuous. They are reasonable, love books, and avoid any possible aggression. Their vast discretion keeps them from interfering with the lives of other people. They hate large crowds of people and would never push into the foreground. The cultured individual can be born under this sign and are usually introverted electing to avoid large gatherings.</p>
 
 <?php navBarBottom( "us_bck_12.php", "us_bck_14.php" ); ?>
 
