f568ba8674737eae9ca6d7af9047c7b6211bf16f
estralis <estralis@illarion.org>
2015-05-15 16:49:05 +0200

    #10537: Rework god pages

--- website/illarion/goetter/us_bck_23.php
+++ website/illarion/goetter/us_bck_23.php
@@ -10,33 +10,25 @@
 
 <h1>Sirani - Goddess of Love and Pleasure</h1>
 
-   <p>Prayed to for:<br />
-   Protection of people in love, or for love, or for attention of loved ones, advice for young
-   females</p>
+<?php cap(S); ?>
+<p>irani is the goddess of love and beauty. She is known to be friendly and hospitable, even sympathetic to those that need her comfort.  Due to this, Sirani is considered both generally peaceful and alluring. However, she is also known to be impulsive and easily overcome by her desires, overpowering all sense of reason. She dislikes rough, cruel or very selfish people and can fly into an intense rage at the deameaning treatment of women and use of derogatory terms. When she walks the land, she takes the form of a young and attractive woman of any race. Loving celebrations and joyful occasions, of even just life itself, she often joins parties and festivals. Pleased by displays of love and affection, she will sometimes attend the weddings of mortals as a sign of blessing to their marriage. Sirani is also known to have a soft spot for those who are lonely and will act to bring them companionship.</p>
 
-   <p>Personality:<br />
-   Friendly, intelligent, sympathetic, sometimes the desire for pleasure overpowers her sense of
-   reason</p>
+<h2>Followers</h2>
+<p>Followers of Sirani are plentiful and many young women in particular look to her as a mentor and guide, hoping she will bless their desires. Known to be a matchmaker, Sirani is prayed to by those seeking love, as well as those already blessed with love. Followers seek advice in love, protection of their beloved, and the secrets of her beauty. Her immense beauty and celeration of life makes it undoubtable that the Queen Rosaline of Cadomyr is blessed by Sirani. Royal weddings and unions of aristocratic families were once held at the Pearl of Sirani, deep in the Kantabi Desert, whilst those for commoners are held at the altar on the Tear of Sirani, part of the Kantabi Delta south of Cadomyr.</p>
 
-   <p>Figure:<br />
-   Appears disguised as young and very attractive female of any race. Often joins festivities and
-   partys for amusement. Sometimes cares for lonely people.</p>
+<p>Although many young girls are ridiculed for their early following of Sirani, some go on to become priestesses and bring much comfort and balance to the world. They dedicate themselves to tending all that is good in life. Most abhor war and celebrate peace, and will consider it to be a grave insult to bring a weapon into her temple. Sirani’s followers are often skilled in healing and along with the great hospitality that is offered at her temples, many of the needy find themselves drawn to seek food and shelter. When a follower of Sirani dies, Sirani looks into their heart and then kisses them. If they have led a good life, they will arrive in a blissful paradise. If they wronged, yet hold regrets for their past mistakes, Sirani’s kiss will enlighten them, and they will arrive in paradise forgiven. Those who are evil, however, and hold no regrets, will be cleansed by her kiss and return to the world as an innocent newborn, losing both their memories and their guilt to be given a fresh start to live a better life.</p>
+   
+<h2>Siros - Month of Love</h2>
 
-   <p>Likes/Dislikes:<br />
-   Likes parties, love, fun, live itself, making friends. Dislikes rough or very selfish people.
-   Dislikes the word &quot;bitch&quot; - can become very furious when someone uses it near her, or
-   when insulted.</p>
+<p>The month of Siros is named after Sirani, the goddess of love who is much celebrated by all races. All known cultures have loyalty and binding celebrations during the month of Siros and Sirani remains popular throughout Salkamar, Gynk and the Serinjahs. Mostly celebratory fairs or dances, they offer young couples the chance to meet and perhaps exchange marriage vows. There is no precise date for that festival but it is usually held around the 12th Siros. Often local officials arrange for a festival to be held in each village on a different day.</p>
 
-   <p>Followers:<br />
-   Many young females pray to her for advice in love or simple beauty tips. Most Sirani priests are
-   good healers. Her followers believe in peace and harmony, and most of them don&#39;t like
-   weapons or violence. In the Sirani temples, poor people are healed and given food, and a place
-   to sleep, and work if they can. Some of those become Sirani priests. The followers believe that,
-   when they die, Sirani will look into their heart, and then kiss them. If they have been good,
-   they will awake in paradise. If not, and they regret their doings, they will be forgiven and
-   enlightened by the Goddess kiss, and awake in paradise too. The evil ones who do not regret,
-   will be &quot;washed&quot; from their guilt by the kiss of the Goddess, and loose most of their
-   knowledge - and will awake as a newborn, unguilty child.</p>
+<p>Although all celebrate, there are differences among the races and religions. In elven tradition the male attaches branches from the Mevranno tree to the entrance of his love's house. The Mevranno tree lives in symbiosis with the climbing Levrenni plant which wraps around trees when still young and blooms beautifully; alone neither plant can thrive. If an elfess accepts the offer she braids a Levrenni stem around the Mevrannon branch. Among the halflings it is the females who take the first step, placing a girdle of flowers on top of their love's head. If the man does not take it off, the offer is considered accepted. Human traditions are rather more complicated with great differences between cultures and often no real offer but more an expression of love. Things that might count as honourable and endearing in the one village could be seen as highly insulting behaviour in another. Most common is for the man to ask his love for a dance, but not all dances are for love. Should the woman be endeared to the man she takes gives him a ribbon which was braided into her hair, as a traditional sign of affection. If the man binds the ribbon around his arm, he signals that he reciprocates her feeling. Orcs are known for a somewhat rougher expression of their affection. Following their tradition the male secretly visits the house of the orcess with his comrades and "steals" her. This thievery is ritualistic so that there are usually no injuries received on each side, unless the orcess has previously rejected her suitor. Generally the female is brought immediately to the male's house, whilst her family gather outside to try to "save" her. If the "kidnapped" orcess opens the door and asks the family to come inside before the house is stormed the marriage offer is accepted. In lizard culture most weddings are arranged by the parents and the children are no voice so it is not unheard of for the festival to be the first time the couple meets. Parents make their agreements before the celebration and then the couple meet at the male's house before everyone proceeds to the temple to witness the marriage vows in front of a priest. In case of the dwarves, the local smith has an important spiritual role in the ceremony beyond the simple crafting of rings. A couple that wants to marry is "linked" by the smith who crafts two rings, forged from the same piece of metal, that interlink to form one when put together. Traditionally these rings are hung above the couple's bed, signifying their linkage and the strength their union brings.</p>
+
+<h2>Sign of the Zodiac - The beautiful Butterfly</h2>
+
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_4.gif" alt="The beautiful Butterfly"/>
+
+<p>The amiable aura of beautiful Butterflies shines from afar. Those born during the month of Siros typically possess a lot of charm and force of attraction, especially to the opposite gender. They love beautiful festivals, enjoy freedom, and live life to its full extent. Awareness of their own attractiveness can on occasion give an impression of smugness but their amiability can usually overcome this. Only seldomly do they reach for the weapon, usually preferring to seek harmony and unison. Even a Butterfly can become angered, however, if you limit their freedom and prevent them from flying freely as they hold a great love for liberty. An emotional, moody Butterfly becomes somewhat less predictable lashing out with rash and unreasoned actions.</p>
 
 <?php navBarBottom( "us_bck_22.php", "us_bck_24.php" ); ?>
 
