27193a0c132205538b50c812a399efe3870fec81
estralis <estralis@illarion.org>
2015-05-15 13:11:24 +0200

    #10537: Rework god pages

--- website/illarion/goetter/us_bck_17.php
+++ website/illarion/goetter/us_bck_17.php
@@ -20,6 +20,7 @@
 <h2>Elos - Month of Magic</h2>
 
 <p>The first month in spring was named after Elara, goddess of wisdom. Traditionally, the birth rate is very high during Elos with many women of every race bearing their first child, depicting the dawn of a new year. The end of winter and the beginning of the new year is celebrated with great bonfires in every village and town, whilst houses are decorated with lamps and lanterns. These festivities culminate in the burning of a scarecrow that is named "Shadow Man" or "Dark Man". The Illumination Festival serves to banish the doom and ruin of Mas. The origin of the festival lies in the scripts of the Nokorakan Cycle, an attempt at a compendium of prophecies of the past where one can read:</p>
+
 <p><i>"Flee from the signs of the long night.<br>
 Beg on your knees for your immortal soul,<br>
 when the light denies itself in the crack of dawn.
