0f3f97c8a762211268814f63dca4e1bb06d260b3
estralis <estralis@illarion.org>
2015-05-15 17:08:04 +0200

    #10537: Rework god pages

--- website/illarion/goetter/us_bck_25.php
+++ website/illarion/goetter/us_bck_25.php
@@ -10,32 +10,23 @@
 
 <h1>Ronagan - God of Thieves and Shadows</h1>
 
-   <p>Prayed to for:<br />
-   Protection from authorities. For all people living in the shadows of life, thieves, assassins,
-   prostitutes, etc. Some merchants pray and donate money to him to avoid being robbed.</p>
+<?php cap(R); ?>
+<p>onagan is the god of the shadows, thieves, and assassins. Those who find themselves in the underbelly of life, may often lift a prayer to Ronagan for guidance and for well being. It is also not uncommon for merchants to leave coins at his altar, seeking protection from his very followers. Ronagan and his thieves live by a code all to their own, and many of his worshippers consider themselves to be brothers in arms. Ronagan is often friendly to those in the shadowy aspects of life, and good humored, but can be aggressive. Claims of Ronagan’s appearance are that of a handsome and charming dark haired man, in his thirties. Rumors have it that Ronagan has appeared to thieves in their times of need, guiding them to safety, granting tips, or outright granting the individual invisibility. His favorite pastime is pulling small pranks on those who represent the authorities, and seeing one of his worshippers succeed in their 'job'. Though Ronagan generally dislikes authority and law, he also dislikes betrayal.</p>
 
-   <p>Personality:<br />
-   Friendly to those roaming in his realm. But has dark moods and can be aggressive. Lives
-   according to his own laws. Good humored.</p>
+<h2>Followers</h2>
+<p>The thief in the night, the assassin with blade drawn, and the woman of mystery are the most common followers of Ronagan. Followers of Ronagan believe that he protects them and gives them guidance on their next mark, helping them steal from the rich and avoid the authorities. Ronagan is not only prayed to by thieves and spies however, but also by merchants and the poor. A merchant may pay protection money to Rongan at his altars, to seek protection from those who might consider him a mark. This money is often collected by his followers tending the altar, who may spread word to watch for the merchant. It is for this reason that Ronagan is a patron god of Galmair, as both a protector to the town, and their dealings in secrecy. Rumor has it that his altar can be found in the shadowy underground of Galmair.</p>
 
-   <p>Figure:<br />
-   Appears as a dark haired and clothed charming man in his thirties. Sometimes personally appears
-   in disguise and helps honorable thieves out of dangerous situations, or renders them invisible
-   when they need it most. Likes to appear as a second thief accidentally doing the same
-   &quot;run&quot;, giving the other one hints and tips.</p>
+<p>The poor pray to Ronagan to help them survive another night, to get a bit of bread, or that some mercy may be shown on them. Upon death, a follower of Ronagan is brought to the front of Ronagan’s castle. Through whatever means they can manage, they are to get to the main hall, the Den of Thieves. There, they will receive and enter paradise. If they are caught, they are granted multiple attempts. If they are particularly unskillful, Ronagan will return them to the land, where they are tasked with mentoring children in the street to steal, or to collect some item of wealth.</p>
+   
+<h2>Ronas - Month of Generosity</h2>
 
-   <p>Likes/Dislikes:<br />
-   Likes good jokes (plays jokes on those representing the authorities) and a &quot;job&quot; well
-   done. Dislikes betrayal and any kind of authority or law abiding system.</p>
+<p>The second month of Summer was named after Ronagan, who is known as god of shadows, secrecy and thieves. The plain folk of the Serinjah know Ronagan as god of freedom, fishermen and hunters, but also as god of secrecy and trickery. It is rumoured that there are frequent secret meetings of self-proclaimed Ronagan followers - thief guilds - in Galmair. But this rumor is unproven and has been told again and again over decades. </p>
 
-   <p>Followers:<br />
-   Ronagan&#39;s followers are mostly poor people who grow up on the streets, and most become
-   thiefs, spies or assassins. They believe that perhaps Ronagan will help them to steal and to
-   protect them from the protectors of the rich, the authorities. When they die, they will have to
-   break into Ronagan&#39;s Castle, and if they reach the main hall without being caught, they will
-   enter paradise. If they are catched, they can try again. If they have much bad luck or no
-   talent, Ronagan will give them a quest, mostly to help street children to learn how to become a
-   thief.</p>
+<h2>Sign of the Zodiac - The Artful Fox</h2>
+
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_6.png" alt="The Artful Fox"/>
+
+<p>Those who are born under the sign of the artful Fox tend to be quick-witted and have a well-developed sense of humour. They love freedom and will only releuctantly bow to laws or rules. They are often moody when they feel inhibited, and occasionally aggressive, so it is best to try to stay on their good side. Foxes will defend their ideals and strive to reach their goals, even though it may mean breaking laws or not complying with constraints of society. True libertines are commonly born during Ronas, but also avant-garde artists and jesters who test the boundaries of propriety. It is constantly speculated that Don Valerio Guiliani of Galmair was born in Ronas, but perhaps this rumour was spread with the intention to conceal that he is in fact a beautiful Butterfly.</p>
 
 <?php navBarBottom( "us_bck_24.php", "us_bck_26.php" ); ?>
 
