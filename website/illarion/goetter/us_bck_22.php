2db9db1517af2a4497c72562ee4cd73892c54bee
estralis <estralis@illarion.org>
2015-05-15 16:37:14 +0200

    #10537: Rework god pages

--- website/illarion/goetter/us_bck_22.php
+++ website/illarion/goetter/us_bck_22.php
@@ -10,30 +10,21 @@
 
 <h1>Irmorom - God of Trade and Craftsmanship</h1>
 
-   <p>Prayed to for:<br />
-   Good business, making of things, protection when working. God of most Blacksmiths, other
-   craftsmen, merchants and dwarves.</p>
+<?php cap(I); ?>
+<p>rmorom is the god of craft and trade. He is typically considered to be a fair and honourable god who remains neutral in most mortal matters, favouring the law to keep his work safe and allowing business and industry to thrive. Irmorom has a love for fine craftmanship and gold. He is good natured, sociable, talkative, and unlikely to refuse a good beer. It is rare that a thief would find favour from Irmorom, as he believes in a fair trade rather thatn the stealing of riches from others. It is said that his appearance is that of a grey haired dwarf with grey skin of an almost stone-like nature, or occasionally a man, adorned with a smithing apron and a mighty hammer. </p>
 
-   <p>Personality:<br />
-   Neutral, honorable, good natured, sociable, talkative</p>
+<h2>Followers</h2>
+<p>Those who follow Irmorom are usually characterised by being either a dwarf, craftsman, or merchant, or in some cases all three. He is prayed to for protection whilst labouring, fine workmanship, good business and wealth. Irmorom, considered by some to be the primary god of Galmair, is worshipped in a temple raised in the mountains on the western edge of town, close to the Well of Dreams; this is the only altar not found underground in Galmair. His followers often honour him with both prayers, and offering some of their finest and most valuable crafted goods at the altar. It is also not uncommon to see competition between his followers, to determine who is blessed more, through their crafts. Though the followers of Irmorom are usually dwarven, it is not uncommon to find other races, primarily craftsmen or merchants, worshipping Irmorom as well. Many will rally behind the symbolic hammer although few would see reason to distrust a gathering of jovial craftsmen or hardworking vendors. When a follower of Irmorom passes, it is said that they are brought to his mountain hall. There, they are to produce their finest piece of their work. If they impress Irmorom, they are sent on to paradise, and if they fail his test they are returned to the land, to try again to learn to create something of value.</p>
+   
+<h2>Irmas - Month of Trade</h2>
 
-   <p>Figure:<br />
-   Appears as rich dressed gray skinned and haired ( as if made of stone ) man or dwarf wearing a
-   hammer or a plain dressed man with an apron.</p>
+<p>Irmas is named after Irmorom, god of craftmanship and trade. From the 1st to the 8th Irmas, Galmair celebrates a great festival with parading craftsmen and merchants. In Cadomyr, a less raucous feast of craftsmen and merchants is also celebrated from the 1st to the 4th Irmas. Every big city across Salkamar, Gynk and Albar also has the largest market day of the year during Irmas. The dwarven kind of Sevenhill host a competition for crafters called The Golden Hammer, which starts on the 12th Irmas. Contests for different crafts are held over three to four days; the five days after are for celebrating and drinking while another good part of the month is necessary to sober up again.</p>
 
-   <p>Likes/Dislikes:<br />
-   Likes Law, Justice, gold and riches, business and craftsmanship. Dislikes thieves. Seldom
-   refuses a nice beer.</p>
+<h2>Sign of the Zodiac - The Busy Capricorn</h2>
 
-   <p>Followers:<br />
-   Most dwarves, businessmen and crafters, i.e. blacksmiths, pray to Irmorom. They believe that in
-   life, they will be rewarded for their doings with riches and money, and they pray for
-   enchantment of crafted things, for a good trade, riches, and things like this. When they die,
-   they will be brought to a big mountain, with an entrance and the dead ones hailing them. They
-   will be lead deep into that mountain, and there they must show Irmorom what they can make. If
-   they do a good job, they will be rewarded with a very good life in great wealth. If not, they
-   will be laughed at , and sent back to be reborn and learn more. If they impress the god, they
-   will be granted some power too.</p>
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_9.png" alt="The Busy Capricorn"/>
+
+<p>Industrious Goats are masters of trade and are usually successful merchants or craftsmen. Material goods and wealth are close to their hearts and everything has a value. They are talkative and sociable, making them excellent conversationalists in any company. With the banter of some merry Goats, a dark tavern is soon turned into a vivid, thriving establishment. They can be impulsive and impatient, but they are rarely unfair and honour a good barter. Nonetheless they are greedy by nature so don’t expect the bargaining to fall in your favour unless their impetuous nature leads them to overreach with a rare bad deal.</p>
 
 <?php navBarBottom( "us_bck_21.php", "us_bck_23.php" ); ?>
 
