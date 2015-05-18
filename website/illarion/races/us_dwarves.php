e509ac34d7897e560a8867964c3ebec7e65e8bd5
estralis <estralis@illarion.org>
2015-05-18 09:27:13 +0200

    #10905: Unify race names

---
+++ website/illarion/races/us_dwarves.php
@@ -0,0 +1,97 @@
+<?php
+	include_once ( $_SERVER['DOCUMENT_ROOT'] . '/shared/shared.php' );
+	create_header( 'Illarion - Background - Dwarfs',
+	'Races Illarions',
+	'Background, Races, History', '', 'menu', '', true );
+	include_header();
+?>
+
+<?php navBarTop( 'us_halflings.php','us_story.php','us_orcs.php' ); ?>
+
+<h1>Dwarfs</h1>
+
+<div class="menu">
+	<ul class="menu_top">
+		<li><a href="<?php echo $url; ?>/illarion/races/us_humans.php">Humans</a></li>
+		<li><a href="<?php echo $url; ?>/illarion/races/us_elfes.php">Elfes</a></li>
+		<li><a href="<?php echo $url; ?>/illarion/races/us_halflings.php">Halflings</a></li>
+		<li class="selected"><a href="<?php echo $url; ?>/illarion/races/us_dwarfs.php">Dwarfs</a></li>
+		<li><a href="<?php echo $url; ?>/illarion/races/us_orcs.php">Orcs</a></li>
+		<li><a href="<?php echo $url; ?>/illarion/races/us_lizards.php">Lizards</a></li>
+		<li class="end" />
+	</ul>
+</div>
+
+<table class="center"><tr><td><img src="<?php echo $url; ?>/shared/pics/races/dwarf_m.png" alt="male dwarf"/></td><td> </td><td><img src="<?php echo $url; ?>/shared/pics/races/dwarf_f.png" alt="female dwarf"/></td></tr></table>
+<?php cap(T); ?>
+<p>he dwarves are an old race. According to history, they were the third race who developed their own writing. In the very
+beginning it was based on pictograms – a simple and easy way to show the true meaning of the word. It was mainly used to
+record the history of the dwarfs and for religious purposes, thus being of a more sacrosanct nature and having no real
+value for daily life. But over the centuries these pictograms developed and became more simple, corresponding with the
+need of a script used for daily life. In the end the old dwarfish writing developed into a rune like writing; only
+few are still able to read or write the old pictograms nowadays.</p>
+
+<?php cap(A); ?>
+<p> dwarf is smaller than a human, but taller than a halfling. Dwarves have very broad shoulders and strong, stable
+bones. In their legends, Irmorom, the God of Craftsmanship, carved the first dwarves out of a rock that was
+given to him by Ushara, the Goddess of Earth.</p>
+
+<p>In early times, most dwarven clans dwelled in Mountain Valleys, where they discovered strange rocks that started
+to burn when exposed to fire – coal. They started to dig for it, and traded the coal with other races. Deeper and
+deeper into the earth dug the tunnels to get access to coal and in this process they found other resources : iron
+ore, jewels and even gold. Iron tools replaced the old copper ones after many years.</p>
+
+<?php cap(T); ?>
+<p>hen one day many orc tribes were forced to seek new hunting grounds. They travelled into dwarven territory. Several
+clashes and confrontations arose from this and the dwarven clans, one by one, decided to retreat into their mines and
+caves and to take their animals and resources with them. They easily fought off the orcs with their advanced weaponry
+and armour and went into the prepared and hidden mines during the winter. The orcs, who scouted the valleys in springtime,
+found abandoned cottages and villages without anything useful left. They thought that the dwarves, who had been very
+strong enemies, must have suffered from a mighty curse and vanished. These rumours were aided as every orcish scout who
+approached the mines or found an entrance by accident or superior scouting skills, vanished and was never seen again.</p>
+
+<?php cap(T); ?>
+<p>he orcish tribes left the "cursed valleys" and moved on to find new hunting grounds while the dwarves dug deeper and
+deeper into the mountains and discovered beautiful caves. They decided to stay in their caves and mines and built great
+halls and vast underground cities. Few dwarven houses were left on the surface, mainly for trading purposes or for farming
+natural resources.</p>
+
+<p>Dwarves are not very interested in magic, except for Rune crafting magic. They prefer good handwork, and fine crafted items.
+They love the shine of gold and the cold fire of beautiful jewels. They love the taste of a good dwarven, strong beer. Their
+towns and caves are lightened by different light sources: Huge, thick glass–structures that direct light from the mountains
+deep into their halls, torches and lamps, as well as different gemstones, crafted with strong runes to produce light and warmth
+where they are needed. The dwarven counterpart to a mage is a "Rune Crafter" or "Rune Master" – they carve magic instead
+of speaking it. This is a "safer", but less spontaneous form of magic, since deleted failures do not hurt the
+crafter, but only the item it was carved on.</p>
+
+<?php cap(T); ?>
+<p>he crafting of runes has different effects, triggered by actions, words, movements or thoughts. These items recharge with a
+certain, slow rate or can be "reloaded" by a rune crafter or mage. Some of the weaker effects work all the time without
+necessity to be "recharged", others draw their magical energies from the owner himself. These rune–magic has more practical
+than aggressive use.</p>
+
+<p>Dwarven Society is organized in clans. A dwarven city can consist of one or two bigger or several smaller clans. The leader
+of a dwarven clan is called "Clan Lord" or "High Lord", the leader of a dwarven town is called "King". Every Clan in a dwarven
+City has a Great hall, where the Clan Lord resides. The family buildings or caves, called "Family Halls" surround
+the Great Hall. Only the king's family quarters are connected with the Great Hall. Most dwarven clan halls have a separate
+shrine for the clan god. In most cases this would be Irmorom but also Ushara is highly praised, sometimes even Cherga is worshipped.</p>
+
+<?php cap(E); ?>
+<p>very dwarven clan consists of several families. A typical dwarven family consists of a man, his wife, and the children. A
+common number of children is two to four. When getting married, a new family is founded and they choose a new family name. This
+is the reason why dwarves call their children "Son of... and ... Son of... etc.", female dwarfs take their name from their
+mothers and grandmothers. The actual, self–chosen family name is not mentioned and never told to strangers. This is done to
+prevent curses against the family.</p>
+
+<?php cap(D); ?>
+<p>warven cemeteries lie underground, too. Buried deep beneath the surface, they are located outside, far below the towns of the
+living. Every Dwarven family has a family tomb. Most dwarven tombs are double graves, for the man and the woman. Every child
+that dies before getting married is buried in the tomb of its parents. Dwarves do not enclose golden gifts into the graves.
+Gold is an element of the living and has nothing to do with the dead. Dwarven grave gifts are often beautiful stone or jewel
+coins representing the good things a dwarf did in his life. At a special place in front of the grave the story of the dead one's
+life is carved and sometimes his whole life story. Dwarven warriors are often buried in stone armour with stone weapons
+and often a stone mask is placed upon their face – their spirits may protect the clan in times of great trouble.</p>
+
+<?php insert_go_to_top_link(); ?>
+
+<?php include_footer(); ?>
