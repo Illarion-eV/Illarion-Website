7d5810902ba71d2ccadb48f6e4a496384d0e56ed
Vilarion <vilarion@illarion.org>
2015-05-23 15:37:37 +0200

    Use system hostname on index pages
    Make local development easier by allowing access via http://localhost or http://illarion.local

--- website/index.php
+++ website/index.php
@@ -1,6 +1,6 @@
 <?php
     if (preg_match('/de/', $_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-        header('Location: http://illarion.org/general/de_startpage.php');
+        header('Location: http://' . $_SERVER['SERVER_NAME'] . '/general/de_startpage.php');
     } else {
-        header('Location: http://illarion.org/general/us_startpage.php');
+        header('Location: http://' . $_SERVER['SERVER_NAME'] . '/general/us_startpage.php');
     }
