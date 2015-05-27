bfeb2311bc20ad29e8ab2f7efe5c9a57f72072a5
estralis <estralis@illarion.org>
2015-05-27 17:31:46 +0200

    Update Eldan page

--- website/illarion/goetter/de_bck_13.php
+++ website/illarion/goetter/de_bck_13.php
@@ -6,34 +6,28 @@
    include_header();
 ?>
 
-   <?php navBarTop( "de_bck_12.php","de_bck_10.php","de_bck_14.php" ); ?>
+<?php navBarTop( "de_bck_12.php","de_bck_10.php","de_bck_14.php" ); ?>
 
-   <h2>Eldan - Gott des Geistes.</h2>
+<h1>Eldan - Gott des Geistes</h1>
 
-   <p>Element: Geist</p>
+<?php cap(E); ?>
+<p>Eldan ist der Gott des Elements Geist und wird als weisester von ihnen angesehen. Wegen seines nachdenklichen Gebahrens könnte man denken, dass er abgehoben ist und daraus schließen, dass sein Geist die Welt verlassen und er Zuflucht in der weltfremden Festung seiner eigenen Gedanken gesucht hat. Weisheit ist sein Erkennungszeichen und doch zieht er es vor still zu bleiben, wenn andere ihre Torheiten hinausschreien. Eldan mag als weiser Magier, Gelehrter, Bettler oder sogar betrunkener Philosoph erscheinen, doch er wird immer einen hölzernen Stab tragen. Er vermeidet größere Städte und Ansammlungen von Leuten und zieht friedvolle Abgeschiedenheit vor, sei es ein Ort des Lernens, oder eine entlegene Taverne.</p>
 
-   <p>Pers&ouml;nlichkeit:<br />
-   Er ist weise, von ruhiger Natur, &uuml;berlegt, weltfremd.</p>
+<h2>Anhänger</h2>
+<p>Eldan war nie sehr aktiv in die Geschicke der Sterblichen verwickelt. Sehr wenige beten zu ihm, da er nur äußerst selten in der Öffentlichkeit gesehen wurde und nur ein paar Leute wissen, wie dieser Gott zu erkennen ist. Seine Anhänger sind geheimnistuerisch und es gibt seltsame Gerüchte, laut derer er in Verkleidung durch Illarion streift. Seine Gefolgsleute sind oft Eremiten oder solche, die ihren Wein gerne bei langen, philosophischen Gesprächen genießen.  Diese Anhänger behaupten, Eldan persönlich getroffen zu haben und statt zu beten, sprechen oder diskutieren sie an einsamen Orten - oder in Runewick. Er wird auch im Tempel der Fünf im Osten Galmairs und in der Kapelle der Fünf im Osten der Kantabi Wüste verehrt. Viele derer, die traditionell Eldans Pfaden gefolgt wären, beten nun zu Elara.</p>
+   
+<h2>Eldas - Monat des Fastens</h2>
 
-   <p>Auftreten:<br />
-   Eldan erscheint als ein Magier, Gelehrter, oder manchmal auch als ein Bettler oder betrunkener
-   Philosoph, er tr&auml;gt einen h&ouml;lzernen Stab mit sich.</p>
+<p>Eldas wurde nach dem Schöpfungsgott Eldan benannt. Dieser Monat ist in allen Kulturen ein Monat des Friedens und der Nachdenklichkeit. Die Leute denken über die Dinge nach, die ihnen das Leben geschenkt hat und erinnern sich in ihren Gebeten derer, die weniger Glück haben. Die Frommeren üben sich im Monat der Enthaltsamkeit in Abstinenz ihres Liebhabers oder eines geliebten Gegenstands, den sie besitzen, um sich während dieser Tage des Eldas an den Wert aller Dinge zu erinnern.</p>
 
-   <p>Vorlieben/ Abneigungen:<br />
-   Er mag keine gro&szlig;en St&auml;dte oder gro&szlig;e Ansammlungen, er bevorzugt die Einsamkeit und Ruhe, aber
-   manchmal kann man Ihn in abgelegenen Tavernen finden.</p>
+<p>Die Halblinge aus Eibental feiern die letzte Woche des Eldas mit den sogenannten "Höhlentagen" und erinnern damit an den Widerstand während der Zeit des Niedergangs von Firnismühle. Traditionell ziehen sie sich für die Dauer der gesamten Woche in Wälder zurück und verbringen dort die Nächte in selbstgebauten Höhlen oder Gruben, sich ausschließlich von Früchten des Waldes ernährend, da jegliches Feuer verboten ist. Am Ende der Woche kehren sie in einer gerüsteten Prozession zurück und verursachen dabei großen Lärm, um die Wiedereinnahme der Gebiete zu symbolisieren, die einst von den Orks besetzt gewesen waren. Modernere Halblinge die in Städten leben, sowie die Oberschicht der Rasse, variieren diese Tradition, indem sie Zelte in ihren Gärten oder vielleicht einem Innenhof ihres Hauses aufstellen. Manchmal mag sogar ein Fleischgericht serviert werden, obwohl dieses dann mit viel Bedacht und großer Sorgfalt üppig mit Früchten und Pilzen dekoriert wird. </p>
 
-   <p>Anh&auml;nger:<br />
-   Eldan hat sich nie pers&ouml;nlich um die Sterblichen gek&uuml;mmert und nur wenige beten zu Ihm. Er wurde
-   nie in der &Ouml;ffentlichkeit gesehen oder erkannt und es gibt keine Erfahrung, woran man Ihn
-   erkennen kann.<br />
-   Seine wenigen Anh&auml;nger sprechen nicht viel &uuml;ber Ihren Gott, alte Geschichten besagen aber, dass
-   er immer noch in einsamen Gegenden Illarions umherwandert. Seine Anh&auml;nger sind meist Einsiedler
-   oder Leute, die nicht nein zu einem Schluck Wein und einem langen, philosophischen Gespr&auml;ch
-   sagen. Alle behaupten, Ihn schon einmal pers&ouml;nlich getroffenen zu haben, doch in Wirklichkeit
-   haben sie sich h&auml;ufig nur einer Illusion
-   hingegeben.</p>
+<h2>Sternzeichen - Die kluge Eule</h2>
 
-   <?php navBarBottom( "de_bck_12.php", "de_bck_14.php" ); ?>
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_8.png" alt="Die kluge Eule"/>
+
+<p>Leute die unter diesem Zeichen geboren wurden erscheinen recht harmlos. Sie sind vernünftig, lieben Bücher und vermeiden jedwede Aggression. Ihre erhebliche Diskretion hält sie davon ab, sich in die Leben Anderer einzumischen, selbst dann, wenn ihre Ansichten von großem Nutzen sein könnten. Besonnene Eulen sind oft ausweichend und würden sich niemals selbst in den Vordergrund rücken. Kultivierte Personen können unter diesem Zeichen geboren sein und sind oft introvertiert, lernen im Stillen und vermeiden große Ansammlungen von Leuten.</p>
+
+<?php navBarBottom( "de_bck_12.php", "de_bck_14.php" ); ?>
 
 <?php include_footer(); ?>
