560f52465fb15b95034e14fb9a46d3516b750d93
estralis <estralis@illarion.org>
2015-05-27 16:54:36 +0200

    Update Ushara page

--- website/illarion/goetter/de_bck_11.php
+++ website/illarion/goetter/de_bck_11.php
@@ -1,32 +1,31 @@
 <?php
    include_once ( $_SERVER['DOCUMENT_ROOT'] . "/shared/shared.php" );
    create_header( "Illarion - Hintergrund - Client",
-                  "Ushara -G&ouml;ttin der Erde.",
-                  "Hintergrund, G&ouml;tter, Geschichte" );
+                  "Ushara - Göttin der Erde.",
+                  "Hintergrund, Götter, Geschichte" );
    include_header();
 ?>
 
-   <?php navBarTop( "de_bck_10.php","de_bck_10.php","de_bck_12.php" ); ?>
+<?php navBarTop( "de_bck_10.php","de_bck_10.php","de_bck_12.php" ); ?>
 
-   <h2>Ushara - G&ouml;ttin der Erde.</h2>
+<h1>Ushara - Göttin der Erde</h1>
 
-   <p>Element: Erde</p>
+<?php cap(U); ?>
+<p>shara ist die Göttin des Elements Erde. Weise und still, wird sie als Mutter der Welt respektiert und ist von ruhigem Gebahren sowie sanfter Strenge. In ihrer zurückhaltenden Persönlichkeit lehnt sie Reichtum und andere Schätze von vergänglichem Wert ab. Sie bevorzugt jene, die schlicht und bescheiden sind und steht oft in enger Verbindung mit der Natur. Wenn Ushara auf Illarion erscheint, tut sie dies in der Gestalt einer betagten, schwarzhaarigen, dunkelhäutigen Frau in dunkler Kleidung.</p>
 
-   <p>Pers&ouml;nlichkeit:<br />
-   Sie ist sehr weise, ruhig, still, zur&uuml;ckhaltend. Sie vertritt das Mutter Bild.</p>
+<h2>Anhänger</h2>
+<p>Ushara hat nur wenige Anhänger, die meisten davon alte weibliche Druiden oder Elfen, die sie persönlich getroffen haben. Sie wird sehr von den Zwergen respektiert, aber wahre Verehrung ist nur selten vorzufinden. Wie die anderen Alten Götter, wird auch sie im Tempel der Fünf verehrt, östlich von Galmair. Außerdem gibt es noch die Kapelle der Fünf im Osten der Kantabi Wüste, wo der Name der Fünf in Ehren gehalten wird. Abgelegenen Orte wie Usharas Felsspitze, tief im Wachwald, dienen als seltenes Zeugnis ihrer wahren Gefolgschaft. Die Bürger Runewicks sind bekannt dafür, mehr Repsekt für die Fünf zu zeigen als die Cadomyrs oder Galmairs, aber jeder Minenarbeiter, der beim Schürfen in der Malachitmine einen Edelstein findet, mag schnell ein Dankesgebet für Ushara und ihre Schöpfung zur Hand haben. Um ihre Schöpfung zu pflegen, ermutigte sie ihre Anhänger dazu, in den neuen Tempeln zu Oldra zu beten.</p>
+   
+<h2>Ushos - Monat der Aussaat</h2>
 
-   <p>Auftreten:<br />
-   Sie erscheint als eine dunkelhaarige, dunkelh&auml;utige, dunkel gekleidete junge oder alte Frau.</p>
+<p>Ushos, benannt nach der Schöpfungsgöttin Ushara, ist der letzte Monat im Frühling und bekannt als Monat der Aussaat. Bauern pflanzen die meisten ihr Samen zu dieser Zeit des Jahres und die Felder rund um Runewick und Galmair sind bis Ende des Monats frisch gepfllügt und die Saaten ausgesäh. In Galmair wird vom 3. bis 17. Ushos das "Krokodilfest" gefeiert. Dies ist ein gewaltiges, andauerndes Straßenfest, bei dem die Leute Tiermasken tragen und große Mengen alkoholischer Getränke zu sich nehmen. Regelmäßig müssen Wachen Betrunkene oder sogar Ertrunkene aus den Sümpfen und Bächen rund um Galmair retten. In Gynk, wo Don Valerio Guilanni geboren wurde, gibt es sogar ein Gerücht, wonach das Krokodilfest seinen Namen erhalten hat, weil es ein Festmahl für die Krokodile ist, welche in den trägen Wasserstraßen leben.</p>
 
-   <p>Vorlieben/ Abneigungen:<br />
-   Ushara lehnt Wohlstand sowie alle anderen verg&auml;nglichen Dinge ab.</p>
+<h2>Sternzeichen - Der beständige Bär</h2>
 
-   <p>Anh&auml;nger:<br />
-   Die G&ouml;ttin hat nur wenige Anh&auml;nger, die meisten sind weibliche alte Druiden oder Elfen, welche
-   sie noch pers&ouml;nlich getroffen haben. Als die alten G&ouml;tter Illarion verlassen haben, hat sie
-   Oldra als Ihre Vertreterin ernannt, welche in neuen Tempeln an Ihrer statt verehrt
-   wird.</p>
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_4.gif" alt="Der beständige Bär"/>
 
-   <?php navBarBottom( "de_bck_10.php", "de_bck_12.php" ); ?>
+<p>Diejenigen, die im Monat Usharas geboren wurden sind stille, introvertierte Menschen. Sie lieben die Natur und haben wenig Sinn für materielle Dinge. Sie sind Familienmenschen und streben stets nach Harmonie. Ausgewogene Persönlichkeiten, die still und weise sind, sind von Ushara gesegnet. Mit Weisheit kommt Selbstbewusstsein und manchmal Arroganz. Unter dem Zeichen des Bären geborene zögern oft nicht, ungewöhnliche Lösungsansätze zu verfolgen und können stur auf ihren Weg beharren, während sich die Welt um sie herum verändert. Gerüchten zufolge soll Erzmagier Elvaine Morgan von Runewick im Monat Ushos geboren sein.</p>
+
+<?php navBarBottom( "de_bck_10.php", "de_bck_12.php" ); ?>
 
 <?php include_footer(); ?>
