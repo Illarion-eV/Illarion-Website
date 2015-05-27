be4ed6ea37fb02972a2d0e5eb6fc46cb22193c9e
estralis <estralis@illarion.org>
2015-05-27 17:26:48 +0200

    Update Brágon page

--- website/illarion/goetter/de_bck_12.php
+++ website/illarion/goetter/de_bck_12.php
@@ -6,29 +6,26 @@
    include_header();
 ?>
 
-   <?php navBarTop( "de_bck_11.php","de_bck_10.php","de_bck_13.php" ); ?>
+<?php navBarTop( "de_bck_11.php","de_bck_10.php","de_bck_13.php" ); ?>
 
-   <h2>Br&aacute;gon - Gott des Feuers.</h2>
+<h1>Brágon - Gott des Feuers</h1>
 
-   <p>Element: Feuer</p>
+<?php cap(U); ?>
+<p>rágon ist der Gott des Elements Feuer, der die Sonne erschuf und der Welt das Feuer schenkte. Die, die ihn getroffen haben, sprechen von seiner einnehmenden Persönlichkeit, die keine zweite Meinung neben seiner toleriert. Brágon ist stark, edel und gerecht, doch die, die sich seinem Urteil widersetzen, werden auf den furchterregendsten Zorn unter all den Alten Göttern treffen. Ihm wird nachgesagt, alle Triebe in den Seelen der Sterblichen ausgelöst zu haben, nicht nur die guten, sondern auch die selbstsüchtigen. Brágon wird mit einer gewissen Vorliebe für Gold in Verbindung gebracht und hat einen auserlesenen Geschmack. Wenn Brágon entscheidet, sich in die Belange der Sterblichen einzumischen, mag er als Riese mit brennendem Bart, oder als dunkelhaariger Ritter, dessen Rüstung in Flammen steht erscheinen, obwohl er es für gewöhnlich vorzieht, unerkannt durch Illarion zu ziehen.</p>
 
-   <p>Pers&ouml;nlichkeit:<br />
-   Er ist sehr weise, stark, gebieterisch, edel, und stattlich.</p>
+<h2>Anhänger</h2>
+<p>Brágon wird nachgesagt, dass er sich von all den Alten Göttern am wenigsten für Illarion interessiert. Manchmal jedoch, stattet er dennoch einen Besuch ab und hat bereits persönlich Tempel errichtet. Nur wenige Anhänger sind bekannt und einem Wesen mit so anspruchsvollem Geschmack eine halbherzige Opfergabe darzubringen, wird nur Zorn zur Folge haben. Er hat Malachin die Macht über viele seiner ehemaligen Gefolgsleute übertragen. Wie alle Alten Götter, wird auch er im Tempel der Fünf im Osten Galmairs verehrt. In der Kapelle der Fünf im Osten der Kantabi Wüste, betet Hohepriester Bathelor seinen Herrn in Brágons Allerheiligstem. Die Leute beten oft dafür, dass die brennende Sonne, die Brágon erschaffen hat, während ihren Wanderungen durch die Wüste gnädig zu ihnen sein möge.</p>
+   
+<h2>Brás - Monat der Opferung</h2>
 
-   <p>Auftreten:<br />
-   Br&aacute;gon erscheint als ein Riese mit einem brennendem Bart, oder als ein dunkelhaariger Ritter mit
-   einer R&uuml;stung aus Feuer, manchmal wandelt er unerkannt unter den Sterblichen.</p>
+<p>Brás ist nach Brágon benannt, dem dritten bekannten Schöpfungsgott, der die Sonne erschuf und der Welt das Feuer schenkte. Einst als Monat der Opferung bekannt, werden Brandopfer heute nur noch selten in Illarion dargebracht. Der längste Tag des Jahres, gefolgt von der kürzesten Nacht, fällt auf den 24. Brás. Während dieser Nacht feiern die Elfen das Fest des Feuers und kleine, geschnitzte Holzstücke, welche zahlreiche Kerzen tragen, können auf vielen Seen und Flüssen schwimmend beobachtet werden. Menschen feiern den Monat auf etwas andere Art, mit langen Festen in Salkamar und Gynk, während derer alle möglichen Künstler ihre Fertigkeiten vorführen. In Albar neigen nur die Adeligen dazu zu feiern, indem sie dekadente Feste ausrichten, die den ganzen Monat Brás andauern. Es ist in vielen Regionen Albars nicht unüblich, dass Sondersteuern erhoben werden, um dies zu finanzieren.</p>
 
-   <p>Vorlieben/ Abneigungen:<br />
-   Er hat einen erlesenen Geschmack und liebt Gold.</p>
+<h2>Sternzeichen - Der edle Drache</h2>
 
-   <p>Anh&auml;nger:<br />
-   Br&aacute;gon war der Erste der Illarion verlassen hat, doch von Zeit zu Zeit erscheint er in den
-   Landen und sieht nach dem Tempel, den er mit eigenen H&auml;nden erbaut hat. Dort gibt es allerdings
-   keine Priester oder Anh&auml;nger. Manchmal segnet er Sterbliche, die dort Opfergaben f&uuml;r Ihn
-   darreichen. Als er Illarion verlie&szlig;, &uuml;bergab er an Malach&iacute;n die Macht &uuml;ber seine
-   Anh&auml;nger.</p>
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_7.png" alt="Der edle Drache"/>
 
-   <?php navBarBottom( "de_bck_11.php", "de_bck_13.php" ); ?>
+<p>Die unter dem Zeichen des Drachen geborenen haben ein feuriges Temperament und können zu Wutausbrüchen neigen. Sie sind ehrgeizig und intelligent. Normalerweise erreichen sie angestrebte Ziele recht einfach, wodurch sie dazu neigen, ihr Schicksal zu sehr herauszufordern. Ihr Sinn für Gerechtigkeit und ihre Leidenschaft machen sie zu guten Anführern. Noble Drachen tendieren dazu, gebieterisch und impulsiv zu sein, aber durch ihre Eleganz und Grazie ist es leicht, ihrer Ausstrahlung zu verfallen. Es ist kein Geheimnis, dass Königin Rosaline Edwards von Cadomyr ein wahrer nobler Drache ist, geboren im Monat des Brágon.</p>
+
+<?php navBarBottom( "de_bck_11.php", "de_bck_13.php" ); ?>
 
 <?php include_footer(); ?>
