2ee390ad5a573b5115a68ef4c95cba9f8a1fc59d
estralis <estralis@illarion.org>
2015-05-27 18:27:43 +0200

    Update Oldra page

--- website/illarion/goetter/de_bck_19.php
+++ website/illarion/goetter/de_bck_19.php
@@ -26,7 +26,7 @@
 
 <h2>Sternzeichen - Die liebenswerte Taube</h2>
 
-<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_13.png" alt="Die liebenswerte Taube"/>
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_12.png" alt="Die liebenswerte Taube"/>
 
 <p>Die unter dem Zeichen der barmherzigen Taube geborene, sind besonders liebenswürdig. Ihr mütterlicher und mitfühlender Charakter verleitet viele dazu, ihre Sorgen mit ihnen zu teilen. Sie lieben es, Dingen beim Wachsen zuzusehen, sind voller Vitalität, standfest und würdevoll. Eine Taube teilt alles, was sie besitzt mit Bedürftigen, aber ihre Naivität lässt sie oft schutzlos gegenüber Leuten sein, die sie ausnutzen möchten. Trotz all des guten Willens in der Welt, führt ein Mangel an selbstkritischer Erkenntnis bei vielen Tauben dazu, dass sie ungefragt ihren Rat geben und andere fast mit ihrer Fürsorge erdrücken. Manchmal könnte es das beste sein, Stillschweigen zu bewahren und anderen zu erlauben, selbst zu wachsen und ihre eigenen Fehler im Leben zu machen. </p>
 
