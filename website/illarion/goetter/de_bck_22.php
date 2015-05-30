df82c7a88ecc51179f18e75afb53d5c0d9ba4b34
estralis <estralis@illarion.org>
2015-05-30 19:26:45 +0200

    Update Irmorom page

--- website/illarion/goetter/de_bck_22.php
+++ website/illarion/goetter/de_bck_22.php
@@ -6,37 +6,28 @@
    include_header();
 ?>
 
-   <?php navBarTop( "de_bck_21.php","de_bck_10.php","de_bck_23.php" ); ?>
+<?php navBarTop( "de_bck_21.php","de_bck_10.php","de_bck_23.php" ); ?>
 
-   <h2>Irmorom - Gott des Handels und des Handwerks.</h2>
+<h1>Irmorom - Gott des Handels und des Handwerks</h1>
 
-   <p>Wird angebetet um:<br />
-   Einen guten Handelsabschluss, die Herstellung von Waren, Schutz vor Arbeitsunf&auml;llen. Er ist der
-   Gott der meisten Schmiede, Handwerkern, H&auml;ndlern und Zwergen.</p>
+<?php cap(M); ?>
+<p>rmorom ist der Gott des Handwerks und Handels. Er wird typischerweise als gerechter und ehrhafter Gott angesehen, der in den meisten Angelegenheiten der Sterblichen eine neutrale Position einnimmt und sich auf das Gesetz verlässt, um seine Arbeit zu schützen und sicherzustellen, dass die Geschäfte laufen und die Wirtschaft wachsen kann. Irmorom hat eine große Vorliebe für Handwerkskunst und Gold. Er besitzt eine positive Persönlichkeit, befindet sich gerne in Gesellschaft und plaudert und lehnt fast nie ein gutes Bier ab. Es kommt nur selten vor, dass ein Dieb Irmoroms Gefallen findet, da er an fairen Handel glaubt und nicht an das Stehlen von Reichtümern Anderer. Es wird gemunkelt, dass er meist in der Gestalt eines grauhaarigen Zwerges mit gräulicher, fast steinartiger Haut erscheint, manchmal als Mann, gekleidet in einer Schmiedeschürze und einem mächtigen Hammer.</p>
 
-   <p>Pers&ouml;nlichkeit:<br />
-   Neutral, ehrenvoll, gutm&uuml;tig, gespr&auml;chig, gesellig.</p>
+<h2>Anhänger</h2>
+<p>Diejenigen, die Irmorom dienen, sind meistens als Zwerg, Handwerker, oder Händler zu beschreiben, oder fallen in manchen Fällen gar unter alle drei dieser Kategorien. Sie beten zu Irmorom für Schutz während der Arbeit, feine Handwerkskunst, gute Geschäfte und Reichtum. Irmorom, der von manchen als der primäre Gott Galmairs angesehen wird, wird in einem Tempel angebetet, der sich in den Bergen am westlichen Stadtrand befindet, in der Nähe des Brunnens der Träume, dem einzigen Altar in Galmair, der sich nicht im Untergrund befindet.</p>
 
-   <p>Auftreten:<br />
-   Er erscheint als ein prunkvoll gekleideter, grauhaariger Mann oder Zwerg mit grauer Haut (als
-   w&auml;re er aus Stein), er tr&auml;gt einen Hammer bei sich oder ist ein einfach gekleideter Mann mit
-   Arbeitssch&uuml;rze.</p>
+<p>Seine Anhänger ehren ihn oft sowohl mit Gebeten, wie auch mit Opfergaben in Form ihrer besten und wertvollsten Handwerksstücke an seinem Altar. Außerdem kommt es nicht selten vor, dass man zwei seiner Jünger dabei beobachten kann, wie sie einen handwerklichen Wettstreit führen, um festzustellen, wer mehr von Irmorom gesegnet ist. Obwohl die Anhänger Imoroms meistens Zwerge sind, ist es nicht ungewöhnlich, unter anderen Rassen Personen zu finden, die ihn anbeten, vor allem Handwerker oder Händler. Viele sammeln sich unter dem symbolischen Hammer, obwohl wenige einen Grund dafür sehen würden, einer Ansammlung heiterer Handwerker oder hart arbeitender Händler zu mistrauen. Wenn ein Anhänger Irmoroms stirbt, wird gesagt, dass dieser zu Irmoroms Hallen in den Bergen gebracht wird. Dort müssen sie ihr bestes Handwerksprodukt herstellen und wenn sie es schaffen, Irmorom zu beeindrucken, schickt er sie ins Paradis. Wenn ihnen dies nicht gelingt, werden sie wieder ins Leben zurückgeschickt, um abermals zu versuchen zu lernen und etwas von Wert herzustellen.</p>
 
-   <p>Vorlieben/ Abneigungen:<br />
-   Er mag Gesetze und die Gerechtigkeit, er liebt Gold und Reicht&uuml;mer, den Handel und das Handwerk.
-   Er hasst Diebe und er schl&auml;gt niemals ein gutes Bier aus.</p>
+<h2>Irmas - Monat des Handwerks</h2>
 
-   <p>Anh&auml;nger:<br />
-   Die meisten seiner Anh&auml;nger sind Zwerge, H&auml;ndler, Handwerker und Schmiede. Sie glauben, wenn sie
-   Irmoron anbeten, gelangen sie zu Wohlstand und Reichtum. Angeblich gew&auml;hrt er auch Kr&auml;fte, um
-   Waffen und Gegenst&auml;nde zu verzaubern.<br />
-   Seine Anh&auml;nger glauben, wenn sie sterben, werden sie zu einem m&auml;chtigen Berg gebracht, wo sie
-   von bereits Verstorbenen empfangen werden und in das Innere des Berges geleitet werden. Dort
-   m&uuml;ssen sie sich vor ihrem Gott beweisen und Ihm ihr K&ouml;nnen demonstrieren. Wenn sie gute Arbeit
-   leisten, werden sie belohnt mit einem Leben in gro&szlig;em Wohlstand. Wenn sie schlechte Arbeit
-   vorbringen, werden sie verh&ouml;hnt und wieder neu geboren, damit sie Ihre Ausbildung beenden. Nur
-   wenige schaffen es, Irmoron zu beeindrucken, doch jenen wird zus&auml;tzliche Macht gew&auml;hrt.</p>
+<p>Der Monat Irmas ist nach Irmorom benannt, dem Gott des Handwerks und Handels. Vom 1. bis 8. Irmas feiert Galmair ein großes Fest mit einer Parade der Handwerker und Händler. In Cadomyr wird ebenfalls ein weniger wildes Fest der Handwerker und Händler vom 1. bis 4. Irmas geifeiert. Jede größere Stadt in Salkamar, Gynk und Albar halten außerdem an irgendeinem Tag im Irmas ihren größten Markttag des Jahres ab. Die Zwerge von Sevenhill richten einen Wettbewerb namens ""Der goldene Hammer"" aus, der am 12. Irmas beginnt. Wettbewerbe für verschiedene Handwerke werden über drei oder vier Tage abgehalten, während die fünf Tage danach mit Feiern und Trinken verbracht werden und ein guter Teil des Monats benötigt wird, um wieder nüchtern zu werden.</p>
 
-   <?php navBarBottom( "de_bck_21.php", "de_bck_23.php" ); ?>
+<h2>Sternzeichen - Der fleißige Steinbock</h2>
+
+<img align="right" vspace="0" hspace="5" src="<?php echo $url; ?>/illarion/calendar/images/image_9.png" alt="Der fleißige Steinbock"/>
+
+<p>Die unter dem Zeichen der Ziege geborenen, sind Meister des Feilschens und meistens erfolgreiche Händler oder Handwerker. Materielle Güter und Reichtum sind ihnen wichtig und alles hat einen bestimmten Wert. Sie reden gerne und sind in Gesellschaft, was sie zu wunderbaren Gesprächspartnern in jeglicher Gegenwart macht. Durch das Feilschen einiger fröhlicher Ziegen wird ein düsteres Wirtshaus schnell zu einer lebhaften, blühenden Einrichtung. Sie können impulsiv und ungeduldig sein, sind aber nur selten unfair und legen Wert auf gutes Feilschen. Dennoch sind sie von Natur aus geizig, also sollte man nicht erwarten, dass das Feilschen zu eigenen Gunsten endet, es sei denn, ihre ungestüme Persönlichkeit verleitet sie zu einem seltenen, schlechten Geschäft.</p>
+
+<?php navBarBottom( "de_bck_21.php", "de_bck_23.php" ); ?>
 
 <?php include_footer(); ?>
