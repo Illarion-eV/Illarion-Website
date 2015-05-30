a7a3e25b13c0c23e491080bd2909f641c338874b
estralis <estralis@illarion.org>
2015-05-30 19:28:55 +0200

    Update Irmorom page

--- website/illarion/goetter/de_bck_22.php
+++ website/illarion/goetter/de_bck_22.php
@@ -20,7 +20,7 @@
 
 <h2>Irmas - Monat des Handwerks</h2>
 
-<p>Der Monat Irmas ist nach Irmorom benannt, dem Gott des Handwerks und Handels. Vom 1. bis 8. Irmas feiert Galmair ein großes Fest mit einer Parade der Handwerker und Händler. In Cadomyr wird ebenfalls ein weniger wildes Fest der Handwerker und Händler vom 1. bis 4. Irmas geifeiert. Jede größere Stadt in Salkamar, Gynk und Albar halten außerdem an irgendeinem Tag im Irmas ihren größten Markttag des Jahres ab. Die Zwerge von Sevenhill richten einen Wettbewerb namens ""Der goldene Hammer"" aus, der am 12. Irmas beginnt. Wettbewerbe für verschiedene Handwerke werden über drei oder vier Tage abgehalten, während die fünf Tage danach mit Feiern und Trinken verbracht werden und ein guter Teil des Monats benötigt wird, um wieder nüchtern zu werden.</p>
+<p>Der Monat Irmas ist nach Irmorom benannt, dem Gott des Handwerks und Handels. Vom 1. bis 8. Irmas feiert Galmair ein großes Fest mit einer Parade der Handwerker und Händler. In Cadomyr wird ebenfalls ein weniger wildes Fest der Handwerker und Händler vom 1. bis 4. Irmas geifeiert. Jede größere Stadt in Salkamar, Gynk und Albar halten außerdem an irgendeinem Tag im Irmas ihren größten Markttag des Jahres ab. Die Zwerge von Sevenhill richten einen Wettbewerb namens "Der goldene Hammer" aus, der am 12. Irmas beginnt. Wettbewerbe für verschiedene Handwerke werden über drei oder vier Tage abgehalten, während die fünf Tage danach mit Feiern und Trinken verbracht werden und ein guter Teil des Monats benötigt wird, um wieder nüchtern zu werden.</p>
 
 <h2>Sternzeichen - Der fleißige Steinbock</h2>
 
