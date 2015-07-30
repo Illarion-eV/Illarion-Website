e9d93c9235b3b76508009d706e59076dedf0a1f5
estralis <estralis@illarion.org>
2015-07-30 16:37:00 +0200

    Fix XML bug

--- website/illarion/calendar/month/de_month_3.php
+++ website/illarion/calendar/month/de_month_3.php
@@ -9,15 +9,15 @@
 
 <p>Der Zhas wurde nach Zhambra, dem Gott der Freundschaft und Loyalität, benannt. Der Monat Zhas ist eine arbeitsreiche Zeit für die Bauern und Landwirte in Runewick und Galmair, in der sie die Felder nach den erfrischenden Wassern des Tanos bearbeiten. Zhambra ist ein Hauptgott des salkamerianischen Pantheons und einige Städte in Salkamar feiern Gedichtsfeste während des Zhas, wohmöglich inspiriert von Elfenfesten. Am 12. Zhas feiern diese den Gedenktag genannt "Tag der Kirschblüte", an dem gedacht wird, dass nichts Ewig überdauert. Elfenlegenden erzählen von der Abspaltung und dem Fall eines Elfenhauses mit Namen "Narnil" und den Nachkommen dieses Hauses, den "Erume-Lie". Weit bekannt ist eine Übersetzung eines antiken Elfenliedes aus der Bibliothek von Nanuk Moonsilver:</p>
 
-<p><i>"Zum Fluss bin ich allein gegangen,<br>
-Wasser zu schöpfen.<br>
-Um mich herum fielen zart wie Schnee,<br>
-die Blätter der Kirschblüte.<br>
-Mit anmutiger Leichtigkeit gleiten sie dahin,<br>
-in ihrer sterbenden Schönheit.<br>
-Das Wasser trug die weiße Pracht hinfort,<br>
-mein Herz ihnen folgend.<br>
-Fröstelnd zittern die leeren Äste,<br>
+<p><i>"Zum Fluss bin ich allein gegangen,<br />
+Wasser zu schöpfen.<br />
+Um mich herum fielen zart wie Schnee,<br />
+die Blätter der Kirschblüte.<br />
+Mit anmutiger Leichtigkeit gleiten sie dahin,<br />
+in ihrer sterbenden Schönheit.<br />
+Das Wasser trug die weiße Pracht hinfort,<br />
+mein Herz ihnen folgend.<br />
+Fröstelnd zittern die leeren Äste,<br />
 kühl ist mir im Wind."</i></p>
 
 </div>
