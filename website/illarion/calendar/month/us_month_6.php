256aa7c8986717add1e0573e23eaf4459bdc0d9e
estralis <estralis@illarion.org>
2015-07-30 17:01:08 +0200

    Fix encoding

--- website/illarion/calendar/month/us_month_6.php
+++ website/illarion/calendar/month/us_month_6.php
@@ -7,7 +7,7 @@
 
 	<h2>Month of Generosity</h2>
 
-<p>Ronas, the second month of summer was named after Ronagan. Although Ronagan is known as the god of shadows, secrecy and thieves throughout much of Albar, Gynk and Salkamar, Ronas is recognised by many as the month of generosity. The plains folk of the Serinjah know Ronagan as god of freedom, fishermen and hunters, but also as god of secrecy and trickery. It is rumoured that there are frequent secret meetings of self-proclaimed Ronagan followers, or thief guilds, in Galmair during Ronas. This rumour remains unproven but has been told for decades, again and again.</p>
+<p>Ronas, the second month of summer was named after Ronagan. Although Ronagan is known as the god of shadows, secrecy and thieves throughout much of Albar, Gynk and Salkamar, Ronas is recognised by many as the month of generosity. The plains folk of the Serinjah know Ronagan as god of freedom, fishermen and hunters, but also as god of secrecy and trickery. It is rumoured that there are frequent secret meetings of self-proclaimed Ronagan followers, or thief guilds, in Galmair during Ronas. This rumour remains unproven but has been told for decades, again and again.</p>
 
 </div>
 <?php include_short_footer(); ?>
