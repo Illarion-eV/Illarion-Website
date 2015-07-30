71a460547082d71e94bcf903449c45b2cc191af6
estralis <estralis@illarion.org>
2015-07-30 16:54:34 +0200

    Fix encoding

--- website/illarion/calendar/month/us_month_2.php
+++ website/illarion/calendar/month/us_month_2.php
@@ -7,7 +7,7 @@
 
 	<h2>Month of Flood</h2>
 
-<p>The second month of spring is named after the creator goddess Tanora, who is supposed to be identical with the water-goddess "Zelphia" of the lizard people. Known as the month of floods, as the winter snows melt and rain falls the rivers surge with bountiful waters bringing life to parched soils and plentiful fish. Even in the coldest lands the last of the sea ice melts to open routes cut off for the winter, allowing ships to sail again and distant coasts to thrive. A harbour feast is held to celebrate in many coastal towns such as Runewick.</p>
+<p>The second month of spring is named after the creator goddess Tanora, who is supposed to be identical with the water-goddess "Zelphia" of the lizard people. Known as the month of floods, as the winter snows melt and rain falls the rivers surge with bountiful waters bringing life to parched soils and plentiful fish. Even in the coldest lands the last of the sea ice melts to open routes cut off for the winter, allowing ships to sail again and distant coasts to thrive. A harbour feast is held to celebrate in many coastal towns such as Runewick.</p>
 
 </div>
 <?php include_short_footer(); ?>
