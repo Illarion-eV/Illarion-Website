5c8a100300eef0c0902cd8af6be82dd19a6d4180
estralis <estralis@illarion.org>
2015-09-19 10:07:37 +0200

    Add structure to world page

--- website/illarion/us_world.php
+++ website/illarion/us_world.php
@@ -3,16 +3,27 @@
 
    Page::setTitle( 'The World' );
    Page::setDescription( 'An overview of the game world.' );
-   Page::setKeywords( array( 'World', 'Illarion', 'Cadomyr', 'Galmair', 'Runewick' ) );
+   Page::setKeywords( array( 'World', 'Illarion', 'Map', 'Cadomyr', 'Galmair', 'Runewick' ) );
 
    Page::setXHTML();
    Page::Init();
 ?>
 <h1>The World Illarion</h1>
 
+<h2>Content</h2>
+<ul>
+<li><a class="hidden" href="#0">World Map</a></li>
+<li><a class="hidden" href="#1">The North</a></li>
+<li><a class="hidden" href="#2">The East</a></li>
+<li><a class="hidden" href="#3">The Centre</a></li>
+<li><a class="hidden" href="#4">The South</a></li>
+</ul>
+
+<h2><a name="0"></a>World Map</h2>
+
 <center><a href="<?php echo Page::getURL(); ?>/general/us_map_of_illarion.jpg"><img width="600" height="497" src="<?php echo $url; ?>/general/us_bigthumb_map_of_illarion.jpg" alt="Map of Illarion"/></a></center>
 
-<h2><a name="1"></a>Cadomyr - Realm of Honour and Glory</h2>
+<h2><a name="1"></a>The North</h2>
 
 <p>High amongst the jagged peaks of the Nameless Mountains an eagle takes flight from its eyrie. Snow capped summits that pierce the firmament isolate the peninsula, dominating the plains, forests and deserts of Illarion below. Majestically the eagle soars above the mystery and turmoil of inexplicable wonders and unimaginable horrors that lie below.</p>
 
@@ -20,15 +31,21 @@
 
 <p>The lush grasslands of the Plains of Silence stretch eastwards as far as the Northern Woods, scarred only by the Syrita Canal cutting deep through the land to the Bounding Stream. Gliding effortlessly above the mixed woodland, the eagle circles curiously over the clearing known as Raban’s Grove. Further north the bird emits a piercing screech as it wheels abruptly above impenetrable fortress walls of the Insurmountable Limes that partition the remainder of the peninsula. Even soaring at great heights, the powerful eagle will not stray into the skies above the ominously dark forested headland.</p>
 
+<h2><a name="2"></a>The East</h2>
+
 <p>As waves crash against the eastern shore bordering the Northern Woods, a craggy outcrop known as Evil Rock looms from the ocean to dominate the skyline. Continuing south the eagle banks over the isolated Northern Harbour and the Pauldron Isle with its scattered ruins and restless cemeteries. From the ocean, marshland harbouring the ruins of a long forgotten temple forms the northern bank of the Bounding Stream. Further inland, where the Bounding Stream and Glowing River converge at the Spider’s Mouth, the Hemp Necktie Inn sits on the island intersection of routes to all three realms.</p>
 
 <p>Extending along the southern bank of the Bounding Stream, the Dead Marsh borders the bountiful Elstree Plain, overlooked by the mountain known only for Igruk’s Cave and the dangers within. The eagle escalates high above the towering trees of Elstree Forest, unaffected by much of the mysterious enchantment hidden beneath the canopy. The grassy Eastland separates the huge expanse of forest from the windswept coast. From the isolated Eastport Harbour, the eagle swerves between the Twin Mountains and over the curiosity that is known to some as Wonderland, before gliding towards the Dewy Swamps.</p>
 
 <p>Set in an archipelago of forested islands, the town of Runewick is surrounded by fertile fields and bountiful glades. The eagle circles over the patchwork of buildings and interconnecting bridges, indifferent to the words of wisdom transcribed in libraries below. Across Lunord Bridge lies the farming community of Yewdale, with Runewick Harbour overlooking the entrance to Snakehead Bay. Beyond the babbling Anthil Brook lies the Bear Cave and rolling meadows of Merryglade.</p>
 
+<h2><a name="3"></a>The Centre</h2>
+
 <p>The eagle rises sharply to soar clear of the Dragon’s Lair, mindful of the power that lies within. Lava courses from the volcano laying waste to the land along the northern flank of the mountain. The putrid swampland of Death’s Stench stretches as far as the Three Fingertips which protrude into Snakehead Bay. Further along Snakehead Coast, Mount Snakehead rises to overlook the bay, with numerous islands scattered before it.</p>
 
 <p>Soaring north towards the Border mountains, the sharp eyes of the eagle picks out elven ruins hidden amongst the trees. Carved into the mountain face lie the entrances to Cherass’ Citidal and the Glittering Cave. Banking left, the eagle follows the Glowing River upstream as it surges along the eastern flank of the Border Mountains. In the shadows of rocky outcrops, the eagle glides unhindered over the scar of destruction known as Death Swath. Water gushes from the Glowing Gate, cutting through the lava flows of the Glowing Forest below, steam rising to envelope the gnarled remains of a long-dead forest. Crossing the Wight Canal and blazing Firelimes the eagle circles curiously over the ruins that stretch along the western shore of Snakehead Bay, only the Lighthouse Harbour still intact.</p>
+
+<h2><a name="4"></a>The South</h2>
 
 <p>From the southernmost foothills of the Border Mountains, the eagle escalates over Sentry Forest that rises to guard the western coast of Snakehead Bay. Banking over the trees, the eagle glides back along the coast, the Lonely Islands dotting the horizon far out in the Great Ocean. As the Border Mountains extend south from the Nameless Mountains, they form the eastern border of the Kantabi Desert. Cave entrances fleck the mountainside, hinting at the beasts and fugitives that find refuge within. Buried deep beneath the mountains, however, lies the Shadowland, the horrors within unbeknown to the soaring eagle.</p>
 
@@ -39,5 +56,3 @@
 <p>Gracefully escalating back to the overlooking peaks, the eagle returns to its eyrie. From its lofty vantage it remains unaffected by the jealousy, betrayal and envy that drive the power struggles between the realms. Each day it continues to circle over Illarion, observing all that happens and yet remaining far out of reach of the turmoil wrought below. Some claim the gods watch from above, but most are too immersed in their own troubles to even notice the soaring eagle.</p>
 
 <?php Page::insert_go_to_top_link(); ?>
-
-<
