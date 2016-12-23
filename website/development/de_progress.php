259bc76f56a4b91587a91e0b2532ed839169e45d
estralis <estralis@illarion.org>
2016-12-23 20:16:55 +0100

    Replace term milestoine with work package

--- website/development/de_progress.php
+++ website/development/de_progress.php
@@ -11,13 +11,13 @@
 
 <h1>Entwicklungsfortschritt</h1>
 
-<p>Die Entwicklung Illarions folgt klar definierten Meilensteinen, welche darauf abzielen, für die existierenden Spielbereiche einen Zustand zu erreichen, der keine zukünftige Überarbeitung mehr erfordert. Somit soll das Spiel anschließend nur noch durch Verbesserungen und zusätzliche Inhalte verbessert werden, wobei keine grundlegenden Probleme oder Fehler mehr auftreten. Die Meilensteine werden nicht der Reihenfolge nach abgeschlossen sondern parallel bearbeitet. Die Zahlen entsprechen hierbei der prognostizierten Fertigstellungsreihenfolge.</p>
+<p>Die Entwicklung Illarions folgt klar definierten Arbeitspaketen, welche darauf abzielen, für die existierenden Spielbereiche einen Zustand zu erreichen, der keine zukünftige Überarbeitung mehr erfordert. Somit soll das Spiel anschließend nur noch durch Verbesserungen und zusätzliche Inhalte verbessert werden, wobei keine grundlegenden Probleme oder Fehler mehr auftreten. Die Arbeitspakete werden nicht in einer Reihenfolge nach abgeschlossen sondern parallel bearbeitet. Die Aufzählung auf dieser Seite entspricht der prognostizierten Fertigstellungsreihenfolge.</p>
 
-<p>Wenn du bei der Entwicklung Illarions mithelfen willst, wende dich an einen der <a href="http://illarion.org/community/de_contact.php#4">Entwickler</a> oder schau einfach mal im <a href="http://illarion.org/community/de_chat.php">Chat</a> vorbei. Klicke auf Titel der Meilensteine, um auf die entsprechende Zusammenstellung der Entwicklungspunkte im Mantis-Bugtracker zu gelangen.</p>
+<p>Wenn du bei der Entwicklung Illarions mithelfen willst, wende dich an einen der <a href="http://illarion.org/community/de_contact.php#4">Entwickler</a> oder schau einfach mal im <a href="http://illarion.org/community/de_chat.php">Chat</a> vorbei. Klicke auf Titel der Arbeitspakete, um auf die entsprechende Zusammenstellung der Entwicklungspunkte im Mantis-Bugtracker zu gelangen.</p>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Meilenstein I: Stabiler Zustand für Reviewer und Werbung</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Arbeitspaket Stabiler Zustand für Reviewer und Werbung</a></h2>
 
-<p>Dieser Meilenstein zielt darauf ab, das Spiel von Fehlern zu befreien und den Eindruck während der ersten Spielstunde und den Weg dorthin zu verbessern. Der gesamte Weg vom Auffinden des Spiels, seiner Installation und dem Start ins Spiel ist enthalten. Inhalte, die sich nicht in der ersten Spielstunde bemerkbar machen, sind nicht Inhalt des Meilensteins, seien sie noch so wichtig für das Spiel.</p>
+<p>Dieses Arbeitspaket zielt darauf ab, das Spiel von Fehlern zu befreien und den Eindruck während der ersten Spielstunde und den Weg dorthin zu verbessern. Der gesamte Weg vom Auffinden des Spiels, seiner Installation und dem Start ins Spiel ist enthalten. Inhalte, die sich nicht in der ersten Spielstunde bemerkbar machen, sind nicht Inhalt des Arbeitspakets, seien sie noch so wichtig für das Spiel.</p>
 
 <h3>Highlights</h3>
 
@@ -27,9 +27,9 @@
     <li>Aktualisierung der Homepage</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Meilenstein II: Neue Gegenstände und verbessertes Handwerk</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Arbeitspaket Neue Gegenstände und verbessertes Handwerk</a></h2>
 
-<p>Mit diesem Meilenstein werden dem Spiel eine sinnvolle Anzahl von Gegenständen hinzugefügt, insbesondere Waffen und Rüstungen. Ihr Nutzen und ihre Herstellung werden in Hinblick auf die Handwerke, deren Verteilung über die Städte, Ressourcen, Verbrauchsmaterialien und Preise gestaltet.</p>
+<p>Mit diesem Arbeitspaket werden dem Spiel eine sinnvolle Anzahl von Gegenständen hinzugefügt, insbesondere Waffen und Rüstungen. Ihr Nutzen und ihre Herstellung werden in Hinblick auf die Handwerke, deren Verteilung über die Städte, Ressourcen, Verbrauchsmaterialien und Preise gestaltet.</p>
 
 <h3>Highlights</h3>
 
@@ -39,9 +39,9 @@
     <li>Verbesserung des Sammelns von Ressourcen</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Meilenstein III: Magie</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Arbeitspaket Magie</a></h2>
 
-<p>Magie ist ein notwendiges Features eines Fantasyspiels. Mit diesem Meilenstein werden wir die arkane Magie wieder einführen und ebenso die heilige Magie der Götter. Entsprechende Anpassungen an handwerksähnliche Systeme wie Alchemie werden erfolgen.</p>
+<p>Magie ist ein notwendiges Features eines Fantasyspiels. Mit diesem Arbeitspaket werden wir die arkane Magie wieder einführen und ebenso die heilige Magie der Götter. Entsprechende Anpassungen an handwerksähnliche Systeme wie Alchemie werden erfolgen.</p>
 
 <h3>Highlights</h3>
 
@@ -51,9 +51,9 @@
     <li>Handwerksähnliche Verzauberungen und Alchemieverbesserungen</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Meilenstein IV: Überarbeitung des Kampfsystems</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Arbeitspaket Überarbeitung des Kampfsystems</a></h2>
 
-<p>Das derzeitige Kampfsystem bedarf einer Überarbeitung, um fairer, klarer und verlässlicher zu werden. Dieser Meilenstein wird für die Spieler nur wenige sichtbare Veränderungen bringen, jedoch zu einem deutlich verbesserten und flexibleren Kampfsystem führen.</p>
+<p>Das derzeitige Kampfsystem bedarf einer Überarbeitung, um fairer, klarer und verlässlicher zu werden. Dieses Arbeitspaket wird für die Spieler nur wenige sichtbare Veränderungen bringen, jedoch zu einem deutlich verbesserten und flexibleren Kampfsystem führen.</p>
 
 <h3>Highlights</h3>
 
@@ -63,9 +63,9 @@
     <li>Der Nutzen der verschiedenen Waffen und Rüstungen wird fairer verteilt</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Meilenstein V: Sandkasten und Immersion</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Arbeitspaket Sandkasten und Immersion</a></h2>
 
-<p>Illarion steht im Wettbewerb mit anderen Spielen. Um sich als Indie-Spiel abzuheben, bedarf es Features, die kein anderes Spiel aufweisen kann. Mit diesem Meilenstein wollen wir solche Features einführen, die der Immersion dienen und dem Spieler zusätzliche Freiräume ermöglichen, seinen Charakter auszuspielen.</p>
+<p>Illarion steht im Wettbewerb mit anderen Spielen. Um sich als Indie-Spiel abzuheben, bedarf es Features, die kein anderes Spiel aufweisen kann. Mit diesem Arbeitspaket wollen wir solche Features einführen, die der Immersion dienen und dem Spieler zusätzliche Freiräume ermöglichen, seinen Charakter auszuspielen.</p>
 
 <h3>Highlights</h3>
 
@@ -75,9 +75,9 @@
     <li>Erhöhung der Atmosphäre im Spiel</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Meilenstein VI: Inhalte, Quests und Dungeons</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Arbeitspaket Inhalte, Quests und Dungeons</a></h2>
 
-<p>Durch das Hinzufügen weiterer Quests und Dungeons soll in diesem Meilenstein die Attraktivität des Spiels erhöht werden. Mit ihnen soll der Hintergrund des Spiels ("Lore") dem Spieler nähergebracht werden. Bücher, NPCs, Inschriften und Ereignisse sollen an jedem wichtigen Ort den Hintergrund präsent machen.</p>
+<p>Durch das Hinzufügen weiterer Quests und Dungeons soll in diesem Arbeitspaket die Attraktivität des Spiels erhöht werden. Mit ihnen soll der Hintergrund des Spiels ("Lore") dem Spieler nähergebracht werden. Bücher, NPCs, Inschriften und Ereignisse sollen an jedem wichtigen Ort den Hintergrund präsent machen.</p>
 
 <h3>Highlights</h3>
 
--- website/development/us_progress.php
+++ website/development/us_progress.php
@@ -11,13 +11,13 @@
 
 <h1>Development Progress</h1>
 
-<p>Illarion's development follows clearly specified milestones. Those milestones stand for a state of the game that does not require any future reworking on existing features. Hence, only improvements and new features will be added without fundamental flaws or bugs. The milestones are worked on in parallel and not in sequence. The number gives the predicted sequence of completion, though.</p>
+<p>Illarion's development follows clearly specified work packages. Those work packages stand for a state of the game that does not require any future reworking on existing features. Hence, only improvements and new features will be added without fundamental flaws or bugs. The work packages are worked on in parallel and not in sequence. This pages orders them in the predicted sequence of completion, though.</p>
 
-<p>If you want to help to develop Illarion, please contact one of the <a href="http://illarion.org/community/us_contact.php#4">developers</a> or join the <a href="http://illarion.org/community/us_chat.php">chat</a>. Click on the title of a milestone to learn more about the individual development items on Mantis Bugtracker.</p>
+<p>If you want to help to develop Illarion, please contact one of the <a href="http://illarion.org/community/us_contact.php#4">developers</a> or join the <a href="http://illarion.org/community/us_chat.php">chat</a>. Click on the title of a work package to learn more about the individual development items on Mantis Bugtracker.</p>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Milestone I: Stable state for reviewers and promotion</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9659" target="_blank">Work package Stable state for reviewers and promotion</a></h2>
 
-<p>With this milestone, we want to get rid of any bugs and improve the impression of the game during the first hour of playing. The whole road from finding the game over installation until starting the game is affected. Content that is not part of the first hour is not part of this milestone albeit important for the overall game.</p>
+<p>With this work package, we want to get rid of any bugs and improve the impression of the game during the first hour of playing. The whole road from finding the game over installation until starting the game is affected. Content that is not part of the first hour is not part of this work package albeit important for the overall game.</p>
 
 <h3>Highlights</h3>
 
@@ -27,9 +27,9 @@
     <li>Update of the website</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Milestone II: New items and improved crafting</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9822" target="_blank">Work package New items and improved crafting</a></h2>
 
-<p>This milestone will add a reasonable amount of new items to the game, especially weapons and armours, define their use properly and make the available to players. The crafts for these items will be rebalanced in terms of craft distribution over towns, resources, needed materials for items and also prices.</p>
+<p>This work package will add a reasonable amount of new items to the game, especially weapons and armours, define their use properly and make the available to players. The crafts for these items will be rebalanced in terms of craft distribution over towns, resources, needed materials for items and also prices.</p>
 
 <h3>Highlights</h3>
 
@@ -39,9 +39,9 @@
     <li>Improvements to gathering of resources</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Milestone III: Magic</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9823" target="_blank">Work package Magic</a></h2>
 
-<p>Magic is a mandatory feature for a fantasy game. With this milestone arcane magic will return to the game and also, we'll release divine magic for priests. Adjustments to craft like actions like alchemy will also be included.</p>
+<p>Magic is a mandatory feature for a fantasy game. With this work package arcane magic will return to the game and also, we'll release divine magic for priests. Adjustments to craft like actions like alchemy will also be included.</p>
 
 <h3>Highlights</h3>
 
@@ -51,9 +51,9 @@
     <li>Craft like enchanting and improvements to alchemy</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Milestone IV: Review the fighting system</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9914" target="_blank">Work package Review the fighting system</a></h2>
 
-<p>The current fighting system is in need of a review to become more fair, clear and relieable. This milestone won't bring many visible changes but an improved and more flexible fighting system.</p>
+<p>The current fighting system is in need of a review to become more fair, clear and relieable. This work package won't bring many visible changes but an improved and more flexible fighting system.</p>
 
 <h3>Highlights</h3>
 
@@ -63,9 +63,9 @@
     <li>The use of weapons and armours will be distributed in a fair way</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Milestone V: Sandboxing and immersion</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9825" target="_blank">Work package Sandboxing and immersion</a></h2>
 
-<p>Illarion competes with other games. To stand out as an indie game, it needs features no other game can offer. This milestone will add features for enhanced immersion and enable new options for the players to shape their characters.</p>
+<p>Illarion competes with other games. To stand out as an indie game, it needs features no other game can offer. This work package will add features for enhanced immersion and enable new options for the players to shape their characters.</p>
 
 <h3>Highlights</h3>
 
@@ -75,9 +75,9 @@
     <li>Enhancement of the game atmosphere</li>
 </ul>
 
-<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Milestone VI: Content, quests and dungeons</a></h2>
+<h2><a href="http://illarion.org/mantis/view.php?id=9824" target="_blank">Work package Content, quests and dungeons</a></h2>
 
-<p>By adding further quests and dungeons, this milestone will increased the attractivity of this game. With these features, the lore shall be transported to the player. Books, NPCs, inscriptions and events will teach the lore at every noteable location.</p>
+<p>By adding further quests and dungeons, this work package will increased the attractivity of this game. With these features, the lore shall be transported to the player. Books, NPCs, inscriptions and events will teach the lore at every noteable location.</p>
 
 <h3>Highlights</h3>
 
