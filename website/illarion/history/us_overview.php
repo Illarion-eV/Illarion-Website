9ac592e5508cac4ec361451172c407b192cb1631
estralis <estralis@illarion.org>
2017-12-21 23:47:09 +0100

    Add new history page showcase

---
+++ website/illarion/history/us_overview.php
@@ -0,0 +1,62 @@
+<?php
+	include_once ($_SERVER['DOCUMENT_ROOT'] . "/shared/shared.php");
+	create_header("Illarion - History",
+	"The History of Illarion  ",
+	"background, gods, story, history");
+	include_header();
+?>
+
+<h1>The History of Illarion</h1>
+
+<?php cap(H); ?>
+
+<p>istory! Even the word itself tells of mysteries and heroic deeds. Isn't it great to
+rummage around in the great libraries, to poke your nose deep into big old books? Well, at least
+for me it is!</p>
+
+<p>Let's see. Scholars do devide the history of Illarion into three parts: The history of the
+Gods, the history of the mortals and the history of Illarion.</p>
+
+<div class="clr"></div>
+<h2>The History of the Gods</h2>
+
+<ul>
+	<li><a href="us_age_1.php">Age of Legends</a></li>
+	<li><a href="us_age_2.php">Age of Dawn</a></li>
+	<li><a href="us_age_3.php">Age of Struggle</a></li>
+	<li><a href="us_age_4.php">Age of Concord</a></li>
+	<li><a href="us_age_5.php">Age of Discord</a></li>
+	<li><a href="us_age_6.php">Age of Isolation</a></li>
+    <li><a href="us_age_7.php">Age of Responsibility</a></li>
+</ul>
+
+<h2>The History of the mortals</h2>
+
+<ul>
+	<li><a href="us_early_orcmigrations.php#1">The First Mortals</a></li>
+	<li><a href="us_early_orcmigrations.php#2">The Rise of Moshran’s Children</a></li>
+	<li><a href="us_early_orcmigrations.php#3">Fire and the Vengeance of Elves</a></li>
+	<li><a href="us_early_orcmigrations.php#4">The Descent of Dwarven Kind</a></li>
+    
+    <li><a href="us_early_orcmigrations.php#1">The Curse of Narnil</a></li>
+	<li><a href="us_early_orcmigrations.php#2">Norodaj and the Sea Dragons</a></li>
+	<li><a href="us_early_orcmigrations.php#3">The Looting of Firnismill</a></li>
+	<li><a href="us_early_orcmigrations.php#4">Human Wars and the Rise of Refugees</a></li>
+    <li><a href="us_early_orcmigrations.php#2">Lor-Angur, The Seat of Magic</a></li>
+	<li><a href="us_early_orcmigrations.php#3">The Rise and Fall of Kharud</a></li>
+	<li><a href="us_early_orcmigrations.php#4">An Island Lost</a></li>
+</ul>
+
+<h2>The History of Illarion</h2>
+
+<ul>
+	<li><a href="us_early_orcmigrations.php#1">A Forgotten Land</a></li>
+	<li><a href="us_early_orcmigrations.php#2">New Beginnings</a></li>
+	<li><a href="us_early_orcmigrations.php#3">Glory, Wisdom and Prosperity</a></li>
+	<li><a href="us_early_orcmigrations.php#4">Foolhardiness, Arrogance and Greed</a></li>
+    <li><a href="us_early_orcmigrations.php#4">An Uneasy Equilibrium</a></li>
+</ul>
+
+<?php insert_go_to_top_link(); ?>
+
+<?php include_footer(); ?>
