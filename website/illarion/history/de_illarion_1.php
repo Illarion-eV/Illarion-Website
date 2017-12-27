35f55bc08a11b752c1683c5e75e931a9d816ada8
estralis <estralis@illarion.org>
2017-12-27 15:00:30 +0100

    Minor corrections

--- website/illarion/history/de_illarion_1.php
+++ website/illarion/history/de_illarion_1.php
@@ -2,18 +2,18 @@
 	include_once ($_SERVER['DOCUMENT_ROOT'] . "/shared/shared.php");
 	create_header("Illarion - Geschichte",
 	"Ein vergessenes Land",
-	"Hintergrund, Götter, Geschichte");
+	"Hintergrund, GÃ¶tter, Geschichte");
 	include_header();
 ?>
 
 <?php navBarTop("","de_overview.php","de_illarion_2.php"); ?>
 
-<h1>Ein vergessenes Land/h1>
+<h1>Ein vergessenes Land</h1>
 
 <?php cap(S); ?>
-<p>chon in den Archiven der alten Imperien finden sich Karten und Berichte, auf denen von Illarion die Rede ist. Zerfallene Elfenruinen entlang des Leuchtenden Flusses, urzeitliche Pyramiden in der Kantabiwüste und die verlassenen Häuser auf der Schulterplatte künden von den früheren Bewohnern. Nur wenig Schriftliches ist von ihnen überliefert und noch viel weniger hat bisher das Geheimnis ihres Niedergangs preisgegeben.</p>
+<p>chon in den Archiven der alten Imperien finden sich Karten und Berichte, auf denen von Illarion die Rede ist. Zerfallene Elfenruinen entlang des Leuchtenden Flusses, urzeitliche Pyramiden in der KantabiwÃ¼ste und die verlassenen HÃ¤user auf der Schulterplatte kÃ¼nden von den frÃ¼heren Bewohnern. Nur wenig Schriftliches ist von ihnen Ã¼berliefert und noch viel weniger hat bisher das Geheimnis ihres Niedergangs preisgegeben.</p>
 
-<p>Hin und wieder finden sich Verweise auf den verlassenen unüberwindlichen Grenzwall, der sich im Norden erstreckt und die Halbinsel vom Rest der Welt trennt. Er geht zurück auf die Zeit, in der Kharud fiel und die neuen Reiche Illarions aufstiegen. Die Geschichten lassen vermuten, dass hinter der Befestigung ein unvorstellbarer Schrecken haust, denn bisher ist niemand bekannt, der lebend aus den endlosen dunklen Wäldern hinter der Mauer zurückkehrte.</p>
+<p>Hin und wieder finden sich Verweise auf den verlassenen unÃ¼berwindlichen Grenzwall, der sich im Norden erstreckt und die Halbinsel vom Rest der Welt trennt. Er geht zurÃ¼ck auf die Zeit, in der Kharud fiel und die neuen Reiche Illarions aufstiegen. Die Geschichten lassen vermuten, dass hinter der Befestigung ein unvorstellbarer Schrecken haust, denn bisher ist niemand bekannt, der lebend aus den endlosen dunklen WÃ¤ldern hinter der Mauer zurÃ¼ckkehrte.</p>
 
 <?php navBarBottom("de_overview.php", "de_illarion_2.php"); ?>
 
