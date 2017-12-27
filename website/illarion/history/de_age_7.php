67f1857e1dbe92297c3acb8cfd55b2a36ffa4e1e
estralis <estralis@illarion.org>
2017-12-27 14:57:48 +0100

    Encoding

--- website/illarion/history/de_age_7.php
+++ website/illarion/history/de_age_7.php
@@ -2,7 +2,7 @@
 	include_once ($_SERVER['DOCUMENT_ROOT'] . "/shared/shared.php");
 	create_header("Illarion - Geschichte",
 	"Das Zeitalter der Verantwortung",
-	"Hintergrund, Götter, Geschichte");
+	"Hintergrund, GÃ¶tter, Geschichte");
 	include_header();
 ?>
 
@@ -11,9 +11,9 @@
 <h1>Das Zeitalter der Verantwortung</h1>
 
 <?php cap(D); ?>
-<p>ie noch nicht verheilten Wunden und Verwüstungen aus den Zeiten der Zwietracht vor Augen und getrieben durch die Gefahr, die von Moshran ausging, erkannten die Götter, dass der Schwur niemals die Hand gegeneinander zu erheben nicht ausreichen würde, Frieden unter den Sterblichen zu schaffen. Um ihre Schöpfungen vor der Auslöschung durch ihre eigenen Hände zu bewahren, schufen sie sieben Edelsteine von unvorstellbarer magischer Kraft. Mit Ausnahme des Diamanten, den die Götter in ihre eigene Obhut nahmen, gaben sie im Geheimen jeweils einen Stein an einen vertrauenswürdigen Vertreter jeder Rasse.</p>
+<p>ie noch nicht verheilten Wunden und VerwÃ¼stungen aus den Zeiten der Zwietracht vor Augen und getrieben durch die Gefahr, die von Moshran ausging, erkannten die GÃ¶tter, dass der Schwur niemals die Hand gegeneinander zu erheben nicht ausreichen wÃ¼rde, Frieden unter den Sterblichen zu schaffen. Um ihre SchÃ¶pfungen vor der AuslÃ¶schung durch ihre eigenen HÃ¤nde zu bewahren, schufen sie sieben Edelsteine von unvorstellbarer magischer Kraft. Mit Ausnahme des Diamanten, den die GÃ¶tter in ihre eigene Obhut nahmen, gaben sie im Geheimen jeweils einen Stein an einen vertrauenswÃ¼rdigen Vertreter jeder Rasse.</p>
 
-<p>Man weiß nicht wer die Ersten waren, die von den Göttern erwählt wurden. Doch es dauerte nicht lange und Gerüchte über den Verbleib der Steine kamen auf. Einige der Wächter von ihnen sollen verrückt geworden sein ob der Geißel der Macht. Andere wurden von ihren Nächsten verraten. Missgunst, Neid und Verrat sind alltägliche Begleiter des Kampfes um die Macht. Manche Steine wurden vererbt, andere lagen in vergessenen Verstecken als ihre Hüter verstarben oder fielen in die Hände von Glücksrittern und Eroberern. Bis heute ist unser Schicksal eng mit den mächtigen Steinen und ihren Wächtern verbunden.</p>
+<p>Man weiÃŸ nicht wer die Ersten waren, die von den GÃ¶ttern erwÃ¤hlt wurden. Doch es dauerte nicht lange und GerÃ¼chte Ã¼ber den Verbleib der Steine kamen auf. Einige der WÃ¤chter von ihnen sollen verrÃ¼ckt geworden sein ob der GeiÃŸel der Macht. Andere wurden von ihren NÃ¤chsten verraten. Missgunst, Neid und Verrat sind alltÃ¤gliche Begleiter des Kampfes um die Macht. Manche Steine wurden vererbt, andere lagen in vergessenen Verstecken als ihre HÃ¼ter verstarben oder fielen in die HÃ¤nde von GlÃ¼cksrittern und Eroberern. Bis heute ist unser Schicksal eng mit den mÃ¤chtigen Steinen und ihren WÃ¤chtern verbunden.</p>
 
 <?php navBarBottom("de_age_6.php", ""); ?>
 
