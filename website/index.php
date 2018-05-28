d788e24e91c11e9d869116e2f5081574c6d4ad2c
Martin Karing <nitram@illarion.org>
2018-05-28 21:32:09 +0200

    Fixed white page on illarion.org

--- website/index.php
+++ website/index.php
@@ -1,6 +1,6 @@
 <?php
     if (preg_match('/de/', $_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-        header('Locations: http://' . $_SERVER['SERVER_NAME'] . '/general/de_startpage.php');
+        header('Location: https://' . $_SERVER['SERVER_NAME'] . '/general/de_startpage.php');
     } else {
-        header('Locations: http://' . $_SERVER['SERVER_NAME'] . '/general/us_startpage.php');
+        header('Location: https://' . $_SERVER['SERVER_NAME'] . '/general/us_startpage.php');
     }
