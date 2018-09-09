a9daa8fb950abd59436ecc4eaebcdf1e4f3a7aee
estralis <estralis@illarion.org>
2018-09-09 09:38:39 +0200

    Integrate Discord

--- website/community/de_chat.php
+++ website/community/de_chat.php
@@ -11,11 +11,13 @@ Page::Init();
 
 <h2>Illarion Discord-Server</h2>
 
-<iframe src="https://discordapp.com/widget?id=401855954272124940&theme=dark" width="700" height="500" allowtransparency="true" frameborder="0"></iframe>
+<iframe src="https://discordapp.com/widget?id=401855954272124940&amp;theme=dark" width="700" height="500" allowtransparency="true" frameborder="0"></iframe>
 
 <h3>Information über Discord</h3>
 
+<p>Auf dieser Seite wird eine Verbindung zu Discord hergestellt um dem Illarion-Chat beizutreten. In diesem Chat gelten sowohl die <a href="<?php echo $url; ?>/illarion/de_rules.php">Regeln von Illarion</a> als auch die <a href="https://discordapp.com/guidelines">Regeln von Discord</a>.</p>
 
+<p>Solltest du dich direkt mit Discord verbinden wollen, hier der nötige Einladungslink: <a href="https://discord.gg/yj3htPN">https://discord.gg/yj3htPN</a></p>
 
 <h2>Illarion IRC-Chat</h2>
 
