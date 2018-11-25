5c52edd1010e04fa616af7ea7015c9b3eb2a4bee
estralis <estralis@illarion.org>
2018-11-25 18:42:15 +0100

    Formatting

--- website/general/de_startpage_new.php
+++ website/general/de_startpage_new.php
@@ -24,16 +24,16 @@
 <h2>Die Welt Illarion</h2>
 <p class="hyphenate">Illarion ist eine reife Fantasy-Welt, in der Orks, Elfen und viele andere Kreaturen anzutreffen sind. Es ist eine offene, lebendige Welt mit greifbarem Hintergrund. Sechzehn Götter werden von den Bewohnern von drei Reichen verehrt. Das edle <a href="/illarion/de_factions.php#1">Cadomyr</a>, das reiche <a href="/illarion/de_factions.php#2">Galmair</a> oder das weise <a href="/illarion/de_factions.php#3">Runewick</a> - welchen Weg wirst du einschlagen?</p>
 
-<h2>Dein Abenteuer</h2>
+<h3>Dein Abenteuer</h3>
 <p class="hyphenate">Deine Entscheidungen und Taten formen und gestalten die Welt. Sie werden eines Tages die Seiten der Geschichtsbücher Illarions füllen. Werde Eins mit der Welt und lass deinen Charakter werden, was du möchtest.</p>
-
-<h2>Kostenlos und Open Source</h2>
-<p class="hyphenate">Illarion ist komplett kostenlos. Das Spiel wird von einem eingetragenen Verein geführt und von hochmotivierten Freiwilligen betrieben. Der Client-, Server- und Spielcode sind Open Source gemäß den Bedingungen der Lizenzen GPLv3 und APGPLv3.</p>
 
 <h2>Keine Grenzen</h2>
 <p class="hyphenate">Dein Charakter wird nicht auf eine feste Klasse eingegrenzt wie bei den meisten MMORPGs. Du kannst deinen eigenen Charakter formen und sein Schicksal ist nur von deinen Entscheidungen, Vorstellungen und den Spielregeln begrenzt. Das Spiel hat ein skillbasiertes Levelsystem, welches es dem Spieler erlaubt, seine eigene einzigartige Spielerfahrung zu machen.</p>
 
-<h2>Immersives Spielkonzept</h2>
+<h3>Kostenlos und Open Source</h3>
+<p class="hyphenate">Illarion ist komplett kostenlos. Das Spiel wird von einem eingetragenen Verein geführt und von hochmotivierten Freiwilligen betrieben. Der Client-, Server- und Spielcode sind Open Source gemäß den Bedingungen der Lizenzen GPLv3 und APGPLv3.</p>
+
+<h3>Immersives Spielkonzept</h3>
 <p class="hyphenate">Die angewandten Spielmechaniken halten die Balance zwischen Komplexität, Innovation und Motivation der Spieler. Illarion ist ein klassisches Spiel im gemächlichen Tempo mit isometrischen Retrografiken und modernen Technologien zugleich.</p>
 
 <?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
--- website/general/us_startpage_new.php
+++ website/general/us_startpage_new.php
@@ -25,16 +25,16 @@
 <h2>The World Illarion</h2>
 <p class="hyphenate">A mature high fantasy setting where elves meet orcs is placed in a persistent, open world with tangible lore. Sixteen gods are worshipped by the citizens of three factions. Noble <a href="/illarion/us_factions.php#1">Cadomyr</a>, wealthy <a href="/illarion/us_factions.php#2">Galmair</a> or wise <a href="/illarion/us_factions.php#3">Runewick</a> - whose side will you join?</p>
 
-<h2>Your Adventure</h2>
+<h3>Your Adventure</h3>
 <p class="hyphenate">The decisions you take will impact and shape the world around you. Your actions will determine the events that one day fill the pages of Illarion's history books. Immerse yourself and let your character become what you want.</p>
-
-<h2>Free and open source</h2>
-<p class="hyphenate">Illarion is free to play without any cost. The game is run by a non-profit organisation and is maintained by a staff of highly motivated volunteers without financial reimbursement. The client, server and game code is open source and released under the licences GPLv3 and AGPLv3.</p>
 
 <h2>No limits</h2>
 <p class="hyphenate">Your character won't be locked inside a fixed class design like most MMORPGs do. You'll be able to sculpt your own character and its destiny is limited only by your choices, imagination, and game rules. The game features a skill-based levelling system, allowing players to compel their own unique gaming experience.</p>
 
-<h2>Immersive mechanics</h2>
+<h3>Free and open source</h3>
+<p class="hyphenate">Illarion is free to play without any cost. The game is run by a non-profit organisation and is maintained by a staff of highly motivated volunteers without financial reimbursement. The client, server and game code is open source and released under the licences GPLv3 and AGPLv3.</p>
+
+<h3>Immersive mechanics</h3>
 <p class="hyphenate">The game mechanics employed keep the balance between complexity, innovation and motivation of players. Illarion shows a classic, slow-paced gameplay with isometric retro graphics while still using modern technologies.</p>
 
 <?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
