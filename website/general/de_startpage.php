d0d12932f7b861928856a42b9c9c6bf51188adbd
estralis <estralis@illarion.org>
2018-11-25 20:03:40 +0100

    Roll out new startpage

--- website/general/de_startpage.php
+++ website/general/de_startpage.php
@@ -14,32 +14,12 @@
 	$xmlC->Set_XML_data( $xml_file );
 ?>
 
-<h1>Die Welt Illarion</h1>
+<h1>Illarion - Gestalte ein neues Leben</h1>
 
-<?php Page::cap('D'); ?>
-<p class="hyphenate">
-as Land ist in Aufruhr. Der Edelsteinkrieg zerrüttete die Reiche,
-Flüchtlinge aller Völker strömen in die Bastionen der Menschheit im Land
-Illarion, die verschont geblieben sind von den Entbehrungen der vergangenen
-Tage. Sechs Edelsteine der Macht waren den Fürsten dieser Bastionen zur
-Verwahrung gegeben; doch Missgunst, Verrat und Neid sind die alltäglichen
-Geißeln der Macht. Das edle <a href="/illarion/de_factions.php#1">Cadomyr</a>, 
-das reiche <a href="/illarion/de_factions.php#2">Galmair</a> oder das weise 
-<a href="/illarion/de_factions.php#3">Runewick</a> - welchen Weg wirst du einschlagen?</p>
+<?php Page::cap('E'); ?>
+<p class="hyphenate">chtes Rollenspiel und die Interaktion mit anderen Spielern - das ist das Onlinespiel Illarion. Alle Charaktere um dich herum werden sich wie lebendige, atmende Wesen dieser eigenständigen, geheimnisvollen Welt verhalten. Jeder Charakter hat eine eigene Vergangenheit, Ziele, Stärken und Schwächen. Erlebe als edler Ritter ruhmvolle Abenteuer oder führe ein Leben als fleißiger Handwerker, geschäftiger Händler oder charismatischer Priester der Götter. Das edle <a href="/illarion/de_factions.php#1">Cadomyr</a>, das reiche <a href="/illarion/de_factions.php#2">Galmair</a> oder das weise <a href="/illarion/de_factions.php#3">Runewick</a> - welchen Weg wirst du einschlagen?</p>
 
-<?php Page::cap('I'); ?>
-<p class="hyphenate">
-llarion ist ein kostenloses Open Source-MMORPG, welches seinen Schwerpunkt
-auf echtes Rollenspiel legt. Alle Charaktere um dich herum werden sich wie
-lebendige, atmende Wesen dieser eigenständigen, geheimnisvollen Welt
-verhalten. Jeder Charakter hat eine eigene Vergangenheit, Ziele, Stärken und
-Schwächen. Erlebe als edler Ritter ruhmvolle Abenteuer oder führe ein Leben
-als fleißiger Handwerker, geschäftiger Händler oder charismatischer Priester
-der <a href="/illarion/goetter/de_bck_10.php">Götter</a>.
-Deine Entscheidungen und Taten formen und gestalten diese Welt und werden 
-eines Tages die Seiten der Geschichtsbücher füllen.</p>
-
-<p>Illarion - Welche Rolle wirst du spielen? <a href="/illarion/de_java_download.php">Lade das Spiel herunter und beginne dein Abenteuer!</a></p>
+<p class="hyphenate">Illarion - Welche Rolle wirst du spielen? <a href="/illarion/us_java_download.php">Lade das Spiel jetzt herunter und beginne dein Abenteuer!</a></p>
 
 <?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
 <div><a id="group<?php echo $currGroup->index; ?>"></a></div>
@@ -55,89 +35,10 @@ eines Tages die Seiten der Geschichtsbücher füllen.</p>
 
 <div class="clr"></div>
 
-<?php
-	$pgSQL =& Database::getPostgreSQL();
-	
-	if(IllaUser::auth('quests'))
-    {
-        $query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
-        .PHP_EOL.' FROM homepage.quests'
-        .PHP_EOL.' WHERE q_status != 3'
-        .PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_de , q_title_us ) ASC'
-        ;
-    }
-    else
-    {
-        $query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
-        .PHP_EOL.' FROM homepage.quests'
-        .PHP_EOL.' WHERE q_status != 3 AND ( q_type != 2 OR q_user_id = '.$pgSQL->Quote( IllaUser::$ID ).' )'
-        .PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_de , q_title_us ) ASC'
-        ;
-    }
-	
-    $pgSQL->setQuery( $query );
-    $quests = $pgSQL->loadAssocList();
-	if ( count($quests) > 0):
-?>
-
 <h1>Aktuelle News</h1>
 
 <?php
-    $newsRenderer = new \News\Renderer\HTMLRenderer(IllaUser::auth('news'));
-    $newsDb = new \News\NewsDatabase(IllaUser::auth('news'));
-    echo $newsRenderer->renderList($newsDb->getNewsList(3), 'de')
+$newsRenderer = new \News\Renderer\HTMLRenderer(IllaUser::auth('news'));
+$newsDb = new \News\NewsDatabase();
+echo $newsRenderer->renderList($newsDb->getNewsList(1), 'de')
 ?>
-
-<h1>Quests und Events</h1>
-
-<h2>Angekündigte Ereignisse</h2>
-
-<table class="quests">
-	<?php foreach($quests as $key=>$quest): ?>
-	<?php
-		if ($quest['q_type'] == 2)
-		{
-			$quest['q_status'] = 3;
-		}
-		if (!is_null( $quest['q_starttime'] ))
-		{
-		    $quest['q_starttime'] = strtotime( $quest['q_starttime'] );
-		}
-	?>
-	<tr>
-		<td class="title">
-			<a style="font-weight:bold;" href="<?php echo Page::getURL(); ?>/statistics/de_quests.php?id=<?php echo $quest['q_id']; ?>">
-				<?php echo ( is_null($quest['q_title_de']) ? $quest['q_title_us'] : $quest['q_title_de'] ); ?>
-			</a>
-		</td>
-		<td class="type"><?php echo ( $quest['q_type'] == 1 ? 'Offizielle Quest' : 'Spieler-Quest'); ?></td>
-		<td class="status<?php echo $quest['q_status']; ?>">
-			<?php
-				switch($quest['q_status'])
-				{
-					case 0:	echo 'Quest in Planung'; break;
-					case 1:	echo 'Quest startet in Kürze'; break;
-					case 2:	echo 'Quest läuft zur Zeit'; break;
-					case 3:	echo 'Nicht aktiviert'; break;
-				}
-				if (!is_null( $quest['q_starttime'] ))
-				{
-					echo '<br />', strftime( '%d. %B %Y - %H:%M', IllaDateTime::TimestampWithOffset( $quest['q_starttime'] ) );
-				}
-			?>
-		</td>
-	</tr>
-	<?php endforeach; ?>
-</table>
-
-<?php endif; ?>
-
-<?php if (IllaUser::auth('quests')): ?>
-<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/de_quests_edit.php'">Neue Quest eintragen</button></p>
-<?php elseif(IllaUser::loggedIn()): ?>
-<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/de_quests_edit.php'">Neue Spieler-Quest eintragen</button></p>
-<?php endif; ?>
-
-<?php if ( count($quests) > 0): ?>
-<?php Page::insert_go_to_top_link(); ?>
-<?php endif; ?>
--- website/general/us_startpage.php
+++ website/general/us_startpage.php
@@ -15,32 +15,12 @@
 	
 ?>
 
-<h1>The World Illarion</h1>
-
-<?php Page::cap('T'); ?>
-<p class="hyphenate">
-he lands are in turmoil. The War of the Gems has shaken the
-realms to their core. Refugees flock to the bastions of the land Illarion
-that were spared from the hardships of the past. Six gems of power were
-given to the Lords of these bastions for safekeeping; but jealousy,
-betrayal and envy are the ever present scourges in the constant struggle
-for power. Noble <a href="/illarion/us_factions.php#1">Cadomyr</a>, wealthy 
-<a href="/illarion/us_factions.php#2">Galmair</a> or wise 
-<a href="/illarion/us_factions.php#3">Runewick</a> - whose side will you join?</p>
+<h1>Illarion - Carve yourself a new life</h1>
 
 <?php Page::cap('I'); ?>
-<p class="hyphenate">
-llarion is a free open source MMORPG that focuses on true role playing.
-All of the characters that you will encounter during your time here are
-living, breathing inhabitants of this mysterious world. Each character has
-their own past, goals, flaws, strengths and personality. Experience
-glorious adventures as a noble knight or live the life of a hardworking
-craftsman, acquisitive merchant, or charismatic priest of the <a href="/illarion/goetter/us_bck_10.php">gods</a>.
-The decisions that you make while playing Illarion will actually impact and
-shape the world around you. Your actions will determine the events that will
-one day fill the pages of Illarion's history books.</p>
+<p class="hyphenate">llarion is an online game where genuine roleplay is enforced. The main design principle is a focus on the interaction with other players. All the player characters you will encounter during your time here are living, breathing inhabitants of the mysterious world of Illarion. Each character has its own past, goals, strengths, flaws and personality. Experience glorious adventures as noble knight or live the life of a hardworking craftsman, acquisitive merchant or charismatic priest of the gods. Noble <a href="/illarion/us_factions.php#1">Cadomyr</a>, wealthy <a href="/illarion/us_factions.php#2">Galmair</a> or wise <a href="/illarion/us_factions.php#3">Runewick</a> - whose side will you join?</p>
 
-<p>Illarion - What role will you play? <a href="/illarion/us_java_download.php">Download now and start your adventure!</a></p>
+<p class="hyphenate">Illarion - What role will you play? <a href="/illarion/us_java_download.php">Download now and start your adventure!</a></p>
 
 <?php foreach( $xmlC->obj_data->screenshots[0]->group as $currGroup ): ?>
 <div><a id="group<?php echo $currGroup->index; ?>"></a></div>
@@ -56,89 +36,10 @@ one day fill the pages of Illarion's history books.</p>
 
 <div class="clr"></div>
 
-<?php
-	$pgSQL =& Database::getPostgreSQL();
-
-	if(IllaUser::auth('quests'))
-	{
-		$query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
-		.PHP_EOL.' FROM homepage.quests'
-		.PHP_EOL.' WHERE q_status != 3'
-		.PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_us , q_title_de ) ASC'
-		;
-	}
-	else
-	{
-		$query = 'SELECT q_title_de, q_title_us, q_status, q_id, q_type, q_starttime'
-		.PHP_EOL.' FROM homepage.quests'
-		.PHP_EOL.' WHERE q_status != 3 AND ( q_type != 2 OR q_user_id = '.$pgSQL->Quote( IllaUser::$ID ).' )'
-		.PHP_EOL.' ORDER BY q_starttime ASC, q_status DESC, q_type DESC, COALESCE( q_title_us , q_title_de ) ASC'
-		;
-	}
-
-	$pgSQL->setQuery( $query );
-	$quests = $pgSQL->loadAssocList();
-	if ( count($quests) > 0):
-?>
-
-<h1>News</h1>
+<h1>Latest news</h1>
 
 <?php
 $newsRenderer = new \News\Renderer\HTMLRenderer(IllaUser::auth('news'));
 $newsDb = new \News\NewsDatabase();
-echo $newsRenderer->renderList($newsDb->getNewsList(3), 'en')
+echo $newsRenderer->renderList($newsDb->getNewsList(1), 'en')
 ?>
-
-<h1>Quests and Events</h1>
-
-<h2>Announced Events</h2>
-
-<table class="quests">
-	<?php foreach($quests as $key=>$quest): ?>
-	<?php
-		if ($quest['q_type'] == 2)
-		{
-			$quest['q_status'] = 3;
-		}
-		if (!is_null( $quest['q_starttime'] ))
-		{
-		    $quest['q_starttime'] = strtotime( $quest['q_starttime'] );
-		}
-	?>
-	<tr>
-		<td class="title">
-			<a style="font-weight:bold;" href="<?php echo Page::getURL(); ?>/statistics/us_quests.php?id=<?php echo $quest['q_id']; ?>">
-				<?php echo ( is_null($quest['q_title_us']) ? $quest['q_title_de'] : $quest['q_title_us'] ); ?>
-			</a>
-		</td>
-		<td class="type"><?php echo ( $quest['q_type'] == 1 ? 'Official Quest' : 'Player Quest'); ?></td>
-		<td class="status<?php echo $quest['q_status']; ?>">
-			<?php
-				switch($quest['q_status'])
-				{
-					case 0:	echo 'Quest is planned'; break;
-					case 1:	echo 'Quest starts soon'; break;
-					case 2:	echo 'Quest takes place now'; break;
-					case 3:	echo 'Not activated'; break;
-				}
-				if (!is_null( $quest['q_starttime'] ))
-				{
-					echo '<br />', strftime( '%d. %B %Y %I:%M %P', IllaDateTime::TimestampWithOffset( $quest['q_starttime'] ) );
-				}
-			?>
-		</td>
-	</tr>
-	<?php endforeach; ?>
-</table>
-
-<?php endif; ?>
-
-<?php if (IllaUser::auth('quests')): ?>
-<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/us_quests_edit.php'">New quest</button></p>
-<?php elseif (IllaUser::loggedIn()): ?>
-<p><button onclick="window.location.href='<?php echo Page::getURL(); ?>/statistics/us_quests_edit.php'">New player quest</button></p>
-<?php endif; ?>
-
-<?php if ( count($quests) > 0): ?>
-<?php Page::insert_go_to_top_link(); ?>
-<?php endif; ?>
