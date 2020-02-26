69707fc59968de746fc7fb633ac61c4d95eb3092
estralis <estralis@illarion.org>
2020-02-26 09:41:43 +0100

    Remove IRC chat (no activity since 2018)

--- website/community/de_chat.php
+++ website/community/de_chat.php
@@ -19,20 +19,4 @@ Page::Init();
 
 <p>Solltest du dich direkt mit Discord verbinden wollen, hier der nötige Einladungslink: <a href="https://discord.gg/yj3htPN">https://discord.gg/yj3htPN</a></p>
 
-<h2>Illarion IRC-Chat</h2>
-
-<iframe src="https://webchat.quakenet.org/?channels=illarion&amp;uio=OT10cnVlJjExPTEzMw1a" width="700" height="500"></iframe>
-
-<h3>Information über IRC</h3>
-
-<p>Auf dieser Seite wird eine Verbindung zum Quakenet IRC-Servernetzwerk hergestellt um dem Illarion-Chat beizutreten. In diesem Chat gelten sowohl die <a href="<?php echo $url; ?>/illarion/de_rules.php">Regeln von Illarion</a> als auch die <a href="https://www.quakenet.org/">Regeln von Quakenet</a>.</p>
-
-<p>Solltest du einen anderen IRC-Client verwenden wollen sind dies die nötigen Zugangsdaten:</p>
-<dl>
-	<dt>Server: irc.quakenet.org</dt>
-	<dt>Channel: #illarion</dt>
-</dl>
-
-<p><span style="font-weight:bold;">Wichtig:</span> Javascript muss in deinem Browser aktiviert sein um den Chat-Client zu verwenden.</p>
-
 <?php include_footer(); ?>
--- website/community/us_chat.php
+++ website/community/us_chat.php
@@ -19,23 +19,4 @@ Page::Init();
 
 <p>If you want to connect directly to Discord, here is the invitation link: <a href="https://discord.gg/yj3htPN">https://discord.gg/yj3htPN</a></p>
 
-<h2>Illarion IRC Chat</h2>
-
-<iframe src="https://webchat.quakenet.org/?channels=illarion&amp;uio=OT10cnVlJjExPTEzMw1a" width="700" height="500"></iframe>
-
-<h3>Information about IRC</h3>
-
-<p>This applet establishes a connection to the Quakenet IRC
-Server network and enters the Illarion Chat room. In this chat room the
-<a href="<?php echo $url; ?>/illarion/us_rules.php">rules of Illarion</a> and
-the rules of <a href="http://www.quakenet.org/">Quakenet</a> apply.</p>
-
-<p>In case you want to use a dedicated IRC client you may connect with the following information</p>
-<dl>
-	<dt>Server: irc.quakenet.org</dt>
-	<dt>Channel: #illarion</dt>
-</dl>
-
-<p><span style="font-weight:bold;">Important:</span> Javascript must be enabled to use this chat client.</p>
-
 <?php include_footer(); ?>
