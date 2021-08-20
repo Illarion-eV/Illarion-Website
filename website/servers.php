73f2ec5e6e1e32e788656aef0adda3a338384636
vilarion <vilarion@illarion.org>
2021-08-21 00:19:48 +0200

    Set json content header

--- website/servers.php
+++ website/servers.php
@@ -1,4 +1,5 @@
 <?php
 $servers = array(array('port' => 3008, 'name' => 'Illarion'), array('port' => 3012, 'name' => 'Development'));
+header('Content-Type:application/json');
 echo json_encode($servers);
 ?>
