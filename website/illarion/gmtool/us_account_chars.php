21db5cb62676df7890a94c5bc005959fb6cde121
brightrim <awildbrightrim@gmail.com>
2025-09-11 18:38:57 +0200

    us account chars fix

--- website/illarion/gmtool/us_account_chars.php
+++ website/illarion/gmtool/us_account_chars.php
@@ -6,7 +6,7 @@
 
 	if (!IllaUser::auth('gmtool_accounts'))
 	{
-		Messages::add('Access denided', 'error');
+		Messages::add('Access denied', 'error');
 		includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/us_gmtool.php' );
 		exit();
 	}
@@ -29,8 +29,8 @@
 
 	$charlist = getChars( $accid );
 
-	Page::setTitle( array( 'GM-Tool', 'Characterlist', $account_name ) );
-	Page::setDescription( 'A overview about all characters that were created in the account "'.$account_name.'"' );
+	Page::setTitle( array( 'GM-Tool', 'Character List', $account_name ) );
+	Page::setDescription( 'An overview of all characters that were created in the account "'.$account_name.'"' );
 	Page::setKeywords( array( 'GM-Tool', 'Account', 'Characters', 'Overview', $account_name ) );
 
 	Page::addCSS( array( 'menu', 'gmtool' ) );
@@ -52,24 +52,41 @@
 <table style="width:100%;">
 	<thead>
 		<tr>
-			<th style="width:90px">ID</th>
+			<th style="width:90px;">ID</th>
 			<th>Name</th>
-			<th style="width:100px">Race</th>
-			<th style="width:70px">Sex</th>
-			<th style="width:150px">Status</th>
-			<th style="width:80px">Server</th>
+			<th style="width:100px;">Race</th>
+			<th style="width:70px;">Sex</th>
+			<th style="width:150px;">Status</th>
+			<th style="width:80px;">Server</th>
 		</tr>
 	</thead>
 	<tbody>
-		<?php foreach ($charlist as $key=>$char): ?>
-		<tr class="row<?php echo (($key+1)%2); ?>">
-			<td><?php echo $char['chr_playerid']; ?></td>
-			<td><a href="<?php echo Page::getURL(); ?>/illarion/gmtool/us_character.php?id=<?php echo $char['chr_playerid']; ?>"><?php echo $char['chr_name']; ?></a></td>
-			<td><?php echo IllarionData::getRaceName($char['chr_race']); ?></td>
-			<td><?php echo IllarionData::getSexName($char['chr_sex']); ?></td>
-			<td><?php echo IllarionData::getCharacterStatusName($char['chr_status']); ?></td>
-			<td><?php echo $char['chr_server']; ?></td>
-		</tr>
-		<?php endforeach; ?>
+		<?php if (count($charlist) == 0) { ?>
+			<tr>
+				<td style='height:50px;text-align:center;' colspan='6'>
+					No characters were found.
+				</td>
+			</tr>
+		<?php } else { ?>
+			<?php foreach ($charlist as $key=>$char): ?>
+			<?php 
+				$class = ( in_array($char['chr_status'], array(CHAR_STATUS_BANNED, CHAR_STATUS_TEMP_BANNED, CHAR_STATUS_JAILED, CHAR_STATUS_TEMP_JAILED)) ) 
+					? "rowerror" 
+					: "row".(($key+1)%2); 
+			?>
+			<tr class="<?php echo $class; ?>">
+				<td><?php echo $char['chr_playerid']; ?></td>
+				<td>
+					<a href="<?php echo Page::getURL(); ?>/illarion/gmtool/us_character.php?id=<?php echo $char['chr_playerid']; ?>">
+						<?php echo $char['chr_name']; ?>
+					</a>
+				</td>
+				<td><?php echo IllarionData::getRaceName($char['chr_race']); ?></td>
+				<td><?php echo IllarionData::getSexName($char['chr_sex']); ?></td>
+				<td><?php echo IllarionData::getCharacterStatusName($char['chr_status']); ?></td>
+				<td><?php echo $char['chr_server']; ?></td>
+			</tr>
+			<?php endforeach; ?>
+		<?php } ?>
 	</tbody>
 </table>
