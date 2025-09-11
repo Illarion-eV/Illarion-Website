285a5b9f9051921a63c11d2fb7cd66bf556b13ff
brightrim <awildbrightrim@gmail.com>
2025-09-11 18:52:17 +0200

    us account chars fix

--- website/illarion/gmtool/us_account_chars.php
+++ website/illarion/gmtool/us_account_chars.php
@@ -2,44 +2,45 @@
 	include $_SERVER['DOCUMENT_ROOT'] . '/shared/shared.php';
 	includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/inc_topmenu.php' );
 	includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/inc_accountmenu.php' );
-	includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/inc_account_chars.php' );
+	includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/inc_account.php' );
 
 	if (!IllaUser::auth('gmtool_accounts'))
 	{
-		Messages::add('Access denied', 'error');
+		Messages::add( ('Access denied'), 'error' );
 		includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/us_gmtool.php' );
 		exit();
 	}
+
+    Page::setTitle( array( 'GM-Tool', 'Characterlist' ) );
+    Page::setDescription( 'A overview about all characters that were created in the account.' );
+    Page::setKeywords( array( 'GM-Tool', 'Account', 'Characters', 'Overview') );
+
+    Page::addCSS( array( 'menu', 'gmtool' ) );
+
+    Page::setXHTML();
+    Page::Init();
 
 	$accid = ( is_numeric($_GET['accid']) ? (int)$_GET['accid'] : 0 );
 	if (!$accid)
 	{
-		Messages::add('Account ID was not found', 'error');
+		Messages::add( ('Account ID was not transferred correctly'), 'error' );
 		includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/us_gmtool.php' );
 		exit();
 	}
-
-	$account_name = getAccountName( $accid );
-	if (!$account_name || !strlen($account_name))
+	$account_login = getAccountLogin( $accid );
+	if (!$account_login || !strlen($account_login))
 	{
-		Messages::add('Account not found', 'error');
+		Messages::add( ('Account not found'), 'error' );
 		includeWrapper::includeOnce( Page::getRootPath().'/illarion/gmtool/us_gmtool.php' );
 		exit();
 	}
 
 	$charlist = getChars( $accid );
 
-	Page::setTitle( array( 'GM-Tool', 'Character List', $account_name ) );
-	Page::setDescription( 'An overview of all characters that were created in the account "'.$account_name.'"' );
-	Page::setKeywords( array( 'GM-Tool', 'Account', 'Characters', 'Overview', $account_name ) );
-
-	Page::addCSS( array( 'menu', 'gmtool' ) );
-
-	Page::setXHTML();
-	Page::Init();
+	
 ?>
 
-<h1>Character List - <?php echo $account_name; ?></h1>
+<h1>Charakterliste - <?php echo $account_login; ?></h1>
 
 <?php include_menu(); ?>
 
@@ -54,33 +55,22 @@
 		<tr>
 			<th style="width:90px;">ID</th>
 			<th>Name</th>
-			<th style="width:100px;">Race</th>
-			<th style="width:70px;">Sex</th>
+			<th style="width:100px;">Rasse</th>
+			<th style="width:70px;">Geschlecht</th>
 			<th style="width:150px;">Status</th>
 			<th style="width:80px;">Server</th>
 		</tr>
 	</thead>
 	<tbody>
-		<?php if (count($charlist) == 0) { ?>
-			<tr>
-				<td style='height:50px;text-align:center;' colspan='6'>
-					No characters were found.
-				</td>
-			</tr>
-		<?php } else { ?>
+		<?php if (count($charlist) == 0)
+        { ?>
+            <tr><td style='height:50px;text-align:center;' colspan='6'>No characters were found.</td></tr>
+	    <?php } else { ?>
 			<?php foreach ($charlist as $key=>$char): ?>
-			<?php 
-				$class = ( in_array($char['chr_status'], array(CHAR_STATUS_BANNED, CHAR_STATUS_TEMP_BANNED, CHAR_STATUS_JAILED, CHAR_STATUS_TEMP_JAILED)) ) 
-					? "rowerror" 
-					: "row".(($key+1)%2); 
-			?>
-			<tr class="<?php echo $class; ?>">
+			<?php $class = ( in_array($char['chr_status'],array(CHAR_STATUS_BANNED, CHAR_STATUS_TEMP_BANNED,CHAR_STATUS_JAILED,CHAR_STATUS_TEMP_JAILED))) ? "rowerror" : "row".(($key+1)%2); ?>
+            <tr class="<?php echo $class; ?>">
 				<td><?php echo $char['chr_playerid']; ?></td>
-				<td>
-					<a href="<?php echo Page::getURL(); ?>/illarion/gmtool/us_character.php?id=<?php echo $char['chr_playerid']; ?>">
-						<?php echo $char['chr_name']; ?>
-					</a>
-				</td>
+				<td><a href="<?php echo Page::getURL(); ?>/illarion/gmtool/us_character.php?charid=<?php echo $char['chr_playerid']; ?>"><?php echo $char['chr_name']; ?></a></td>
 				<td><?php echo IllarionData::getRaceName($char['chr_race']); ?></td>
 				<td><?php echo IllarionData::getSexName($char['chr_sex']); ?></td>
 				<td><?php echo IllarionData::getCharacterStatusName($char['chr_status']); ?></td>
